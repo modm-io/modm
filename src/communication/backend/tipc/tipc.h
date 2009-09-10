876ff63adb17bc6475d66763268cdefbfbfe2052
Martin Rosekeit <martin.rosekeit@rwth-aachen.de>
2009-09-10 22:19:53 +0000

    Documentation to TIPC
    Public Interface of the class.

--- src/communication/backend/tipc/tipc.h
+++ src/communication/backend/tipc/tipc.h
@@ -28,19 +28,108 @@
  * $Id$
  */
 // ----------------------------------------------------------------------------
-#ifndef TIPC_H_
-#define TIPC_H_
+#ifndef XPCC_TIPC_H_
+#define XPCC_TIPC_H_
 
 /**
- * @ingroup		icc
- * @defgroup 	tipc (TIPC) Transparent Inter Process Communication
+ * @ingroup		communication
+ * @defgroup 	tipc (TIPC) Transparent Inter-Process Communication
  * @brief 		Interface to the Linux-TIPC.
  * 
- * Use intern the TIPC-Socket-Interface to comunicate.
+ * TIPC is a network transparent, packet orientated communication protocol that
+ * is part of the linux kernel. In the xpcc it is used in broadcast mode with
+ * filter on incoming messages. So it has the same structure as the CAN
+ * communication. Compared to TCP/IP this does not need a master in the system
+ * and because of the given filter functionality, it produce less traffic and a
+ * faster transport.
+ *
+ * @section	tipc_project	The TIPC project
+ *
+ * (from the TIPC web page: http://tipc.sourceforge.net/)
+ *
+ * The Transparent Inter-Process Communication protocol allows applications in a
+ * clustered computer environment to communicate quickly and reliably with other
+ * applications, regardless of their location within the cluster. TIPC
+ * originated at the telecommunications manufacturer, Ericsson, and has been
+ * deployed in their products for years.
+ *
+ * The TIPC project is an open source implementation of TIPC, and its software
+ * is now being used in commercial, educational, and private products around the
+ * world. TIPC applications are typically written in C (or C++) and utilize
+ * sockets of the AF_TIPC address family. Support for Perl, Python, Ruby, and D
+ * is also available.
+ *
+ * TIPC continues to evolve to meet the needs of its users. Users and developers
+ * who are interested in contributing enhancements or bug fixes to TIPC can
+ * obtain further information by contacting the TIPC project team on its mailing
+ * list.
+ *
+ * @section tipc_setup	Setup your System to Use TIPC
+ * Since kernel 2.6.16 TIPC is part of the linux kernel. Please check if TIPC is
+ * shipped with your distribution or if you have to compile your kernel.
+ *
+ * For local communication you need only to activate TIPC with
+ * @code
+ *  # modprobe tipc
+ * @endcode
+ *
+ * For communication with other PCs in a network you need to configure the TIPC
+ * with the tool "tipc-config" or "tipcutils". It should be provided with the most
+ * distributions or see the TIPC web page.
+ *
+ * Here is a example configuration:
+ * @code
+ * 	# "true" or "false" to en-/disable TIPC
+ *	TIPC_CONFIGURED=true
+ *
+ *	# the local tipc address (each PC in the network need its unique ADDR
+ *	TIPC_ADDR=1.1.1
+ *
+ *	# the network id used by tipc (all members of the communication need the same NETID)
+ *	TIPC_NETID=1234
+ *
+ *	# interfaces to enable for TIPC
+ *	# the elements of this list are separated by spaces
+ *	TIPC_INTERFACES="eth0"
+ * @endcode
  * 
  * @version		$Id$
  * @author		Carsten Schmidt,
  * 				Martin Rosekeit <martin.rosekeit@rwth-aachen.de>
  */
+
+#include "../backend_interface.h"
+
+namespace xpcc {
+
+	class Tipc : public BackendInterface
+	{
+	public:
+		Tipc();
+
+		~Tipc();
+
+		void
+		addFilter(
+				Type type,
+				Direction direction,
+				uint8_t destination,
+				uint8_t actionIdentifier);
+
+		virtual bool
+		sendPacket(Packet &packet);
+
+		virtual bool
+		isPacketAvailable() const;
+
+		virtual void
+		retrievePacket();
+
+		virtual const Packet&
+		getPacket() const;
+	};
+
+
+}
  
-#endif /*TIPC_H_*/
+#endif /*XPCC_TIPC_H_*/
