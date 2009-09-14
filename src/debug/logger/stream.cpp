bfda286cc30e58e6c31b7721ae1c68cbafd5c7e3
Martin Rosekeit <martin.rosekeit@rwth-aachen.de>
2009-09-14 21:57:54 +0000

    minimized stream object

---
+++ src/debug/logger/stream.cpp
@@ -0,0 +1,108 @@
+// coding: utf-8
+// ----------------------------------------------------------------------------
+/* Copyright (c) 2009, Roboterclub Aachen e.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Roboterclub Aachen e.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ROBOTERCLUB AACHEN E.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: logger_message_forwarder.hpp 18 2009-09-10 21:20:17Z thundernail $
+ */
+// ----------------------------------------------------------------------------
+
+#include "stream.hpp"
+#include <stdio.h>
+#include <string.h>
+
+#define DEFAULT_SIZE 8
+
+xpcc::Stream::Stream() :
+	container ( new char[DEFAULT_SIZE] ),
+	size ( DEFAULT_SIZE ),
+	pos ( 0 )
+{
+}
+
+// -----------------------------------------------------------------------------
+
+xpcc::Stream::~Stream()
+{
+	delete[] this->container;
+}
+
+// -----------------------------------------------------------------------------
+
+xpcc::Stream&
+xpcc::Stream::operator<< ( uint8_t value )
+{
+	while( this->pos + 4 > this->size ) {
+		if( !this->allocate() ) {
+			return *this;
+		}
+	}
+
+	this->pos += sprintf(&this->container[this->pos], "%u", value);
+
+	return *this;
+}
+
+//------------------------------------------------------------------------------
+
+void
+xpcc::Stream::addString( const char* str, uint8_t need )
+{
+	while( this->pos + need > this->size ) {
+		if( !this->allocate() ) {
+			return;
+		}
+	}
+
+	memcpy( &this->container[this->pos], str, need );
+}
+
+// -----------------------------------------------------------------------------
+
+bool
+xpcc::Stream::allocate()
+{
+	char* tmp = new char[2*this->size];
+	// TODO: check that allocation was successful
+
+	memcpy( tmp, &this->container[0], this->size );
+
+	delete[] this->container;
+
+	this->container = tmp;
+	this->size = 2*this->size;
+
+	return true;
+}
+
+// -----------------------------------------------------------------------------
+
+std::ostream&
+xpcc::operator<<(std::ostream& os, const xpcc::Stream& c)
+{
+	os << std::string(c.container);
+
+	return os;
+}
---
+++ src/debug/logger/stream.hpp
@@ -0,0 +1,79 @@
+// coding: utf-8
+// ----------------------------------------------------------------------------
+/* Copyright (c) 2009, Roboterclub Aachen e.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Roboterclub Aachen e.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ROBOTERCLUB AACHEN E.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: logger_message_forwarder.hpp 18 2009-09-10 21:20:17Z thundernail $
+ */
+// ----------------------------------------------------------------------------
+
+#include <iostream>
+
+#ifndef XPCC_STREAM_HPP_
+#define XPCC_STREAM_HPP_
+
+namespace xpcc {
+	class Stream {
+		public :
+			Stream();
+
+			~Stream();
+
+			template<typename T>
+			inline Stream&
+			operator<< ( const T& c )
+			{
+				this->addString( (const char*)&c, sizeof(T) );
+
+				return *this;
+			}
+
+			Stream&
+			operator<< ( uint8_t );
+
+
+		protected :
+			void
+			addString( const char*, uint8_t size );
+
+			bool
+			allocate();
+
+		public :
+			friend std::ostream&
+			operator<<(std::ostream& os, const Stream& c);
+
+			char* 	container;
+			uint8_t	size;
+			uint8_t	pos;
+
+	};
+
+	std::ostream&
+	operator<<(std::ostream& os, const xpcc::Stream& c);
+};
+
+
+#endif /* XPCC_STREAM_HPP_ */
