4e67dde21b27f20c929cfd6746c0cd4379be0a88
Martin Rosekeit <martin.rosekeit@rwth-aachen.de>
2009-09-27 14:49:21 +0000

    Testcase for Logger using the DEV0 LogDevice, so the sice of the logger can be terminated.

---
+++ tests/debug/logger/dev0/test_logger_dev0.cpp
@@ -0,0 +1,53 @@
+// coding: utf-8
+// ----------------------------------------------------------------------------
+/* Copyright (c) 2009, Roboterclub Aachen e.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Roboterclub Aachen e.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ROBOTERCLUB AACHEN E.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+// ----------------------------------------------------------------------------
+
+#include "../../../../src/debug/logger/logger.hpp"
+#include "../../../../src/debug/logger/backend/dev0/dev0_log_device.hpp"
+
+xpcc::log::DeviceDev0 device;
+
+
+// This is a test case, to calculate the size, a program with the logger
+// needs.
+// The logger device will forward the log messages to nowhere (internal dev0), so
+// there should be no output of this program.
+int
+main()
+{
+	xpcc::log::setDevice( device );
+
+	xpcc::log::setFilter(xpcc::log::WARNING);
+
+	xpcc::log::warning << 'L' << xpcc::flush;
+//	xpcc::log::debug << 100 << xpcc::flush;	// using snprintf() => 4kB more code
+
+	return 0;
+}
