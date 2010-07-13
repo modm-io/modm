c8b7158f8f7b9f49564dfd2c4b7c938dc2edcf05
Thorsten Lajewski <thorsten.lajewski@rwth-aachen.de>
2010-07-13 21:59:35 +0000

    added canusb and changed folderstructure

---
+++ src/xpcc/driver/can/canusb/canusb.hpp
@@ -0,0 +1,42 @@
+#ifndef XPCC__CAN_USB_HPP
+#define XPCC__CAN_USB_HPP
+
+namespace xpcc
+{
+	class CanUsb
+	{
+		CanUsb();
+		
+		~CanUsb();
+		
+		static bool open();
+		
+		static bool close();
+		
+		static inline bool
+		isMessageAvailable();
+		
+		static bool
+		getMessage(Can::Message& message);
+		
+		/*
+		 * The CAN controller has a free slot to send a new message.
+		 *
+		 * \return true if a slot is available, false otherwise
+		 */
+		static inline bool
+		canSend()
+		{
+			return true;
+		}
+		
+		/*
+		 * Send a message over the CAN.
+		 *
+		 * \return true if the message was send, false otherwise
+		 */
+		static bool
+		sendMessage(const Can::Message& message);	
+	};
+}
+#endif // XPCC__CAN_USB_HPP
