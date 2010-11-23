43f1ae62a59390da1c2808cdf2d1667e8f78cda7
Fabian Greif <fabian.greif@rwth-aachen.de>
2010-11-23 16:48:26 +0000

    Simplified the MCP2515 example

--- examples/can/mcp2515/main.cpp
+++ examples/can/mcp2515/main.cpp
@@ -4,19 +4,18 @@
 #include <xpcc/driver/can/mcp2515.hpp>
 #include <xpcc/driver/software_spi.hpp>
 
-namespace pin
-{
-	GPIO__OUTPUT(CS, B, 4);
-	GPIO__INPUT(INT, B, 2);
+GPIO__OUTPUT(Cs, B, 4);
+GPIO__INPUT(Int, B, 2);
 
-	GPIO__OUTPUT(SCLK, B, 7);
-	GPIO__OUTPUT(MOSI, B, 5);
-	GPIO__INPUT(MISO, B, 6);
-}
+GPIO__OUTPUT(Sclk, B, 7);
+GPIO__OUTPUT(Mosi, B, 5);
+GPIO__INPUT(Miso, B, 6);
 
-typedef xpcc::SoftwareSpi<pin::SCLK, pin::MOSI, pin::MISO> CanSpi;
-xpcc::Mcp2515< CanSpi, pin::CS, pin::INT > mcp2515;
+typedef xpcc::SoftwareSpi<Sclk, Mosi, Miso> Spi;
 
+xpcc::Mcp2515<Spi, Cs, Int> mcp2515;
+
+// Default filters to receive any extended CAN frame
 FLASH(uint8_t canFilter[]) =
 {
 	MCP2515_FILTER_EXTENDED(0),	// Filter 0
@@ -34,20 +33,24 @@ FLASH(uint8_t canFilter[]) =
 int
 main()
 {
-	CanSpi::initialize();
-	pin::CS::setOutput();
-	pin::INT::configure(xpcc::gpio::PULLUP);
+	// Initialize SPI interface and the other pins
+	// needed by the MCP2515
+	Spi::initialize();
+	Cs::setOutput();
+	Int::configure(xpcc::gpio::PULLUP);
 	
+	// Configure MCP2515 and set the filters
 	mcp2515.initialize(xpcc::can::BITRATE_125_KBPS);
 	mcp2515.setFilter(xpcc::accessor::asFlash(canFilter));
 	
+	// Create a new message
 	xpcc::can::Message message(0x123456);
 	message.length = 2;
 	message.data[0] = 0xab;
 	message.data[1] = 0xcd;
-
+	
 	mcp2515.sendMessage(message);
-
+	
 	while (1)
 	{
 		if (mcp2515.isMessageAvailable())
