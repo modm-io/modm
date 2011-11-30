aa19b05d5eb6e64a8e5d72f1ed985c4948780866
Georgi Grinshpun <georgi.grinshpun@rwth-aachen.de>
2011-11-30 01:23:05 +0000

    driver for nokia6610 display connected by i2c with Epson S1D15G10 controller, not implemented for Philips PCF8833 due to not available test hardware.

---
+++ src/xpcc/driver/ui/display/nokia6610_defines.hpp
@@ -0,0 +1,133 @@
+// coding: utf-8
+// ----------------------------------------------------------------------------
+/* Copyright (c) 2009, Roboterclub Aachen e.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Roboterclub Aachen e.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ROBOTERCLUB AACHEN E.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+// ----------------------------------------------------------------------------
+
+#ifndef XPCC__NOKIA6610_DEFINES_HPP
+#define XPCC__NOKIA6610_DEFINES_HPP
+
+// ----------------------------------------------------------------------------
+// Defines for the NOKIA6610 display controller.
+// ----------------------------------------------------------------------------
+
+namespace xpcc{
+	namespace nokia {
+		enum NokiaGE12 {
+			NOKIA_GE12_NOP = 0x00, // nop
+			NOKIA_GE12_SOFTRST = 0x01, // software reset
+			NOKIA_GE12_BOOSTVOFF = 0x02, // booster voltage OFF
+			NOKIA_GE12_BOOSTVON = 0x03, // booster voltage ON
+			NOKIA_GE12_TESTMODE1 = 0x04, // test mode
+			NOKIA_GE12_DISPSTATUS = 0x09, // display status
+			NOKIA_GE12_SLEEPIN = 0x10, // sleep in
+			NOKIA_GE12_SLEEPOUT = 0x11, // sleep out
+			NOKIA_GE12_PARTIAL = 0x12, // partial display mode
+			NOKIA_GE12_NORMALMODE = 0x13, // display normal mode
+			NOKIA_GE12_INVERSIONOFF = 0x20, // inversion OFF
+			NOKIA_GE12_INVERSIONON = 0x21, // inversion ON
+			NOKIA_GE12_ALLPIXELOFF = 0x22, // all pixel OFF
+			NOKIA_GE12_ALLPIXELON = 0x23, // all pixel ON
+			NOKIA_GE12_CONTRAST = 0x25, // write contrast
+			NOKIA_GE12_DISPLAYOFF = 0x28, // display OFF
+			NOKIA_GE12_DISPLAYON = 0x29,// display ON
+			NOKIA_GE12_COLADDRSET = 0x2A, // column address set
+			NOKIA_GE12_PAGEADDRSET = 0x2B, // page address set
+			NOKIA_GE12_MEMWRITE = 0x2C, // memory write
+			NOKIA_GE12_COLORSET = 0x2D, // colour set
+			NOKIA_GE12_READRAMDATA = 0x2E, // RAM data read
+			NOKIA_GE12_PARTIALAREA = 0x30, // partial area
+			NOKIA_GE12_VERTSCROLL = 0x33, // vertical scrolling definition
+			NOKIA_GE12_TESTMODE2 = 0x34,// test mode
+			NOKIA_GE12_TESTMODE3 = 0x35, // test mode
+			NOKIA_GE12_ACCESSCTRL = 0x36, // memory access control
+			NOKIA_GE12_VSCRLSADDR = 0x37, // vertical scrolling start address
+			NOKIA_GE12_IDLEOFF = 0x38, // idle mode OFF
+			NOKIA_GE12_IDLEON = 0x39, // idle mode ON
+			NOKIA_GE12_PIXELFORMAT = 0x3A, // interface pixel format
+			NOKIA_GE12_TESTMODE4 = 0xDE, // test mode
+			NOKIA_GE12_NOP2 = 0xAA, // nop
+			NOKIA_GE12_INITESC = 0xC6, // initial escape
+			NOKIA_GE12_TESTMODE5 = 0xDA, // test mode
+			NOKIA_GE12_TESTMODE6 = 0xDB, // test mode
+			NOKIA_GE12_TESTMODE7 = 0xDC, // test mode
+			NOKIA_GE12_TESTMODE8 = 0xB2, // test mode
+			NOKIA_GE12_GRAYSCALE0 = 0xB3, // gray scale position set 0
+			NOKIA_GE12_GRAYSCALE1 = 0xB4, // gray scale position set 1
+			NOKIA_GE12_GAMMA = 0xB5, // gamma curve set
+			NOKIA_GE12_DISPCTRL = 0xB6, // display control
+			NOKIA_GE12_TEMPGRADIENT = 0xB7, // temp gradient set
+			NOKIA_GE12_TESTMODE9 = 0xB8, // test mode
+			NOKIA_GE12_REFSET = 0xB9, // refresh set
+			NOKIA_GE12_VOLTCTRL = 0xBA, // voltage control
+			NOKIA_GE12_COMMONDRV = 0xBD, // common driver output select
+			NOKIA_GE12_PWRCTRL = 0xBE, // power control
+		};
+
+		enum NokiaGE8 {
+			NOKIA_GE8_DISON = 0xAF, // Display on
+			NOKIA_GE8_DISOFF = 0xAE, // Display off
+			NOKIA_GE8_DISNOR = 0xA6, // Normal display
+			NOKIA_GE8_DISINV = 0xA7, // Inverse display
+			NOKIA_GE8_COMSCN = 0xBB, // Common scan direction
+			NOKIA_GE8_DISCTL = 0xCA, // Display control
+			//NOKIA_GE8_DISCTL = 0xBA, // Display control
+			NOKIA_GE8_SLPIN = 0x95, // Sleep in
+			NOKIA_GE8_SLPOUT = 0x94, // Sleep out
+			NOKIA_GE8_PASET = 0x75, // Page address set
+			NOKIA_GE8_CASET = 0x15, // Column address set
+			NOKIA_GE8_DATCTL = 0xBC, // Data scan direction, etc.
+			NOKIA_GE8_RGBSET8 = 0xCE, // 256-color position set
+			NOKIA_GE8_RAMWR = 0x5C, // Writing to memory
+			NOKIA_GE8_RAMRD = 0x5D, // Reading from memory
+			NOKIA_GE8_PTLIN = 0xA8, // Partial display in
+			NOKIA_GE8_PTLOUT = 0xA9, // Partial display out
+			NOKIA_GE8_RMWIN = 0xE0, // Read and modify write
+			NOKIA_GE8_RMWOUT = 0xEE, // End
+			NOKIA_GE8_ASCSET = 0xAA, // Area scroll set
+			NOKIA_GE8_SCSTART = 0xAB, // Scroll start set
+			NOKIA_GE8_OSCON = 0xD1, // Internal oscillation on
+			NOKIA_GE8_OSCOFF = 0xD2, // Internal oscillation off
+			NOKIA_GE8_PWRCTR = 0x20, // Power control
+			NOKIA_GE8_VOLCTR = 0x81, // Electronic volume control
+			NOKIA_GE8_VOLUP = 0xD6, // Increment electronic control by 1
+			NOKIA_GE8_VOLDOWN = 0xD7, // Decrement electronic control by 1
+			NOKIA_GE8_TMPGRD = 0x82, // Temperature gradient set
+			NOKIA_GE8_EPCTIN = 0xCD, // Control EEPROM
+			NOKIA_GE8_EPCOUT = 0xCC, // Cancel EEPROM control
+			NOKIA_GE8_EPMWR = 0xFC, // Write into EEPROM
+			NOKIA_GE8_EPMRD = 0xFD, // Read from EEPROM
+			NOKIA_GE8_EPSRRD1 = 0x7C, // Read register 1
+			NOKIA_GE8_EPSRRD2 = 0x7D, // Read register 2
+			NOKIA_GE8_NOP = 0x25, // NOP instruction
+		};
+	}
+}
+
+#endif // XPCC__NOKIA6610_DEFINES_HPP
---
+++ src/xpcc/driver/ui/display/nokia6610_impl.hpp
@@ -0,0 +1,252 @@
+// coding: utf-8
+// ----------------------------------------------------------------------------
+/* Copyright (c) 2009, Roboterclub Aachen e.V.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Roboterclub Aachen e.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ROBOTERCLUB AACHEN E.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+// ----------------------------------------------------------------------------
+
+#ifndef XPCC__NOKIA6610_HPP
+#error	"Don't include this file directly, use 'nokia6610.hpp' instead!"
+#endif
+
+#include "nokia6610_defines.hpp"
+
+// ----------------------------------------------------------------------------
+template <typename SPI, typename CS, typename Reset, bool GE12>
+void
+xpcc::Nokia6610<SPI, CS, Reset, GE12>::initialize()
+{
+
+	// CS pin
+	CS::set();
+	CS::setOutput();
+
+
+	// Reset pin
+	Reset::setOutput();
+	Reset::reset();
+	xpcc::delay_ms(1);
+	Reset::set();
+	xpcc::delay_ms(10);
+
+
+	lcdSettings();
+
+	this->clear();
+//	this->update();
+}
+
+uint16_t temp = 0;				//
+uint8_t countValidBits = 0;		// always < 8
+
+template <typename SPI, typename CS, typename Reset, bool GE12>
+void
+xpcc::Nokia6610<SPI, CS, Reset, GE12>::writeSpi9Bit(uint16_t data){
+	// add new bits to temp
+	temp = temp | ((data&0x1ff)<< (7-countValidBits));
+	countValidBits += 9;
+
+	while(countValidBits >= 8){
+		SPI::write(temp>>8);
+		countValidBits -= 8;
+		temp <<= 8;
+	}
+}
+
+template <typename SPI, typename CS, typename Reset, bool GE12>
+void
+xpcc::Nokia6610<SPI, CS, Reset, GE12>::writeSpi9BitFlush(){
+	if (countValidBits > 0){
+		SPI::write(temp>>8);
+	}
+	countValidBits = 0;
+	temp = 0;
+}
+
+
+template <typename SPI, typename CS, typename Reset, bool GE12>
+void
+xpcc::Nokia6610<SPI, CS, Reset, GE12>::writeSpiCommand(uint16_t data){
+	writeSpi9Bit(data & ~0x0100);
+}
+
+template <typename SPI, typename CS, typename Reset, bool GE12>
+void
+xpcc::Nokia6610<SPI, CS, Reset, GE12>::writeSpiData(uint16_t data){
+	writeSpi9Bit(data | 0x0100);
+}
+
+template <typename SPI, typename CS, typename Reset, bool GE12>
+void
+xpcc::Nokia6610<SPI, CS, Reset, GE12>::setContrast(uint8_t contrast) {
+	CS::reset();
+
+	if (GE12){
+	}
+	else{
+		writeSpiCommand(nokia::NOKIA_GE8_VOLCTR);
+		writeSpiData(contrast); // contrast
+		writeSpiData(3);
+	}
+
+	writeSpi9BitFlush();
+
+	CS::set();
+}
+
+template <typename SPI, typename CS, typename Reset, bool GE12>
+void
+xpcc::Nokia6610<SPI, CS, Reset, GE12>::lcdSettings() {
+	CS::reset();
+
+	if( GE12){
+	}
+	else{
+		// Hardware reset
+		Reset::reset();
+		xpcc::delay_ms(50);
+		Reset::set();
+		xpcc::delay_ms(50);
+
+		// Display vontrol
+		writeSpiCommand(nokia::NOKIA_GE8_DISCTL);
+		writeSpiData(0x00); // default
+		writeSpiData(0x20); // (32 + 1) * 4 = 132 lines (of which 130 are visible)
+		writeSpiData(0x0a); // default
+
+		// COM scan
+		writeSpiCommand(nokia::NOKIA_GE8_COMSCN);
+		writeSpiData(0x00);  // Scan 1-80
+
+		// Internal oscilator ON
+		writeSpiCommand(nokia::NOKIA_GE8_OSCON);
+
+		writeSpi9BitFlush();
+
+		CS::set();
+		// wait aproximetly 100ms
+		xpcc::delay_ms(100);
+		CS::reset();
+
+		// Sleep out
+		writeSpiCommand(nokia::NOKIA_GE8_SLPOUT);
+
+		// Voltage control
+		writeSpiCommand(nokia::NOKIA_GE8_VOLCTR);
+		writeSpiData(43); // middle value of V1
+		writeSpiData(0x03); // middle value of resistance value
+
+		// Power control
+		writeSpiCommand(nokia::NOKIA_GE8_PWRCTR);
+		writeSpiData(0x0f);   // referance voltage regulator on, circuit voltage follower on, BOOST ON
+
+		// Data control
+		writeSpiCommand(nokia::NOKIA_GE8_DATCTL);
+		writeSpiData(0x04); // page scan
+		writeSpiData(0x00); // RGB sequence
+		writeSpiData(0x02); // 12bit per pixel mode A others may not work
+
+		// Page Address set
+		writeSpiCommand(nokia::NOKIA_GE8_PASET);
+		writeSpiData(2); // start at 2 others corrupt display settings in a unpredictable way
+		writeSpiData(2 + this->getHeight()-1 + 2); // 2 more for filling, but not handled by buffered display
+
+		// Page Column set
+		writeSpiCommand(nokia::NOKIA_GE8_CASET);
+		writeSpiData(0);
+		writeSpiData(0 + this->getWidth()-1);
+
+		// Display On
+		writeSpiCommand(nokia::NOKIA_GE8_DISON);
+
+	}
+
+	writeSpi9BitFlush();
+	CS::set();
+}
+
+template <typename SPI, typename CS, typename Reset, bool GE12>
+void
+xpcc::Nokia6610<SPI, CS, Reset, GE12>::update() {
+	CS::reset();
+
+	if (GE12)
+	{
+		return;
+	}
+	else
+	{
+		// Display OFF
+		//writeSpiCommand(nokia::NOKIA_GE8_DISOFF);
+
+		// WRITE MEMORY
+		writeSpiCommand(nokia::NOKIA_GE8_RAMWR);
+	}
+
+	static const uint32_t mask1Blank  = 0x00f000; // RGB000
+	static const uint32_t mask1Filled = 0xff0000; // RGB000
+
+	static const uint32_t mask2Blank = mask1Blank>>12; // 000RGB
+	static const uint32_t mask2Filled = mask1Filled>>12; // 000RGB
+
+	for(uint8_t x = 0; x < this->getWidth(); ++x)
+	{
+		for(uint8_t y = 0; y < this->getHeight()/8; ++y) {
+			uint8_t group = this->buffer[x][y];
+			for (uint8_t pix = 0; pix < 8; pix+=2, group>>=2){
+				uint32_t data =
+						((group&1)?mask1Filled:mask1Blank)|
+						((group&2)?mask2Filled:mask2Blank);
+
+				writeSpiData(data>>16);
+				writeSpiData(data>>8);
+				writeSpiData(data);
+			}
+		}
+		// fill pixel not handled by buffered display
+		uint32_t data = mask1Blank | mask2Blank;
+		writeSpiData(data>>16);
+		writeSpiData(data>>8);
+		writeSpiData(data);
+	}
+
+	if (GE12)
+	{
+	}
+	else
+	{
+		// wait aproximetly 100ms
+		xpcc::delay_ms(100);
+
+		// Display On
+		writeSpiCommand(nokia::NOKIA_GE8_DISON);
+	}
+
+	writeSpi9BitFlush();
+	CS::reset();
+}
