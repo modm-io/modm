6dbbc05140bd27348daaca9d7f4c7496183bd280
David Hebbeker <david.hebbeker@gmail.com>
2012-04-04 23:06:27 +0200

    Added some functionality to the internal ADC. Simple ADC readouts are possible.

--- tests/stm32_adc/Adc.hpp
+++ tests/stm32_adc/Adc.hpp
@@ -14,6 +14,74 @@ namespace xpcc
 {
 	namespace stm32
 	{
+
+	//Für ADC 1 und 2
+	enum Channels
+	{
+		PIN_C0 = 10,
+		PIN_C1 = 11,
+		PIN_C2 = 12,
+		PIN_C3 = 13,
+		PIN_A0 = 0,
+		PIN_A1 = 1,
+		PIN_A2 = 2,
+		PIN_A3 = 3,
+		PIN_A4 = 4,
+		PIN_A5 = 5,
+		PIN_A6 = 6,
+		PIN_A7 = 7,
+		PIN_C4 = 14,
+		PIN_C5 = 15,
+		PIN_B0 = 8,
+		PIN_B1 = 9,
+
+		TEMPERATURE_SENSOR = 16,
+		V_REFINT = 17,// internal reference voltage
+		VBAT = 18,
+
+		CHANNEL_0 = 0,
+		CHANNEL_1 = 1,
+		CHANNEL_2 = 2,
+		CHANNEL_3 = 3,
+		CHANNEL_4 = 4,
+		CHANNEL_5 = 5,
+		CHANNEL_6 = 6,
+		CHANNEL_7 = 7,
+		CHANNEL_8 = 8,
+		CHANNEL_9 = 9,
+		CHANNEL_10= 10,
+		CHANNEL_11 = 11,
+		CHANNEL_12 = 12,
+		CHANNEL_13 = 13,
+		CHANNEL_14 = 14,
+		CHANNEL_15 = 15,
+		CHANNEL_16 = 16,
+		CHANNEL_17 = 17,
+		CHANNEL_18 = 18
+	};
+
+	/* Für ADC 3
+	enum Channels{
+		PIN_F3 = 9,
+		PIN_F4 = 14,
+		PIN_F5 = 15,
+		PIN_F6 = 4,
+		PIN_F7 = 5,
+		PIN_F8 = 6,
+		PIN_F9 = 7,
+		PIN_F10 = 8,
+		PIN_C0 = 10,
+		PIN_C1 = 11,
+		PIN_C2 = 12,
+		PIN_C3 = 13,
+		PIN_A0 = 0,
+		PIN_A1 = 1,
+		PIN_A2 = 2,
+		PIN_A3 = 3,
+	};
+	*/
+
+
 		/**
 		 * \brief	Generic Analog/Digital-Converter module
 		 *
@@ -31,6 +99,26 @@ namespace xpcc
 				PRESCALER_8 = ADC_CCR_ADCPRE_1 | ADC_CCR_ADCPRE_0
 			};
 
+			enum SampleTime
+			{
+				CYCLES_3 	= 0b000,
+				CYCLES_15 	= 0b001,
+				CYCLES_28 	= 0b010,
+				CYCLES_56 	= 0b011,
+				CYCLES_84 	= 0b100,
+				CYCLES_112 	= 0b101,
+				CYCLES_144 	= 0b110,
+				CYCLES_480 	= 0b111
+			};
+
+			enum InterruptFlag
+			{
+				END_OF_CONVERSION_REGULAR	= ADC_SR_EOC,
+				END_OF_CONVERSION_INJECTED	= ADC_SR_JEOC,
+				ANALOG_WATCHDOG				= ADC_SR_AWD,
+				OVERRUN						= ADC_SR_OVR
+			};
+
 
 		public:
 
@@ -43,54 +131,29 @@ namespace xpcc
 			 * of any ongoing conversions.
 			 */
 			static inline void
-			setLeftAdjustResult(bool enable);
+			setLeftAdjustResult(const bool enable);
 
 			/**
 			 * \brief Analog channel selection
-			 *
-			 * Selects which analog inputs are connected to the ADC.
-			 * If this is changed during a conversion, the change will not go
-			 * in effect until this conversion is complete.
-			 *
-			 * Available on all ATmegas.
-			 */
-			static inline bool
-			setChannel(uint8_t channel);
-
-			/**
-			 * \return The analog channel connected to the ADC
-			 *
-			 * Available on all ATmegas.
-			 */
-			static inline uint8_t
-			getChannel();
-
-			/**
-			 * \brief Enables the ADC
-			 *
-			 * Available on all ATmegas.
 			 */
 			static inline void
-			setEnableAdc(bool enable) {}
+			setChannel(const Channels channel, const SampleTime sampleTime=CYCLES_3)
+			{
+				ADC1->SQR1 = 0;		// clear number of conversions in the sequence and set number of conversions to 1
+				ADC1->SQR3 |= channel;
 
-			/**
-			 * In Single Conversion mode, this starts each conversion.
-			 * In Free Running mode, this starts the first conversion.
-			 *
-			 * Available on all ATmegas.
-			 */
-			static inline void
-			setStartConversion() {}
+				if(static_cast<uint8_t>(channel) < 10)
+					ADC1->SMPR2 |= sampleTime << (static_cast<uint8_t>(channel) * 3);
+				else
+					ADC1->SMPR1 |= sampleTime << ((static_cast<uint8_t>(channel)-10) * 3);
 
-			/**
-			 * \brief Enable auto triggering of the ADC
-			 *
-			 * The ADC will start a conversion on a positive edge of the
-			 * selected trigger signal.
-			 * \see setAutoTriggerSource
-			 */
-			static inline void
-			setAutoTriggerEnable(bool enable);
+				if(channel <8)
+					GPIOA->MODER |= 0b11 << ((channel + 0) * 2);
+				else if(channel <10)
+					GPIOB->MODER |= 0b11 << ((channel - 8) * 2);
+				else
+					GPIOC->MODER |= 0b11 << ((channel - 10) * 2);
+			}
 
 			/**
 			 * \brief Enables free running mode
@@ -99,7 +162,16 @@ namespace xpcc
 			 * recent result in the ADC register.
 			 */
 			static inline void
-			setFreeRunningMode(bool enable);
+			enableFreeRunningMode(void)
+			{
+				ADC1->CR2 |= ADC_CR2_CONT;		// set to continuous mode
+			}
+
+			static inline void
+			disableFreeRunningMode(void)
+			{
+				ADC1->CR2 &= ~ADC_CR2_CONT;		// set to single
+			}
 
 			/**
 			 * \return \c true if the flag is set
@@ -107,7 +179,10 @@ namespace xpcc
 			 * Available on all ATmegas.
 			 */
 			static inline bool
-			isInterruptFlagSet() {}
+			isInterruptFlagSet(const InterruptFlag flag)
+			{
+				return ADC1->SR & flag;
+			}
 
 			/**
 			 * \brief Clears the interrupt flag
@@ -115,7 +190,18 @@ namespace xpcc
 			 * Available on all ATmegas.
 			 */
 			static inline void
-			clearInterruptFlag() {}
+			clearInterruptFlag(const InterruptFlag flag)
+			{
+				ADC1->SR &= ~flag;
+			}
+
+
+			static inline void
+			disableInterrupt(void)
+			{
+				NVIC_DisableIRQ(ADC_IRQn);
+				ADC1->CR1 &= ~ADC_CR1_EOCIE;
+			}
 
 			/**
 			 * \brief Enables the ADC Conversion Complete Interrupt
@@ -125,79 +211,56 @@ namespace xpcc
 			 * \see AdcInterrupt
 			 */
 			static inline void
-			setInterruptEnable(bool enable) {}
+			enableInterrupt(const uint8_t priority)
+			{
+				// Set priority for the interrupt vector
+				NVIC_SetPriority(ADC_IRQn, priority);
+
+				// register IRQ at the NVIC
+				NVIC_EnableIRQ(ADC_IRQn);
+				ADC1->CR1 |= ADC_CR1_EOCIE;
+			}
 
 			/**
 			 * Set the division factor between the system clock frequency
 			 * and the input clock to the ADC.
 			 */
 			static inline void
-			setPrescaler(Prescaler prescaler);
-
-
-			/**
-			 * \brief Selects which source will trigger an ADC conversion
-			 *
-			 * A conversion will be triggered by the rising edge of the
-			 * selected Interrupt Flag. Note that switching from a trigger
-			 * source that is cleared to a trigger source that is set, will
-			 * generate a positive edge on the trigger signal.
-			 * Set to 0 to enable Free Running Mode.
-			 */
-			static inline void
-			setAutoTriggerSource(uint8_t source);
-
-			static inline uint16_t
-			getDataRegister();
+			setPrescaler(const Prescaler prescaler)
+			{
+				ADC->CCR &= 3 << 17;
+				ADC->CCR |= prescaler << 17;
+			}
 
 			/**
 			 * \brief	Initialize and enable the A/D converter
 			 */
 			static inline void
-			initialize(Prescaler prescaler)
+			initialize(Prescaler prescaler=PRESCALER_2)
 			{
+				// Initialize ADC
+				RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; // start ADC Clock
+
+				ADC1->CR2 |= ADC_CR2_ADON;		// switch on ADC
+
 				setPrescaler(prescaler);
-				setEnableAdc(true);
 			}
 
-			/**
-			 * \brief Correctly enables Free Running Mode
-			 *
-			 * Available on all ATmegas.
-			 */
 			static inline void
-			enableFreeRunningMode()
+			shutdownADC(void)
 			{
-				setFreeRunningMode(true);
-			}
-
-			/**
-			 * \brief	Read the value an analog channel
-			 */
-			static inline uint16_t
-			readChannel(uint8_t channel)
-			{
-				if (!startConversion(channel)) return 0;
-
-				while (!isInterruptFlagSet()) {
-					// wait until the conversion is finished
-				}
-
-				return getDataRegister();
+				ADC1->CR2 &= ~(ADC_CR2_ADON);
+				RCC->APB2ENR &= ~RCC_APB2ENR_ADC1EN; // start ADC Clock
 			}
 
 			/**
 			 * \brief Start a new conversion
 			 */
-			static inline bool
-			startConversion(uint8_t channel)
+			static inline void
+			startConversion(void)
 			{
-				if (!setChannel(channel)) return false;
-
-				clearInterruptFlag();
-				setStartConversion();
-
-				return true;
+				clearInterruptFlag(static_cast<InterruptFlag>(END_OF_CONVERSION_REGULAR | END_OF_CONVERSION_INJECTED | ANALOG_WATCHDOG | OVERRUN));
+				ADC1->CR2 |= ADC_CR2_SWSTART;	// starts single conversion for the regular group
 			}
 
 			/**
@@ -206,9 +269,9 @@ namespace xpcc
 			 * Available on all ATmegas.
 			 */
 			static inline bool
-			isFinished()
+			isConversionFinished(void)
 			{
-				return isInterruptFlagSet();
+				return (ADC1->SR & ADC_SR_EOC);
 			}
 
 			/**
@@ -217,9 +280,9 @@ namespace xpcc
 			 * Available on all ATmegas.
 			 */
 			static inline uint16_t
-			getValue()
+			getValue(void)
 			{
-				return getDataRegister();
+				return ADC1->DR;
 			}
 
 		};
--- tests/stm32_adc/main.cpp
+++ tests/stm32_adc/main.cpp
@@ -1,6 +1,8 @@
 #include <xpcc/architecture.hpp>
 #include <xpcc/debug/logger.hpp>
 
+#include "Adc.hpp"
+
 /**! \file main.cpp
  * @author David Hebbeker
  */
@@ -82,77 +84,32 @@ static inline void enableInterrupt()
 	ADC1->CR1 |= ADC_CR1_EOCIE;
 }
 
+
+using namespace xpcc;
+
 MAIN_FUNCTION
 {
 	initClock();
 
 	uart.configurePins(uart.REMAP_PC12_PD2);
 
-	const uint8_t adc_channel = 1;
+	XPCC_LOG_INFO << xpcc::endl << "This is an ADC Test program by David." << xpcc::endl;
 
-	XPCC_LOG_INFO    << xpcc::endl << "This is an ADC Test program by David."    << xpcc::endl;
-
-	// --------------------------------------------------
-	// Here some (pseudo) code to use the ADC:
-	// --------------------------------------------------
-
-
-	GPIOA->MODER |= GPIO_MODER_MODER1;
-	GPIOA->MODER |= GPIO_MODER_MODER2;
-	GPIOA->MODER |= GPIO_MODER_MODER3;
-	GPIOA->MODER |= GPIO_MODER_MODER4;
-	GPIOA->MODER |= GPIO_MODER_MODER5;
-	GPIOA->MODER |= GPIO_MODER_MODER6;
-	GPIOA->MODER |= GPIO_MODER_MODER7;
-
-	// Initialize ADC
-	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; // start ADC Clock
-
-
-	ADC1->CR2 |= ADC_CR2_ADON;		// switch on ADC
-
-	// enable Update Interrupt
-		enableInterruptVector(true, 15);
-		enableInterrupt();
-
-
-	ADC1->CR1 |= ADC_CR1_SCAN;		// set to scan mode
-	ADC1->CR2 |= ADC_CR2_CONT;		// set to continuous mode
-
-	// configure sequence
-	ADC1->SQR1 &= 0;		// clear number of conversions in the sequence
-	ADC1->SQR1 |= ((ADC_CHANNEL_NUM-1) << 20);		// set number of conversions to 1
-	//@TODO 	ADC1->SQR1 |= (3 << 20);		// set number of conversions to 1
-
-
-
-	ADC1->SQR3 |= adc_channel | (3 << 5);	// set the first conversion channel to adc_channel
-
-	ADC1->SMPR2 |= 0b111; // set high sample time
-
-	ADC1->CR2 |= ADC_CR2_EOCS; //!< Select, that an interrupt is called after each conversion and not only after the sequence
-
-
-
-	ADC1->CR2 |= ADC_CR2_SWSTART;	// starts single conversion for the regular group
-
-	// The regular channels and their order in the conversion sequence must be
-	// selected in the ADC_SQRx registers. The total number of conversions in
-	// the regular group must be written in the L[3:0] bits in the ADC_SQR1
-	// register.
+	Adc::initialize();
+	Adc::enableFreeRunningMode();
+	Adc::setChannel(PIN_A1);
+	Adc::startConversion();
 
 	while(1)
 	{
-		for(uint32_t t1=0; t1<16200000; t1+=2) asm volatile ("NOP"); // knowing, that there is probably a better function for this (@see http://xpcc.sourceforge.net/api/classxpcc_1_1stm32_1_1_general_purpose_timer.html)
-		//while( (ADC1->SR & ADC_SR_EOC)==0 ) { XPCC_LOG_INFO << "ADC Status is " << xpcc::hex << (uint16_t) ADC1->SR << xpcc::ascii << "| Control is: " << xpcc::hex << (uint16_t) ADC1->CR2 << xpcc::endl; } //wait for a conversion
-		XPCC_LOG_INFO << xpcc::ascii << "PINA1=" << ADC_buffer[0] << " PINA3=" << ADC_buffer[1] << xpcc::endl; // send the value
-		//XPCC_LOG_INFO << "The status registers are ADC1_SR_EOC=" << xpcc::hex << ADC1->SR & ADC_SR_EOC << xpcc::ascii << " and ADC_CCR_EOC1=" << xpcc::hex << ADC->CCR & ADC_CCR_EOC1 << xpcc::endl;
+		for(uint32_t t1=0; t1<16200000; t1+=2) asm volatile ("NOP");
+		XPCC_LOG_INFO << xpcc::ascii << "adc=" << Adc::getValue() << xpcc::endl; // send the value
 	}
 
-	// Shut down ADC
-	ADC1->CR2 &= ~(ADC_CR2_ADON);				// switch off ADC
+	Adc::shutdownADC();
 
 	while (1)
 	{
+		asm volatile("NOP");
 	}
 }
