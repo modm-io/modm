15d9dd50c323d287bca71bc4e223bf2769ad2302
strongly-typed <strongly-typed@nan>
2012-08-12 00:27:40 +0200

    Driver for a display made of several seven-segment digits connected to daisy-chained shift registers.

---
+++ src/xpcc/driver/ui/seven_segment_impl.hpp
@@ -0,0 +1,52 @@
+// ----------------------------------------------------------------------------
+
+#ifndef XPCC__SEVEN_SEGMENT_DISPLAY_HPP
+#	error "Don't include this file directly, use 'seven_segment.hpp' instead!"
+#endif
+
+// ----------------------------------------------------------------------------
+
+template<typename Spi, typename Load, uint8_t DIGITS>
+void
+xpcc::sevenSegment::SevenSegmentDisplay<Spi, Load, DIGITS>::initialize()
+{
+
+}
+
+template<typename Spi, typename Load, uint8_t DIGITS>
+void
+xpcc::sevenSegment::SevenSegmentDisplay<Spi, Load, DIGITS>::write(uint32_t val)
+{
+	for (uint_fast8_t ii = 0; ii < DIGITS; ++ii)
+	{
+		Spi::write(segmentLutHex[val % 10]);
+		val /= 10;
+	}
+
+	// wait until all data is written to the segments
+	while(Spi::isBusy());
+
+	// on rising edge data is moved to storage registers
+	Load::reset();
+	Load::set();
+
+}
+
+template<typename Spi, typename Load, uint8_t DIGITS>
+void
+xpcc::sevenSegment::SevenSegmentDisplay<Spi, Load, DIGITS>::writeHex(uint32_t val)
+{
+	for (uint_fast8_t ii = 0; ii < DIGITS; ++ii)
+	{
+		Spi::write(segmentLutHex[val & 0x0f]);
+		val >>= 4;
+	}
+
+	// wait until all data is written to the segments
+	while(Spi::isBusy());
+
+	// on rising edge data is moved to storage registers
+	Load::reset();
+	Load::set();
+}
+
