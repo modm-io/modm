92cb1735a27c981a4d024c4c3a06853ba4551533
strongly-typed <strongly-typed@nan>
2012-08-12 21:21:41 +0200

    Initial commit with communcation examples from AVR.

---
+++ examples/lpcxpresso/lpc11c24/communication/receiver/main.cpp
@@ -0,0 +1,98 @@
+
+#include <xpcc/architecture.hpp>
+#include <xpcc/communication.hpp>
+#include <xpcc/communication/backend/can/can_connector.hpp>
+#include <xpcc/debug/logger.hpp>
+
+#include <xpcc/driver/connectivity/can/mcp2515.hpp>
+#include <xpcc/driver/connectivity/spi/software_spi.hpp>
+
+// set new log level
+#undef XPCC_LOG_LEVEL
+#define	XPCC_LOG_LEVEL xpcc::log::DEBUG
+
+#include "component_receiver/receiver.hpp"
+
+#include "communication/postman.hpp"
+#include "communication/identifier.hpp"
+
+// ----------------------------------------------------------------------------
+// Logging
+
+xpcc::atmega::BufferedUart0 loggerUart(115200);
+xpcc::IODeviceWrapper<xpcc::atmega::BufferedUart0> loggerDevice(loggerUart);
+
+xpcc::log::Logger xpcc::log::debug(loggerDevice);
+xpcc::log::Logger xpcc::log::info(loggerDevice);
+xpcc::log::Logger xpcc::log::warning(loggerDevice);
+xpcc::log::Logger xpcc::log::error(loggerDevice);
+
+// ----------------------------------------------------------------------------
+// CAN communication
+
+GPIO__OUTPUT(Cs, B, 4);
+GPIO__INPUT(Int, B, 2);
+
+GPIO__OUTPUT(Sclk, B, 7);
+GPIO__OUTPUT(Mosi, B, 5);
+GPIO__INPUT(Miso, B, 6);
+
+typedef xpcc::SoftwareSpi<Sclk, Mosi, Miso> Spi;
+typedef xpcc::Mcp2515<Spi, Cs, Int> CanDevice;
+
+static CanDevice device;
+static xpcc::CanConnector< CanDevice > connector(&device);
+
+// create an instance of the generated postman
+Postman postman;
+
+xpcc::Dispatcher dispatcher(&connector, &postman);
+
+// Default filters to receive any extended CAN frame
+FLASH_STORAGE(uint8_t canFilter[]) =
+{
+	MCP2515_FILTER_EXTENDED(0),	// Filter 0
+	MCP2515_FILTER_EXTENDED(0),	// Filter 1
+
+	MCP2515_FILTER_EXTENDED(0),	// Filter 2
+	MCP2515_FILTER_EXTENDED(0),	// Filter 3
+	MCP2515_FILTER_EXTENDED(0),	// Filter 4
+	MCP2515_FILTER_EXTENDED(0),	// Filter 5
+
+	MCP2515_FILTER_EXTENDED(0),	// Mask 0
+	MCP2515_FILTER_EXTENDED(0),	// Mask 1
+};
+
+namespace component
+{
+	Receiver receiver(robot::component::RECEIVER, &dispatcher);
+}
+
+// ----------------------------------------------------------------------------
+MAIN_FUNCTION
+{
+	// Initialize SPI interface and the other pins
+	// needed by the MCP2515
+	Spi::initialize();
+	Cs::setOutput();
+	Int::setInput(xpcc::atmega::PULLUP);
+	
+	// Configure MCP2515 and set the filters
+	device.initialize(xpcc::can::BITRATE_125_KBPS);
+	device.setFilter(xpcc::accessor::asFlash(canFilter));
+	
+	// Enable Interrupts
+	sei();
+	
+	XPCC_LOG_INFO << "Welcome to the communication test!" << xpcc::endl; 
+	
+	while (1)
+	{
+		// deliver received messages
+		dispatcher.update();
+		
+		component::receiver.update();
+		
+		xpcc::delay_us(100);
+	}
+}
---
+++ examples/lpcxpresso/lpc11c24/communication/sender/component_sender/sender.cpp
@@ -0,0 +1,82 @@
+// coding: utf-8
+// ----------------------------------------------------------------------------
+/* Copyright (c) 2010, Roboterclub Aachen e.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Roboterclub Aachen e.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ROBOTERCLUB AACHEN E.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+// ----------------------------------------------------------------------------
+
+#include <xpcc/debug/logger.hpp>
+
+// set new log level
+#undef XPCC_LOG_LEVEL
+#define	XPCC_LOG_LEVEL xpcc::log::DEBUG
+
+#include "communication/identifier.hpp"
+#include "communication/packets.hpp"
+
+#include "sender.hpp"
+
+// ----------------------------------------------------------------------------
+component::Sender::Sender(uint8_t id, xpcc::Dispatcher *communication) :
+	xpcc::AbstractComponent(id, communication),
+	positionCallback(this, &Sender::getPositionCallback),
+	timer(2000)
+{
+}
+
+// ----------------------------------------------------------------------------
+void
+component::Sender::getPositionCallback(const xpcc::Header&,
+		const robot::packet::Position *parameter)
+{
+	XPCC_LOG_INFO << XPCC_FILE_INFO
+			<< "get position callback: x=" << parameter->x
+			<< ", y=" << parameter->y << xpcc::endl;
+}
+
+// ----------------------------------------------------------------------------
+void
+component::Sender::update()
+{
+	if (timer.isExpired())
+	{
+		XPCC_LOG_INFO << XPCC_FILE_INFO << "sender update" << xpcc::endl;
+		
+		robot::packet::Position position(10, 20);
+		
+		this->callAction(
+				robot::component::RECEIVER,
+				robot::action::SET_POSITION,
+				position);
+		
+		this->callAction(
+				robot::component::RECEIVER,
+				robot::action::GET_POSITION,
+				positionCallback);
+	}
+}
