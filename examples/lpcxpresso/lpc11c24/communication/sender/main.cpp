05c5834ac60f8d805b859283143ef790f6dfc274
strongly-typed <strongly-typed@nan>
2012-08-12 21:49:45 +0200

    AVR and MCP replaced by LPC11C24.

--- examples/lpcxpresso/lpc11c24/communication/sender/main.cpp
+++ examples/lpcxpresso/lpc11c24/communication/sender/main.cpp
@@ -4,9 +4,6 @@
 #include <xpcc/communication/backend/can/can_connector.hpp>
 #include <xpcc/debug/logger.hpp>
 
-#include <xpcc/driver/connectivity/can/mcp2515.hpp>
-#include <xpcc/driver/connectivity/spi/software_spi.hpp>
-
 // set new log level
 #undef XPCC_LOG_LEVEL
 #define	XPCC_LOG_LEVEL xpcc::log::DEBUG
@@ -19,8 +16,8 @@
 // ----------------------------------------------------------------------------
 // Logging
 
-xpcc::atmega::BufferedUart0 loggerUart(115200);
-xpcc::IODeviceWrapper<xpcc::atmega::BufferedUart0> loggerDevice(loggerUart);
+xpcc::lpc::Uart1 loggerUart(115200);
+xpcc::IODeviceWrapper<xpcc::lpc::Uart1> loggerDevice(loggerUart);
 
 xpcc::log::Logger xpcc::log::debug(loggerDevice);
 xpcc::log::Logger xpcc::log::info(loggerDevice);
@@ -30,15 +27,7 @@ xpcc::log::Logger xpcc::log::error(loggerDevice);
 // ----------------------------------------------------------------------------
 // CAN communication
 
-GPIO__OUTPUT(Cs, B, 4);
-GPIO__INPUT(Int, B, 2);
-
-GPIO__OUTPUT(Sclk, B, 7);
-GPIO__OUTPUT(Mosi, B, 5);
-GPIO__INPUT(Miso, B, 6);
-
-typedef xpcc::SoftwareSpi<Sclk, Mosi, Miso> Spi;
-typedef xpcc::Mcp2515<Spi, Cs, Int> CanDevice;
+typedef xpcc::lpc::Can CanDevice;
 
 static CanDevice device;
 static xpcc::CanConnector< CanDevice > connector(&device);
@@ -48,21 +37,6 @@ Postman postman;
 
 xpcc::Dispatcher dispatcher(&connector, &postman);
 
-// Default filters to receive any extended CAN frame
-FLASH_STORAGE(uint8_t canFilter[]) =
-{
-	MCP2515_FILTER_EXTENDED(0),	// Filter 0
-	MCP2515_FILTER_EXTENDED(0),	// Filter 1
-
-	MCP2515_FILTER_EXTENDED(0),	// Filter 2
-	MCP2515_FILTER_EXTENDED(0),	// Filter 3
-	MCP2515_FILTER_EXTENDED(0),	// Filter 4
-	MCP2515_FILTER_EXTENDED(0),	// Filter 5
-
-	MCP2515_FILTER_EXTENDED(0),	// Mask 0
-	MCP2515_FILTER_EXTENDED(0),	// Mask 1
-};
-
 namespace component
 {
 	Sender sender(robot::component::SENDER, &dispatcher);
@@ -71,18 +45,9 @@ namespace component
 // ----------------------------------------------------------------------------
 MAIN_FUNCTION
 {
-	// Initialize SPI interface and the other pins
-	// needed by the MCP2515
-	Spi::initialize();
-	Cs::setOutput();
-	Int::setInput(xpcc::atmega::PULLUP);
-	
-	// Configure MCP2515 and set the filters
+	// Configure Can device and set the filters
 	device.initialize(xpcc::can::BITRATE_125_KBPS);
-	device.setFilter(xpcc::accessor::asFlash(canFilter));
-	
-	// Enable Interrupts
-	sei();
+//	device.setFilter(xpcc::accessor::asFlash(canFilter));
 	
 	XPCC_LOG_INFO << "Welcome to the communication test!" << xpcc::endl; 
 	
