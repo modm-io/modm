8cfe61d86e78cbecb263fef6af12c21e96ee0a9b
strongly-typed <strongly-typed@nan>
2012-08-14 05:35:23 +0200

    Use scons program and scons init for all examples projects with LPC11C24.

--- examples/lpcxpresso/lpc11c24/communication/receiver/program.sh
+++
@@ -1,1 +0,0 @@
-/opt/lpcxpresso/lpcxpresso/bin/crt_emu_lpc11_13_nxp -wire=winusb -pLPC11C24/301 -flash-load-exec=../../../../../build/lpcxpresso/lpc11c24/communication_can_receiver/communication_can_receiver.bin  -load-base=0x0
