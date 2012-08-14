364331911f994681a7eb3398e7462daef43bbdec
strongly-typed <strongly-typed@nan>
2012-08-14 02:38:37 +0200

    Indiviual build directories for all examples.

--- examples/lpcxpresso/lpc11c24/communication/sender/program.sh
+++ examples/lpcxpresso/lpc11c24/communication/sender/program.sh
@@ -1,1 +1,1 @@
-/opt/lpcxpresso/lpcxpresso/bin/crt_emu_lpc11_13_nxp -wire=winusb -pLPC11C24/301 -flash-load-exec=../../../../../build/communication_can_sender/communication_can_sender.bin  -load-base=0x0
+/opt/lpcxpresso/lpcxpresso/bin/crt_emu_lpc11_13_nxp -wire=winusb -pLPC11C24/301 -flash-load-exec=../../../../../build/lpcxpresso/lpc11c24/communication_can_sender/communication_can_sender.bin  -load-base=0x0
