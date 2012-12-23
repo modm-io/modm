79f959efb460979fbc706c81a44224a642bb49d1
strongly-typed <strongly-typed@nan>
2012-12-23 23:11:28 +0100

    Copy blink example from STM32 F4 Discovery

---
+++ examples/stm32/f3/blink/main.cpp
@@ -0,0 +1,47 @@
+
+#include <xpcc/architecture.hpp>
+
+// ----------------------------------------------------------------------------
+GPIO__OUTPUT(LedOrange, D, 13);		// User LED 3
+GPIO__OUTPUT(LedGreen, D, 12);		// User LED 4
+GPIO__OUTPUT(LedRed, D, 14);		// User LED 5
+GPIO__OUTPUT(LedBlue, D, 15);		// User LED 6
+
+GPIO__OUTPUT(VBusPresent, A, 9);		// green LED (LD7)
+GPIO__OUTPUT(VBusOvercurrent, D, 5);	// red LED   (LD8)
+
+GPIO__INPUT(Button, A, 0);
+
+using namespace xpcc::stm32;
+
+static bool
+initClock()
+{
+	// use external 8MHz crystal
+	if (!Clock::enableHse(Clock::HseConfig::HSE_CRYSTAL)) {
+		return false;
+	}
+	
+	Clock::enablePll(Clock::PllSource::PLL_HSE, 4, 168);
+	return Clock::switchToPll();
+}
+
+// ----------------------------------------------------------------------------
+MAIN_FUNCTION
+{
+	initClock();
+
+	LedOrange::setOutput(xpcc::gpio::HIGH);
+	LedGreen::setOutput(xpcc::gpio::LOW);
+	LedRed::setOutput(xpcc::gpio::HIGH);
+	LedBlue::setOutput(xpcc::gpio::HIGH);
+	
+	while (1)
+	{
+		LedBlue::toggle();
+		LedGreen::toggle();
+		xpcc::delay_ms(500);
+	}
+
+	return 0;
+}
