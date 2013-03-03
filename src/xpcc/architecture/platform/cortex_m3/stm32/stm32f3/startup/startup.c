244bb2b7f0bef4f948ec54786c5bf813b3f7ebd8
strongly-typed <strongly-typed@nan>
2013-03-03 19:17:40 +0100

    Addresses added in startup code and verified.

--- src/xpcc/architecture/platform/cortex_m3/stm32/stm32f3/startup/startup.c
+++ src/xpcc/architecture/platform/cortex_m3/stm32/stm32f3/startup/startup.c
@@ -33,6 +33,8 @@
 
 #if defined(STM32F30X)
 #	include <stm32f30x.h>
+#else
+	#error "This file is not supposed to be used with given CPU (only STM32F3xx)"
 #endif
 
 #include "xpcc_config.hpp"
@@ -82,7 +84,6 @@ void DMA1_CH3_IRQHandler(void)		__attribute__ ((weak, alias("defaultHandler")));
 void DMA1_CH4_IRQHandler(void)		__attribute__ ((weak, alias("defaultHandler")));
 void DMA1_CH5_IRQHandler(void)		__attribute__ ((weak, alias("defaultHandler")));
 void DMA1_CH6_IRQHandler(void)		__attribute__ ((weak, alias("defaultHandler")));
-void DMA1_CH7_IRQHandler(void)		__attribute__ ((weak, alias("defaultHandler")));
 void ADC1_2_IRQHandler(void)		__attribute__ ((weak, alias("defaultHandler")));
 void CAN1_TX_IRQHandler(void)		__attribute__ ((weak, alias("defaultHandler")));
 void CAN1_RX0_IRQHandler(void)		__attribute__ ((weak, alias("defaultHandler")));
@@ -155,111 +156,111 @@ FunctionPointer flashVectors[] __attribute__ ((section(".reset"))) =
 #endif
 {
 	(FunctionPointer) &__stack_end,	// stack pointer
-	Reset_Handler,				// code entry point
-	NMI_Handler,				// NMI handler
-	HardFault_Handler,			// hard fault handler
-	MemManage_Handler,			// MPU Fault Handler
-	BusFault_Handler,			// Bus Fault Handler
-	UsageFault_Handler,			// Usage Fault Handler
-	0,
-	0,
-	0,
-	0,
-	SVC_Handler,				// SVCall Handler
-	0,
-	0,
-	PendSV_Handler,				// PendSV Handler
-	SysTick_Handler,			// SysTick Handler
+	Reset_Handler,				// 0000 0004 code entry point
+	NMI_Handler,				// 0000 0008 NMI handler
+	HardFault_Handler,			// 0000 000C hard fault handler
+	MemManage_Handler,			// 0000 0010 MPU Fault Handler
+	BusFault_Handler,			// 0000 0014 Bus Fault Handler
+	UsageFault_Handler,			// 0000 0018 Usage Fault Handler
+	0,							// 0000 001C
+	0,							// 0000 0020
+	0,							// 0000 0024
+	0,							// 0000 0028
+	SVC_Handler,				// 0000 002C SVCall Handler
+	0,							// 0000 0030
+	0,							// 0000 0034
+	PendSV_Handler,				// 0000 0038 PendSV Handler
+	SysTick_Handler,			// 0000 003C SysTick Handler
 	
 	// Peripheral interrupts (STM32 specific)
-	WWDG_IRQHandler,			//  0: Window Watchdog
-	PVD_IRQHandler,				//  1: PVD through EXTI Line detect
-	TAMP_STAMP_IRQHandler,		//  2: Tamper and TimeStamps through the EXTI line
-	RTC_WKUP_IRQHandler,		//  3: Wakeup through the EXTI line
-	FLASH_IRQHandler,			//  4: Flash
-	RCC_IRQHandler,				//  5: RCC
-	EXTI0_IRQHandler,			//     EXTI Line 0
-	EXTI1_IRQHandler,			//     EXTI Line 1
-	EXTI2_IRQHandler,			//     EXTI Line 2
-	EXTI3_IRQHandler,			//     EXTI Line 3
-	EXTI4_IRQHandler,			// 10: EXTI Line 4
-	DMA1_CH1_IRQHandler,		//     DMA1 Channel 1
-	DMA1_CH1_IRQHandler,		//     DMA1 Channel 2
-	DMA1_CH2_IRQHandler,		//     DMA1 Channel 3
-	DMA1_CH3_IRQHandler,		//     DMA1 Channel 4
-	DMA1_CH4_IRQHandler,		// 15: DMA1 Channel 5
-	DMA1_CH5_IRQHandler,		//     DMA1 Channel 6
-	DMA1_CH6_IRQHandler,		//     DMA1 Channel 7
-	ADC1_2_IRQHandler,			//     ADC1 and ADC2
-	CAN1_TX_IRQHandler,  		//     CAN1 TX
-	CAN1_RX0_IRQHandler, 		// 20: CAN1 RX0
-	CAN1_RX1_IRQHandler,		//     CAN1 RX1
-	CAN1_SCE_IRQHandler,		//     CAN1 SCE
-	EXTI9_5_IRQHandler,			//     EXTI Line 9..5
-	TIM1_BRK_TIM15_IRQHandler,	//     TIM1 Break
-	TIM1_UP_TIM16_IRQHandler,	// 25: TIM1 Update
-	TIM1_TRG_COM_TIM17_IRQHandler,	// TIM1 Trigger and Commutation
-	TIM1_CC_IRQHandler,			//     TIM1 Capture Compare
-	TIM2_IRQHandler,			//     TIM2
-	TIM3_IRQHandler,			//     TIM3
-	TIM4_IRQHandler,			// 30: TIM4
-	I2C1_EV_EXTI23_IRQHandler,	//     I2C1 Event and EXTI Line23
-	I2C1_ER_IRQHandler,			//     I2C1 Error
-	I2C2_EV_EXTI24_IRQHandler,	//     I2C2 Event and EXTI Line24
-	I2C2_ER_IRQHandler,			//     I2C2 Error
-	SPI1_IRQHandler,			// 35: SPI1
-	SPI2_IRQHandler,			//     SPI2
-	USART1_IRQHandler,			//     USART1
-	USART2_IRQHandler,			//     USART2
-	USART3_IRQHandler,			//     USART3
-	EXTI15_10_IRQHandler,		// 40: External Line[15:10]s
-	RTCAlarm_IRQHandler,		//     RTC Alarm (A and B) through EXTI Line
-	USB_WKUP_IRQHandler,		//     USB Wakeup through EXTI line
-	TIM8_BRK_IRQHandler,		//     TIM8 Break
-	TIM8_UP_IRQHandler,			//     TIM8 Update and TIM13
-	TIM8_TRG_COM_IRQHandler,	// 45: TIM8 Trigger and Commutation
-	TIM8_CC_IRQHandler,			//     TIM8 Capture compare
-	ADC3_IRQHandler,			//     ADC3
-	0,
-	0,
-	0,							// 50:
-	SPI3_IRQHandler,
-	UART4_IRQHandler,
-	UART5_IRQHandler,
-	TIM6_DAC_IRQHandler,		//     TIM6 and DAC1&2 under-run errors
-	TIM7_IRQHandler,			// 55:
-	DMA2_CH1_IRQHandler,
-	DMA2_CH2_IRQHandler,
-	DMA2_CH3_IRQHandler,
-	DMA2_CH4_IRQHandler,
-	DMA2_CH5_IRQHandler,		// 60:
-	ADC4_IRQHandler,			//
-	0,
-	0,
-	COMP123_IRQHandler,
-	COMP456_IRQHandler,			// 65:
-	COMP7_IRQHandler,
-	0,
-	0,							//
-	0,							//
-	0,							// 70:
-	0,							//
-	0,							//
-	0,							//
-	USB_HP_IRQHandler,			//     USB High priority
-	USB_LP_IRQHandler,			// 75: USB Low prioroty
-	USB_WKUP_IRQHandler,		//     USB Wakeup through EXTI
-	0,							//
-	0,							//
-	0,							//
-	0,							// 80:
-	FPU_IRQHandler,				//     FPU
+	WWDG_IRQHandler,			// 0000 0040: Window Watchdog
+	PVD_IRQHandler,				// 0000 0044 1: PVD through EXTI Line detect
+	TAMP_STAMP_IRQHandler,		// 0000 0048 2: Tamper and TimeStamps through the EXTI line
+	RTC_WKUP_IRQHandler,		// 0000 004C 3: Wakeup through the EXTI line
+	FLASH_IRQHandler,			// 0000 0050 4: Flash
+	RCC_IRQHandler,				// 0000 0054 5: RCC
+	EXTI0_IRQHandler,			// 0000 0058    EXTI Line 0
+	EXTI1_IRQHandler,			// 0000 005C    EXTI Line 1
+	EXTI2_IRQHandler,			// 0000 0060    EXTI Line 2
+	EXTI3_IRQHandler,			// 0000 0064    EXTI Line 3
+	EXTI4_IRQHandler,			// 0000 0068	10: EXTI Line 4
+	DMA1_CH1_IRQHandler,		// 0000 006C    	DMA1 Channel 1
+	DMA1_CH1_IRQHandler,		// 0000 0070    	DMA1 Channel 2
+	DMA1_CH2_IRQHandler,		// 0000 0074    	DMA1 Channel 3
+	DMA1_CH3_IRQHandler,		// 0000 0078    	DMA1 Channel 4
+	DMA1_CH4_IRQHandler,		// 0000 007C	15: DMA1 Channel 5
+	DMA1_CH5_IRQHandler,		// 0000 0080    	DMA1 Channel 6
+	DMA1_CH6_IRQHandler,		// 0000 0084    	DMA1 Channel 7
+	ADC1_2_IRQHandler,			// 0000 0088    	ADC1 and ADC2
+	CAN1_TX_IRQHandler,  		// 0000 008C    	CAN1 TX
+	CAN1_RX0_IRQHandler, 		// 0000 0090	20: CAN1 RX0
+	CAN1_RX1_IRQHandler,		// 0000 0094    	CAN1 RX1
+	CAN1_SCE_IRQHandler,		// 0000 0098    	CAN1 SCE
+	EXTI9_5_IRQHandler,			// 0000 009C    	EXTI Line 9..5
+	TIM1_BRK_TIM15_IRQHandler,	// 0000 00A0    	TIM1 Break
+	TIM1_UP_TIM16_IRQHandler,	// 0000 00A4	25: TIM1 Update
+	TIM1_TRG_COM_TIM17_IRQHandler,	// 0000 00A8	TIM1 Trigger and Commutation
+	TIM1_CC_IRQHandler,			// 0000 00AC    	TIM1 Capture Compare
+	TIM2_IRQHandler,			// 0000 00B0    	TIM2
+	TIM3_IRQHandler,			// 0000 00B4    	TIM3
+	TIM4_IRQHandler,			// 0000 00B8	30: TIM4
+	I2C1_EV_EXTI23_IRQHandler,	// 0000 00BC	    I2C1 Event and EXTI Line23
+	I2C1_ER_IRQHandler,			// 0000 00C0	    I2C1 Error
+	I2C2_EV_EXTI24_IRQHandler,	// 0000 00C4	    I2C2 Event and EXTI Line24
+	I2C2_ER_IRQHandler,			// 0000 00C8	    I2C2 Error
+	SPI1_IRQHandler,			// 0000 00CC	35: SPI1
+	SPI2_IRQHandler,			// 0000 00D0	    SPI2
+	USART1_IRQHandler,			// 0000 00D4	    USART1
+	USART2_IRQHandler,			// 0000 00D8	    USART2
+	USART3_IRQHandler,			// 0000 00DC	    USART3
+	EXTI15_10_IRQHandler,		// 0000 00E0	40: External Line[15:10]s
+	RTCAlarm_IRQHandler,		// 0000 00E4	    RTC Alarm (A and B) through EXTI Line
+	USB_WKUP_IRQHandler,		// 0000 00E8	    USB Wakeup through EXTI line
+	TIM8_BRK_IRQHandler,		// 0000 00EC	    TIM8 Break
+	TIM8_UP_IRQHandler,			// 0000 00F0	    TIM8 Update and TIM13
+	TIM8_TRG_COM_IRQHandler,	// 0000 00F4	45: TIM8 Trigger and Commutation
+	TIM8_CC_IRQHandler,			// 0000 00F8	    TIM8 Capture compare
+	ADC3_IRQHandler,			// 0000 00FC	    ADC3
+	0,							// 0000 0100
+	0,							// 0000 0104
+	0,							// 0000 0108	50:
+	SPI3_IRQHandler,			// 0000 010C
+	UART4_IRQHandler,			// 0000 0110
+	UART5_IRQHandler,			// 0000 0114
+	TIM6_DAC_IRQHandler,		// 0000 0118    	TIM6 and DAC1&2 under-run errors
+	TIM7_IRQHandler,			// 0000 011C	55:
+	DMA2_CH1_IRQHandler,		// 0000 0120
+	DMA2_CH2_IRQHandler,		// 0000 0124
+	DMA2_CH3_IRQHandler,		// 0000 0128
+	DMA2_CH4_IRQHandler,		// 0000 012C
+	DMA2_CH5_IRQHandler,		// 0000 0130	60:
+	ADC4_IRQHandler,			// 0000 0134
+	0,							// 0000 0138
+	0,							// 0000 013C
+	COMP123_IRQHandler,			// 0000 0140
+	COMP456_IRQHandler,			// 0000 0144	65:
+	COMP7_IRQHandler,			// 0000 0148
+	0,							// 0000 014C
+	0,							// 0000 0150
+	0,							// 0000 0154
+	0,							// 0000 0158	70:
+	0,							// 0000 015C
+	0,							// 0000 0160
+	0,							// 0000 0164
+	USB_HP_IRQHandler,			// 0000 0168	     USB High priority
+	USB_LP_IRQHandler,			// 0000 016C	 75: USB Low prioroty
+	USB_WKUP_IRQHandler,		// 0000 0170	     USB Wakeup through EXTI
+	0,							// 0000 0174
+	0,							// 0000 0178
+	0,							// 0000 017C
+	0,							// 0000 0180	 80:
+	FPU_IRQHandler,				// 0000 0184	     FPU
 };
 
 // FIXME set USB_IT_RMP bit in the Section 9.1.1: SYSCFG configuration register 1 (SYSCFG_CFGR1)
 
 // ----------------------------------------------------------------------------
-// defined in the linkerscript
+// defined in the linker script
 extern uint32_t __fastcode_load;
 extern uint32_t __fastcode_start;
 extern uint32_t __fastcode_end;
@@ -314,12 +315,8 @@ Reset_Handler(void)
 		*(dest++) = 0;
 	}
 	
-#if defined(STM32F3XX)
 	// prepare flash latency for working at 72MHz and supply voltage > 2.7
 	FLASH->ACR = (FLASH->ACR & ~FLASH_ACR_LATENCY) | FLASH_ACR_LATENCY_1;
-#else
-	#error "This file is not supposed to be used with given CPU (only STM32F3xx)"
-#endif
 	
 	// enable flash prefetch
 	FLASH->ACR |= FLASH_ACR_PRFTBE;
