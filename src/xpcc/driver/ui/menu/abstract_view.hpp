b8090a24a692910cc6f8316b9dcf0156b6c0e84b
thorsten <thorsten.lajewski@rwth-aachen.de>
2013-05-18 17:48:35 +0200

    added documentation to AbstractView class

--- src/xpcc/driver/ui/menu/abstract_view.hpp
+++ src/xpcc/driver/ui/menu/abstract_view.hpp
@@ -31,7 +31,7 @@
 #ifndef XPCC__ABSTRACT_VIEW_HPP
 #define XPCC__ABSTRACT_VIEW_HPP
 
-#include <xpcc/driver/ui/display/graphic_display.hpp>
+#include "../display/graphic_display.hpp"
 
 #include "menu_buttons.hpp"
 
@@ -40,41 +40,85 @@ namespace xpcc
 	// forward declaration
 	class ViewStack;
 	
+	/**
+	 * @brief The AbstractView class is the base class for all screens
+	 *        handled by the ViewStack class
+	 *
+	 *\author Thorsten Lajewski
+	 */
+
 	class AbstractView
 	{
 		friend class ViewStack;
 		
 	public:
+		/**
+		 * @param stack pointer to the stack, the screen should be displayed on.
+		 * @param identifier can be used to determine which screen is the currently
+		 *        displayed on the graphicDisplay
+		 */
 		AbstractView(xpcc::ViewStack* stack, uint8_t identifier);
 		
 		virtual ~AbstractView() = 0;
 
+		/**
+		 * @brief update The update function of the top most display gets called
+		 *        as often as possible. Only the update of the top view in each
+		 *        ViewStack gets called.
+		 */
 		virtual void
 		update();
 
+		/**
+		 * @brief hasChanged indicates the current displayed view has changed.
+		 *        This function prevents unnecessary drawing of the display
+		 * @return if true the display has to be redrawn.
+		 */
 		virtual bool
 		hasChanged() = 0;
 		
+		/**
+		 * @brief draw determine the output on the Graphic Display
+		 */
 		virtual void
 		draw() = 0;
 		
+
+		/**
+		 * @brief shortButtonPress handle the action for the pressed button
+		 */
 		virtual void 
 		shortButtonPress(xpcc::MenuButtons::Button button);
 
+		/**
+		 * @brief isAlive tells the ViewStack if it should remove this screen.
+		 * @return
+		 */
 		bool
 		isAlive() const;
 
+		/**
+		 * @brief remove the view from the screen. The viewStack handles the deletion.
+		 */
 		void
 		remove();
 
+		/**
+		 * @brief getIdentifier of the view.
+		 */
 		inline uint8_t getIdentifier(){
 			return this->identifier;
 		}
 
 	protected:
+
 		xpcc::GraphicDisplay&
 		display();
 		
+		/**
+		 * @brief onRemove will be called right before the view gets deleted,
+		 *        can be reimplemented to reset external data.
+		 */
 		virtual void
 		onRemove();
 		
