f6b05186ba93b38bdf6052742d1d1c5012a27095
Niklas Hauser <niklas.hauser@gmail.com>
2013-06-21 19:50:36 +0200

    Peripherals: Add Uart to ATmega.

--- src/xpcc/architecture/platform/peripheral/uart/atmega/uart_defines.h
+++ src/xpcc/architecture/platform/peripheral/uart/atmega/uart_defines.h
@@ -1,11 +1,11 @@
 // coding: utf-8
 // ----------------------------------------------------------------------------
-/* Copyright (c) 2009, Roboterclub Aachen e.V.
+/* Copyright (c) 2011, Roboterclub Aachen e.V.
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -14,7 +14,7 @@
  *     * Neither the name of the Roboterclub Aachen e.V. nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -28,17 +28,53 @@
  */
 // ----------------------------------------------------------------------------
 
-#ifndef XPCC__DEVICE_HPP
-#define XPCC__DEVICE_HPP
+#ifndef	UART_DEFINES_H
+#define	UART_DEFINES_H
 
-#include <stdint.h>
-// Defines for example the ALWAYS_INLINE macro
-#include <xpcc/architecture/utils.hpp>
+// Atmel made a mess with the register names for UART0 between the "old" devices.
+// To avoid to much conditional compiling we try to mimic the new names for
+// the old devices.
+#if defined (UDR) && defined (UDR0)
+#undef UDR0
+#endif
 
-// Include external device headers
-{% for header in headers -%}
-#include <{{header}}>
-{% endfor -%}
+#if defined (UDR)
+#	define UDR0		UDR
+#	define UBRR0L	UBRRL
+#	define UBRR0H	UBRRH
+#	define UCSR0A	UCSRA
+#	define UCSR0B	UCSRB
+#	define UCSR0C	UCSRC
+#	define DOR0		DOR
+#	define FE0		FE
+#	define RXCIE0	RXCIE
+#	define TXCIE0	TXCIE
+#	define RXEN0	RXEN
+#	define TXEN0	TXEN
+#	define U2X0		U2X
+#	define URSEL0	URSEL
+#	define UCSZ00	UCSZ0
+#	define UCSZ01	UCSZ1
+#	define UDRIE0	UDRIE
+#endif
 
-#endif	// XPCC__DEVICE_HPP
+// We want the interrupt names to be "USART0_RX_vect", "USART0_UDRE_vect" and
+// "USART0_TX_vect".
+#if defined(USART0_RX_vect)
+// everything is fine
+#elif defined(USART_RX_vect)
+#	define USART0_RX_vect		USART_RX_vect
+#	define USART0_UDRE_vect		USART_UDRE_vect
+#	define USART0_TX_vect		USART_TX_vect
+#elif defined(USARTRXC_vect)
+#	define USART0_RX_vect		USARTRXC_vect
+#	define USART0_UDRE_vect		USARTUDRE_vect
+#	define USART0_TX_vect		USARTTXC_vect
+#elif defined(USART_RXC_vect)
+#	define USART0_RX_vect		USART_RXC_vect
+#	define USART0_UDRE_vect		USART_UDRE_vect
+#	define USART0_TX_vect		USART_TXC_vect
+#endif
+
+#endif	// UART_DEFINES_H
 
