00c8e11926bf7274749f0f3f17fda31127451ea7
Electronic Kiwi <electron.kiwi@gmail.com>
2013-07-10 18:27:09 +0200

    Stm32F4D: Blink: run from external 8MHz Clock.

--- examples/stm32f4_discovery/blink/main.cpp
+++ examples/stm32f4_discovery/blink/main.cpp
@@ -11,6 +11,8 @@ GPIO__OUTPUT(VBusPresent, A, 9);		// green LED (LD7)
 GPIO__OUTPUT(VBusOvercurrent, D, 5);	// red LED   (LD8)
 
 GPIO__INPUT(Button, A, 0);
+GPIO__OUTPUT(ClockOut, A, 8);
+GPIO__OUTPUT(SystemClockOut, C, 9);
 
 using namespace xpcc::stm32;
 
@@ -18,12 +20,22 @@ using namespace xpcc::stm32;
 MAIN_FUNCTION
 {
 	// New Static Clock Setup (S:: => Static)
-	typedef S::Pll<S::ExternalOscillator<MHz8>, MHz168, MHz48> clockSource;
+	//typedef S::Pll<S::ExternalOscillator<MHz8>, MHz168, MHz48> clockSource;
+	// typedef S::Pll<S::InternalClock<MHz16>, MHz168, MHz48> clockSource;
+	typedef S::ExternalOscillator<MHz8> clockSource;
+	// typedef S::InternalClock<MHz16> clockSource;
 	StartupError err =
 		S::SystemClock<clockSource>::enable();
 
 	// Output clock source on PA8
+	ClockOut::setAlternateFunction(Gpio::AF_0, Gpio::PUSH_PULL);
+	S::MCO1::setDivision(S::MCO1::Division::By1);
 	S::MCO1::connect(clockSource::Id);
+
+	// Output SystemClock on PC9
+	SystemClockOut::setAlternateFunction(Gpio::AF_0, Gpio::PUSH_PULL);
+	S::MCO2::setDivision(S::MCO2::Division::By1);
+	S::MCO2::connect(S::SystemClock<clockSource>::Id);
 
 	LedOrange::setOutput(xpcc::Gpio::HIGH);
 	LedGreen::setOutput(xpcc::Gpio::LOW);
