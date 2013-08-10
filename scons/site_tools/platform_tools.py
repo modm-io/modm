9058deb49efa80f2b0e8e60d705890f6c34c0071
Electronic Kiwi <electron.kiwi@gmail.com>
2013-08-10 15:26:07 +0200

    SCons: PlatformTools: remove platform.hpp before every build

--- scons/site_tools/platform_tools.py
+++ scons/site_tools/platform_tools.py
@@ -174,6 +174,9 @@ def platform_tools_generate(env, architecture_path):
 		driver_list.append(ddic)
 
 	####### Generate Header Templates #########################################
+	# Remove architecture/platform.hpp file
+	# if it is not removed an old platform file from a different build might be used
+	os.remove(os.path.join(architecture_path, 'platform.hpp'))
 	# Show SCons how to build the architecture/platform.hpp file:
 	src = os.path.join(platform_path, 'platform.hpp.in')
 	tar = os.path.join(architecture_path, 'platform.hpp')
