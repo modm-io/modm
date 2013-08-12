5df67bf9ad808a3f2223a6d802ea02481d23b00c
Electronic Kiwi <electron.kiwi@gmail.com>
2013-08-12 20:44:02 +0200

    Examples: STM32F3D: uart/hal example now works with revised code

---
+++ examples/stm32f3_discovery/uart/hal/main.cpp
@@ -0,0 +1,40 @@
+
+#include <xpcc/architecture.hpp>
+
+// ----------------------------------------------------------------------------
+using namespace xpcc::stm32;
+
+// Usart 3 Pins
+typedef GpioOutputA2 UART2_TX;
+typedef GpioInputA3 UART2_RX;
+
+// ----------------------------------------------------------------------------
+MAIN_FUNCTION
+{
+	StartupError err =
+		SystemClock<Pll<ExternalOscillator<MHz8>, MHz72>>::enable();
+
+	// Initialize Usart Hal
+	UART2_TX::connect(UsartHal2::Id);
+	UART2_RX::connect(UsartHal2::Id);
+	UsartHal2::initialize(115200, UsartHal2::Parity::Odd);
+	UsartHal2::enableInterruptVector(true, 14);
+	UsartHal2::enableInterrupt(UsartHal2::Interrupt::TxEmpty);
+	UsartHal2::setTransmitterEnable(true);
+
+	while (1)
+	{
+	}
+
+	return 0;
+}
+
+// Interrupt Handler
+extern "C" void
+USART2_IRQHandler(void)
+{
+	UsartHal2::getInterruptFlags();
+	if(UsartHal2::isTransmitRegisterEmpty()) {
+		UsartHal2::write('A');
+	}
+}
