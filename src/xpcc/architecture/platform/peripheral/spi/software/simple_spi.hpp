93f35d2a7e39f23a096bc05dd92a159761e124d8
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2013-08-12 20:51:01 +0200

    Peripheral: Simplify SimpleSpi.

--- src/xpcc/architecture/platform/peripheral/spi/software/simple_spi.hpp
+++ src/xpcc/architecture/platform/peripheral/spi/software/simple_spi.hpp
@@ -1,35 +1,14 @@
 // coding: utf-8
-// ----------------------------------------------------------------------------
-/* Copyright (c) 2009, Roboterclub Aachen e.V.
- * All rights reserved.
+/* Copyright (c) 2013, Roboterclub Aachen e.V.
+ * All Rights Reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Roboterclub Aachen e.V. nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL ROBOTERCLUB AACHEN E.V. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
  */
 // ----------------------------------------------------------------------------
 
-#ifndef XPCC__SOFTWARE_SPI_HPP
-#define XPCC__SOFTWARE_SPI_HPP
+#ifndef XPCC_SOFTWARE_SIMPLE_SPI_HPP
+#define XPCC_SOFTWARE_SIMPLE_SPI_HPP
 
 #include <stdint.h>
 #include <xpcc/architecture/peripheral/spi.hpp>
@@ -37,83 +16,58 @@
 
 namespace xpcc
 {
-	/**
-	 * \brief	Software emulation of a SPI Master
-	 * 
-	 * \tparam	Clk			clock pin [output]
-	 * \tparam	Mosi		master out slave in pin [output]
-	 * \tparam	Miso		master in slave out pin [input]
-	 * \tparam	Frequency	requested SPI frequency in Hz (default = 2 MHz)
-	 * 
-	 * \ingroup	connectivity
-	 * \see		gpio
-	 */
-	template< typename Clk,
-			  typename Mosi,
-			  typename Miso,
-			  int32_t Frequency = 2000000UL >
-	class SoftwareSpiMaster : public ::xpcc::SpiMaster
-	{
-	public:
-		static void
-		initialize();
-		
-		static uint8_t
-		writeReadBlocking(uint8_t data);
 
-		static void
-		writeBlocking(uint8_t data);
+/**
+ * Software emulation of a Simple Spi
+ *
+ * @tparam	Clk			clock pin [output]
+ * @tparam	Mosi		master out slave in pin [output]
+ * @tparam	Miso		master in slave out pin [input]
+ * @tparam	Frequency	requested SPI frequency in Hz (default = 2 MHz)
+ *
+ * @ingroup	spi
+ * @see		gpio
+ */
+template< typename Clk,
+		  typename Mosi,
+		  typename Miso = GpioUnused,
+		  uint32_t Frequency = 2000000UL >
+class SoftwareSimpleSpi : public ::xpcc::SimpleSpi
+{
+public:
+	static void
+	initialize();
 
-		static bool
-		write(uint8_t data);
+	static uint8_t
+	writeReadBlocking(uint8_t data);
 
-		static uint8_t
-		getResult();
+	static ALWAYS_INLINE void
+	writeBlocking(uint8_t data);
 
-		static bool
-		isFinished();
-		
-	protected:
-		static ALWAYS_INLINE void
-		delay();
-		
-		// calculate the delay in microseconds needed to achieve the
-		// requested SPI frequency
-		static const uint32_t delayTime = (1000000.0 / Frequency) / 2.0;
-		
-		static Clk clk;
-		static Mosi mosi;
-		static Miso miso;
-		static bool finished;
-		static uint8_t result;
-	};
+	static ALWAYS_INLINE bool
+	write(uint8_t data);
 
-	template< typename Clk,
-			  typename Mosi,
-			  typename Miso,
-			  int32_t Frequency = 2000000UL >
-	class SoftwareSpiBlockMaster : public ::xpcc::SpiBlockMaster
-	{
-	public:
-		/**
-		 * \brief	Initialize SPI module
-		 *
-		 * This also sets the directions of the I/O pins.
-		 */
-		static ALWAYS_INLINE void
-		initialize();
+	static ALWAYS_INLINE uint8_t
+	getResult();
 
-		static inline bool
-		start(uint8_t * tx, uint8_t * rx, std::size_t length, BufferOptions options=BufferOptions::TxRxIncrement);
+	static inline bool
+	transfer(uint8_t *tx, uint8_t *rx, std::size_t length, BufferOptions options=BufferOptions::TxRxIncrement);
 
-		static ALWAYS_INLINE bool
-		isFinished();
+	static ALWAYS_INLINE bool
+	isFinished();
 
-	private:
-		static bool finished;
-	};
-}
+protected:
+	static ALWAYS_INLINE void
+	delay();
 
-#include "spi_master_impl.hpp"
+	static constexpr uint32_t delayTime = (1000000.0 / Frequency) / 2.0;
 
-#endif // XPCC__SOFTWARE_SPI_HPP
+	static bool finished;
+	static uint8_t result;
+};
+
+} // namespace xpcc
+
+#include "simple_spi_impl.hpp"
+
+#endif // XPCC_SOFTWARE_SIMPLE_SPI_HPP
--- src/xpcc/architecture/platform/peripheral/spi/software/simple_spi_impl.hpp
+++ src/xpcc/architecture/platform/peripheral/spi/software/simple_spi_impl.hpp
@@ -1,70 +1,37 @@
 // coding: utf-8
-// ----------------------------------------------------------------------------
-/* Copyright (c) 2009, Roboterclub Aachen e.V.
- * All rights reserved.
+/* Copyright (c) 2013, Roboterclub Aachen e.V.
+ * All Rights Reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Roboterclub Aachen e.V. nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL ROBOTERCLUB AACHEN E.V. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
  */
 // ----------------------------------------------------------------------------
 
-#ifndef XPCC__SOFTWARE_SPI_HPP
-	#error	"Don't include this file directly, use 'software_spi.hpp' instead!"
+#ifndef XPCC_SOFTWARE_SIMPLE_SPI_HPP
+#	error	"Don't include this file directly, use 'simple_spi.hpp' instead!"
 #endif
 
-// ----------------------------------------------------------------------------
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
-Clk xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::clk;
+template <typename Clk, typename Mosi, typename Miso, uint32_t Frequency>
+bool xpcc::SoftwareSimpleSpi<Clk, Mosi, Miso, Frequency>::finished;
 
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
-Mosi xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::mosi;
-
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
-Miso xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::miso;
-
-// ----------------------------------------------------------------------------
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
-bool xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::finished;
-
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
-uint8_t xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::result;
+template <typename Clk, typename Mosi, typename Miso, uint32_t Frequency>
+uint8_t xpcc::SoftwareSimpleSpi<Clk, Mosi, Miso, Frequency>::result;
 
 
 // ----------------------------------------------------------------------------
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
+template <typename Clk, typename Mosi, typename Miso, uint32_t Frequency>
 void
-xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::initialize()
+xpcc::SoftwareSimpleSpi<Clk, Mosi, Miso, Frequency>::initialize()
 {
-	clk.setOutput();
-	mosi.setOutput();
-	miso.setInput();
+	Clk::setOutput();
+	Mosi::setOutput();
+	Miso::setInput();
 	finished = true;
 }
 
-// ----------------------------------------------------------------------------
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
+template <typename Clk, typename Mosi, typename Miso, uint32_t Frequency>
 uint8_t
-xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::writeReadBlocking(uint8_t data)
+xpcc::SoftwareSimpleSpi<Clk, Mosi, Miso, Frequency>::writeReadBlocking(uint8_t data)
 {
 	if (!isFinished())
 		return 0;
@@ -72,87 +39,72 @@ xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::writeReadBlocking(uint8_t d
 
 	uint8_t input = 0;
 
-	clk.reset();
-	for (uint8_t i = 0; i < 8; ++i)
+	Clk::reset();
+	for (uint_fast8_t ii = 0; ii < 8; ++ii)
 	{
 		input <<= 1;
 		if (data & 0x80) {
-			mosi.set();
+			Mosi::set();
 		}
 		else {
-			mosi.reset();
+			Mosi::reset();
 		}
 		delay();
 
-		clk.set();
+		Clk::set();
 		delay();
 
-		if (miso.read()) {
+		if (Miso::read()) {
 			input |= 1;
 		}
 		data <<= 1;
 
-		clk.reset();
+		Clk::reset();
 	}
 	finished = true;
-	
+
 	return input;
 }
 
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
+template <typename Clk, typename Mosi, typename Miso, uint32_t Frequency>
 void
-xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::writeBlocking(uint8_t data)
+xpcc::SoftwareSimpleSpi<Clk, Mosi, Miso, Frequency>::writeBlocking(uint8_t data)
 {
 	writeReadBlocking(data);
 }
 
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
+template <typename Clk, typename Mosi, typename Miso, uint32_t Frequency>
 bool
-xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::write(uint8_t data)
+xpcc::SoftwareSimpleSpi<Clk, Mosi, Miso, Frequency>::write(uint8_t data)
 {
 	result = writeReadBlocking(data);
 	return true;
 }
 
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
+template <typename Clk, typename Mosi, typename Miso, uint32_t Frequency>
 uint8_t
-xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::getResult()
+xpcc::SoftwareSimpleSpi<Clk, Mosi, Miso, Frequency>::getResult()
 {
 	return result;
 }
 
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
+template <typename Clk, typename Mosi, typename Miso, uint32_t Frequency>
 bool
-xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::isFinished()
+xpcc::SoftwareSimpleSpi<Clk, Mosi, Miso, Frequency>::isFinished()
 {
 	return finished;
 }
 
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
+template <typename Clk, typename Mosi, typename Miso, uint32_t Frequency>
 void
-xpcc::SoftwareSpiMaster<Clk, Mosi, Miso, Frequency>::delay()
+xpcc::SoftwareSimpleSpi<Clk, Mosi, Miso, Frequency>::delay()
 {
 	xpcc::delay_us(delayTime);
 }
 
-
-// ============================================================================
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
-bool xpcc::SoftwareSpiBlockMaster<Clk, Mosi, Miso, Frequency>::finished;
-
-
-// ----------------------------------------------------------------------------
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
-void
-xpcc::SoftwareSpiBlockMaster<Clk, Mosi, Miso, Frequency>::initialize()
-{
-	SoftwareSpiMaster< Clk, Mosi, Miso, Frequency >::initialize();
-	finished = true;
-}
-
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
+template <typename Clk, typename Mosi, typename Miso, uint32_t Frequency>
 bool
-xpcc::SoftwareSpiBlockMaster<Clk, Mosi, Miso, Frequency>::start(uint8_t * tx, uint8_t * rx, std::size_t length, BufferOptions options)
+xpcc::SoftwareSimpleSpi<Clk, Mosi, Miso, Frequency>::transfer(uint8_t *tx, uint8_t *rx, std::size_t length, BufferOptions options)
 {
 	if (!isFinished())
 		return false;
@@ -165,18 +117,11 @@ xpcc::SoftwareSpiBlockMaster<Clk, Mosi, Miso, Frequency>::start(uint8_t * tx, ui
 	{
 		if (tx) tx_byte = tx[static_cast<uint8_t>(options) & 0b10 ? i : length-i-1];
 
-		rx_byte = SoftwareSpiMaster< Clk, Mosi, Miso, Frequency >::writeReadBlocking(tx_byte);
+		rx_byte = writeReadBlocking(tx_byte);
 
 		if (rx) rx[static_cast<uint8_t>(options) & 0b01 ? i : length-i-1] = rx_byte;
 	}
 
 	finished = true;
 	return true;
-}
-
-template <typename Clk, typename Mosi, typename Miso, int32_t Frequency>
-bool
-xpcc::SoftwareSpiBlockMaster<Clk, Mosi, Miso, Frequency>::isFinished()
-{
-	return finished;
 }
