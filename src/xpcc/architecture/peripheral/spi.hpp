d100310d84ddc19a02d0c8f108cb23d868fca7cd
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2013-08-12 20:48:32 +0200

    Peripheral: Add BufferOptions to Spi.

--- src/xpcc/architecture/peripheral/spi.hpp
+++ src/xpcc/architecture/peripheral/spi.hpp
@@ -1,34 +1,14 @@
 // coding: utf-8
-// ----------------------------------------------------------------------------
 /* Copyright (c) 2013, Roboterclub Aachen e.V.
- * All rights reserved.
+ * All Rights Reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Roboterclub Aachen e.V. nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL ROBOTERCLUB AACHEN E.V. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
  */
+// ----------------------------------------------------------------------------
 
-#ifndef XPCC_PERIPHERAL__SPI_HPP
-#define XPCC_PERIPHERAL__SPI_HPP
+#ifndef XPCC_PERIPHERAL_SPI_HPP
+#define XPCC_PERIPHERAL_SPI_HPP
 
 #include "../peripheral.hpp"
 
@@ -61,6 +41,17 @@ struct Spi
 		ErrorCondition,	///< A bus error occured and the bus was reset
 		SoftwareReset	///< The master is initializing itself
 	};
+
+    /// transfer options for the receive and transmit buffers
+    /// @see SpiBlockMaster
+    enum class
+    BufferOptions : uint8_t
+    {
+        TxRxIncrement = 0b11,           ///< Increments both Tx and Rx buffer
+        TxIncrementRxDecrement = 0b10,  ///< Increments Tx, but decrements Rx buffer
+        TxDecrementRxIncrement = 0b01,  ///< Decrements Tx, but increments Rx buffer
+        TxRxDecrement = 0b00,           ///< Decrements both Tx and Rx buffer
+    };
 
 	/// Operations after a transmission.
 	enum class
@@ -244,6 +235,6 @@ public:
 	detaching(DetachCause cause) = 0;
 };
 
-}
+} // namespace xpcc
 
-#endif // XPCC_PERIPHERAL__SPI_HPP
+#endif // XPCC_PERIPHERAL_SPI_HPP
