580fec8bf987cae25ae45baaee7f9c951b43b1ba
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2013-08-16 23:39:34 +0200

    Peripheral: Add Xmega buffered Uart implementation.

---
+++ src/xpcc/architecture/platform/peripheral/uart/xmega/type_ids.hpp
@@ -0,0 +1,50 @@
+// coding: utf-8
+/* Copyright (c) 2013, Roboterclub Aachen e.V.
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
+ */
+// ----------------------------------------------------------------------------
+
+#ifndef XPCC_XMEGA_UART_TYPE_IDS_HPP
+#define XPCC_XMEGA_UART_TYPE_IDS_HPP
+
+namespace xpcc
+{
+
+namespace xmega
+{
+
+namespace TypeId
+{
+	typedef struct{} UartC0Rxd;
+	typedef struct{} UartC0Txd;
+
+	typedef struct{} UartC1Rxd;
+	typedef struct{} UartC1Txd;
+
+	typedef struct{} UartD0Rxd;
+	typedef struct{} UartD0Txd;
+
+	typedef struct{} UartD1Rxd;
+	typedef struct{} UartD1Txd;
+
+	typedef struct{} UartE0Rxd;
+	typedef struct{} UartE0Txd;
+
+	typedef struct{} UartE1Rxd;
+	typedef struct{} UartE1Txd;
+
+	typedef struct{} UartF0Rxd;
+	typedef struct{} UartF0Txd;
+
+	typedef struct{} UartF1Rxd;
+	typedef struct{} UartF1Txd;
+}
+
+} // namespace xmega
+
+} // namespace xpcc
+
+#endif // XPCC_XMEGA_UART_TYPE_IDS_HPP
