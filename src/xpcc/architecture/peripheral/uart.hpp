85205b6611bceeaa02d78dcad512d1af11e02fa9
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2013-09-27 14:19:39 +0200

    Peripheral: Add most common Baudrates to Uart interface.

--- src/xpcc/architecture/peripheral/uart.hpp
+++ src/xpcc/architecture/peripheral/uart.hpp
@@ -30,6 +30,32 @@ namespace xpcc
  */
 class Uart : public ::xpcc::Peripheral
 {
+public:
+	/// Commonly used baudrates.
+	/// Most Serial-to-USB converters only support baudrates up to 115200 Baud
+	enum
+	Baudrate
+	{
+		    B300 =     300,
+		    B600 =     600,
+		   B1200 =    1200,
+		   B1800 =    1800,
+		   B2400 =    2400,
+		   B4800 =    4800,
+		   B9600 =    9600,
+		  B14400 =   14400,
+		  B19200 =   19200,
+		  B28800 =   28800,
+		  B38400 =   38400,
+		  B57600 =   57600,
+		  B76800 =   76800,
+		 B115200 =  115200,
+		 B230400 =  230400,
+		 B250000 =  250000,
+		 B500000 =  500000,
+		B1000000 = 1000000
+	};
+
 #ifdef __DOXYGEN__
 public:
 	/**
