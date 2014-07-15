e0d87f193586e88a934ffc1a746da972e20135fc
daniel-k <da.krebs@web.de>
2014-07-16 00:12:41 +0200

    nrf24: add commands and start adding functions

--- src/xpcc/driver/radio/nrf24/nrf24.hpp
+++ src/xpcc/driver/radio/nrf24/nrf24.hpp
@@ -8,17 +8,17 @@ namespace nrf24
 {
 
     /* Raw bits */
-    constexpr char Bit0 = (1 << 0);
-    constexpr char Bit1 = (1 << 1);
-    constexpr char Bit2 = (1 << 2);
-    constexpr char Bit3 = (1 << 3);
-    constexpr char Bit4 = (1 << 4);
-    constexpr char Bit5 = (1 << 5);
-    constexpr char Bit6 = (1 << 6);
-    constexpr char Bit7 = (1 << 7);
+    constexpr uint8_t Bit0 = (1 << 0);
+    constexpr uint8_t Bit1 = (1 << 1);
+    constexpr uint8_t Bit2 = (1 << 2);
+    constexpr uint8_t Bit3 = (1 << 3);
+    constexpr uint8_t Bit4 = (1 << 4);
+    constexpr uint8_t Bit5 = (1 << 5);
+    constexpr uint8_t Bit6 = (1 << 6);
+    constexpr uint8_t Bit7 = (1 << 7);
 
 
-    enum class Register : char
+    enum class Register : uint8_t
     {
         
         CONFIG = 0x00,          ///< Configuration Register
@@ -50,7 +50,7 @@ namespace nrf24
     };
 
     /* CONFIG register */
-    enum class Config : char
+    enum class Config : uint8_t
     {
         PRIM_RX      = Bit0,
         PWR_UP       = Bit1,
@@ -62,7 +62,7 @@ namespace nrf24
     };
 
     /* EN_AA register */
-    enum class EnAA : char
+    enum class EnAA : uint8_t
     {
         ENAA_P0 = Bit0,
         ENAA_P1 = Bit1,
@@ -73,7 +73,7 @@ namespace nrf24
     };
 
     /* EN_RXADDR register */
-    enum class EnRxAddr : char
+    enum class EnRxAddr : uint8_t
     {
         ERX_P0 = Bit0,
         ERX_P1 = Bit1,
@@ -84,26 +84,26 @@ namespace nrf24
     };
     
     /* SETUP_AW register */
-    enum class SetupAw : char
+    enum class SetupAw : uint8_t
     {
         AW = Bit0 | Bit1
     };
     
     /* SETUP_RETR register */
-    enum class SetupRetr : char
+    enum class SetupRetr : uint8_t
     {
         ARC = Bit0 | Bit1 | Bit2 | Bit3,
         ARD = Bit4 | Bit5 | Bit6 | Bit7
     };
     
     /* RF_CH register */
-    enum class RfCh : char
+    enum class RfCh : uint8_t
     {
         RF_CH = Bit0 | Bit1 | Bit2 | Bit3 | Bit4 | Bit5 | Bit6
     };
     
     /* RF_SETUP register */
-    enum class RfSetup : char
+    enum class RfSetup : uint8_t
     {
         RF_PWR       = Bit1 | Bit2,
         RF_DR_HIGH   = Bit3,
@@ -113,7 +113,7 @@ namespace nrf24
     };
     
     /* STATUS register */
-    enum class Status : char
+    enum class Status : uint8_t
     {
         TX_FULL  = Bit0,
         RX_P_NO  = Bit1 | Bit2 | Bit3,
@@ -123,56 +123,56 @@ namespace nrf24
     };
     
     /* OBSERVE_TX register */
-    enum class ObserveTx : char
+    enum class ObserveTx : uint8_t
     {
         ARC_CNT  = Bit0 | Bit1 | Bit2 | Bit3,
         PLOS_CNT = Bit4 | Bit5 | Bit6 | Bit7
     };
     
     /* RPD register*/
-    enum class Rpd : char
+    enum class Rpd : uint8_t
     {
         RPD = Bit0
     };
     
     /* RX_PW_P0 register */
-    enum class RxPwP0 : char
+    enum class RxPwP0 : uint8_t
     {
         RX_PW_P0 = Bit0 | Bit1 | Bit2 | Bit3 | Bit4 | Bit5
     };
     
     /* RX_PW_P1 register */
-    enum class RxPwP1 : char
+    enum class RxPwP1 : uint8_t
     {
         RX_PW_P1 = Bit0 | Bit1 | Bit2 | Bit3 | Bit4 | Bit5
     };
     
     /* RX_PW_P2 register */
-    enum class RxPwP2 : char
+    enum class RxPwP2 : uint8_t
     {
         RX_PW_P2 = Bit0 | Bit1 | Bit2 | Bit3 | Bit4 | Bit5
     };
     
     /* RX_PW_P3 register */
-    enum class RxPwP3 : char
+    enum class RxPwP3 : uint8_t
     {
         RX_PW_P3 = Bit0 | Bit1 | Bit2 | Bit3 | Bit4 | Bit5
     };
     
     /* RX_PW_P4 register */
-    enum class RxPwP4 : char
+    enum class RxPwP4 : uint8_t
     {
         RX_PW_P4 = Bit0 | Bit1 | Bit2 | Bit3 | Bit4 | Bit5
     };
     
     /* RX_PW_P5 register */
-    enum class RxPwP5 : char
+    enum class RxPwP5 : uint8_t
     {
         RX_PW_P5 = Bit0 | Bit1 | Bit2 | Bit3 | Bit4 | Bit5
     };
     
     /* FIFO_STATUS register */
-    enum class FifoStatus : char
+    enum class FifoStatus : uint8_t
     {
         RX_EMPTY = Bit0,
         RX_FULL  = Bit1,
@@ -182,7 +182,7 @@ namespace nrf24
     };
     
     /* DYNPD register*/
-    enum class DynPd : char
+    enum class DynPd : uint8_t
     {
         DPL_P0 = Bit0,
         DPL_P1 = Bit1,
@@ -193,21 +193,49 @@ namespace nrf24
     };
     
     /* FEATURE register */
-    enum class Feature : char
+    enum class Feature : uint8_t
     {
         EN_DYN_ACK   = Bit0,
         EN_ACK_PAY   = Bit1,
         EN_DPL       = Bit2
     };
+
+    enum class Command : uint8_t
+    {
+    	R_REGISTER 			= 0b00000000,	///< 1 -  5 data bytes | lower 5 bit = Register address
+    	W_REGISTER			= 0b00100000,	///< 1 -  5 data bytes | lower 5 bit = Register address
+    	R_RX_PAYLOAD 		= 0b01100001,	///< 1 - 32 data bytes
+    	W_TX_PAYLOAD 		= 0b10100000,	///< 1 - 32 data bytes
+    	FLUSH_TX 			= 0b11100001,	///<      0 data bytes
+    	FLUSH_RX 			= 0b11100010,	///<      0 data bytes
+    	REUSE_TX_PL 		= 0b11100011,	///<      0 data bytes
+    	R_RX_PL_WID 		= 0b01100000,	///<      1 data bytes
+    	W_ACK_PAYLOAD 		= 0b10101000,	///< 1 - 32 data bytes | lower 3 bit = Pipe # (000 to 101)
+    	W_TX_PAYLOAD_NOACK 	= 0b10110000,	///< 1 - 32 data bytes
+    	NOP 				= 0b11111111	///<      0 data bytes
+    };
 }
 
 
-template <typename SPI, typename CS, typename RS, typename Reset>
+template <typename Spi, typename Csn, typename Ce>
 class Nrf24
 {
 
+public:
 	static void
 	initialize();
+
+
+private:
+
+	static void
+	writeRegister(nrf24::Register, uint8_t& data, uint8_t length);
+
+	static uint8_t
+	writeCommand(nrf24::Command, uint8_t& data, uint8_t length);
+
+private:
+	static uint8_t status;
 
 };
 
