8f8b26b67c597491cd76bb91a350dde4a567f02b
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-10-06 18:14:40 +0200

    Add very simple OLED SSD1306 example.

---
+++ examples/stm32f4_discovery/oled_display/main.cpp
@@ -0,0 +1,50 @@
+#include <xpcc/architecture/platform.hpp>
+#include "../stm32f4_discovery.hpp"
+
+#include <xpcc/driver/display/ssd1306.hpp>
+
+/**
+ * Example to demonstrate a XPCC graphics display SSD1306.
+ *
+ * PB9	SDA
+ * PB8	SCL
+ *
+ * GND and +5V or +3V3 are connected to the display module.
+ */
+
+typedef GpioB9 Sda;
+typedef GpioB8 Scl;
+typedef I2cMaster1 MyI2cMaster;
+xpcc::Ssd1306<MyI2cMaster> display;
+
+// ----------------------------------------------------------------------------
+MAIN_FUNCTION
+{
+	defaultSystemClock::enable();
+	xpcc::cortex::SysTickTimer::enable();
+
+	Sda::connect(MyI2cMaster::Sda);
+	Scl::connect(MyI2cMaster::Scl);
+	MyI2cMaster::initialize<defaultSystemClock, 420000>();
+
+	display.initialize();
+	display.setFont(xpcc::font::Assertion);
+	display << "Hello World!";
+	display.update();
+
+	xpcc::PeriodicTimer<> timer(1000);
+	uint16_t counter(0);
+
+	while (1)
+	{
+		if (timer.isExpired())
+		{
+			display.setCursor(0,20);
+			display << counter++;
+			display.update();
+		}
+	}
+
+
+	return 0;
+}
