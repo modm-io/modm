eb43b62a4803eb195fd2fec0553c5ec1730002a5
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-10-16 17:38:45 +0200

    Fix STM32 protothread example.

--- examples/stm32f4_discovery/protothreads/main.cpp
+++ examples/stm32f4_discovery/protothreads/main.cpp
@@ -10,177 +10,69 @@
 xpcc::IODeviceWrapper<Usart2> device;
 xpcc::IOStream stream(device);
 
-// example of a debug transaction, which outputs the wrapped transactions choices.
-class CustomI2cTransaction : public xpcc::I2cTransaction
-{
-	xpcc::I2cTransaction *transaction;
-
-public:
-	CustomI2cTransaction()
-			:	transaction(nullptr)
-	{}
-
-	void
-	setTransaction(xpcc::I2cTransaction *transaction)
-	{
-		this->transaction = transaction;
-	}
-
-protected:
-	virtual bool
-	attaching()
-	{
-		bool result = transaction->attaching();
-		stream << "T: attaching: " << result << xpcc::endl;
-		return result;
-	}
-
-	virtual Starting
-	starting()
-	{
-		xpcc::I2cTransaction::Starting result = transaction->starting();
-		stream << "T: starting: addr=" << result.address << " next=" << (int)result.next << xpcc::endl;
-		return result;
-	}
-
-	virtual Writing
-	writing()
-	{
-		xpcc::I2cTransaction::Writing result = transaction->writing();
-		stream << "T: writing: length=" << result.length << " next=" << (int)result.next << xpcc::endl;
-		return result;
-	}
-
-	virtual Reading
-	reading()
-	{
-		xpcc::I2cTransaction::Reading result = transaction->reading();
-		stream << "T: reading: length=" << result.length << " next=" << (int)result.next << xpcc::endl;
-		return result;
-	}
-
-	virtual void
-	detaching(DetachCause cause)
-	{
-		transaction->detaching(cause);
-		stream << "T: detaching: cause=" << (int)cause << xpcc::endl;
-	}
-};
-static CustomI2cTransaction myTransaction;
-
-
-// this custom I2c Master wraps the original transaction into the above debug transaction.
-class MyI2cMaster2 : public I2cMaster1
-{
-public:
-	static bool
-	start(xpcc::I2cTransaction *transaction, xpcc::I2c::Configuration_t configuration = nullptr)
-	{
-		myTransaction.setTransaction(transaction);
-		stream << "st" << xpcc::endl;
-		return I2cMaster1::start(&myTransaction, configuration);
-	}
-
-	static bool
-	startBlocking(xpcc::I2cTransaction *transaction, xpcc::I2c::Configuration_t configuration = nullptr)
-	{
-		myTransaction.setTransaction(transaction);
-		stream << "stBl" << xpcc::endl;
-		return I2cMaster1::start(&myTransaction, configuration);
-	}
-};
-
 typedef I2cMaster1 MyI2cMaster;
 
-struct DeviceConfiguration
-{
-	// shared memory for both devices as a cheap way
-	// to `tag` the current configuration of the peripheral
-	// so we do not need to configure it unnecessarily.
-	static char tag;
 
-	/// 20kHz I2C configuration
-	static void
-	slow_configuration()
-	{
-		if (tag != 's') {
-			stream << "config: 20kHz" << xpcc::endl;
-			MyI2cMaster::initialize<defaultSystemClock,  20000>();
-			tag = 's';
-		}
-	}
-
-	/// 100kHz I2C configuration
-	static void
-	fast_configuration()
-	{
-		if (tag != 'f') {
-			stream << "config: 100kHz" << xpcc::endl;
-			MyI2cMaster::initialize<defaultSystemClock, 100000>();
-			tag = 'f';
-		}
-	}
-};
-char DeviceConfiguration::tag = 0;
-
-
-class ProtofaceTemperatureOne : public xpcc::pt::Protothread
+class ThreadOne : public xpcc::pt::Protothread
 {
 public:
-	ProtofaceTemperatureOne()
-	:	temperature(temperatureData, 0x48)
+	ThreadOne()
+	:	temp(temperatureData, 0x48)
 	{
-		// this i2c device will be driven with only 20kHz
-		temperature.attachPeripheralConfiguration(DeviceConfiguration::slow_configuration);
 	}
 
 	bool
 	update()
 	{
+		temp.update();
+
 		PT_BEGIN();
 
 		// ping the device until it responds
 		while(true)
 		{
 			// we wait until the task started
-			PT_WAIT_UNTIL(temperature.startPing());
-			// we wait until the task finished
-			PT_WAIT_WHILE(temperature.runPing());
-			// if the device responds to the ping, break
-			if (temperature.isPingSuccessful())
+			if (PT_CALL(temp.ping(this)))
 				break;
 			// otherwise, try again in 100ms
 			this->timer.restart(100);
 			PT_WAIT_UNTIL(this->timer.isExpired());
 		}
 
-		do
-		{
-			// we need to configure the device before we can use it
-			// so we wait until the task started
-			PT_WAIT_UNTIL(temperature.startConfiguration());
-			// and we wait until it finished
-			PT_WAIT_WHILE(temperature.runConfiguration());
-		}
-		while(!temperature.isConfigurationSuccessful());
+
+		PT_CALL(temp.setUpdateRate(this, 200));
+		PT_CALL(temp.enableExtendedMode(this));
+
+		PT_CALL(temp.configureAlertMode(this,
+				xpcc::tmp102::ThermostatMode::Comparator,
+				xpcc::tmp102::AlertPolarity::ActiveLow,
+				xpcc::tmp102::FaultQueue::Faults6));
+		PT_CALL(temp.writeLowerLimit(this, 28.f));
+		PT_CALL(temp.writeUpperLimit(this, 30.f));
 
 		while (true)
 		{
-			// lets wait until we can read out the device
-			PT_WAIT_UNTIL(temperature.startReadTemperature());
-			// and until this task finishes
-			PT_WAIT_WHILE(temperature.runReadTemperature());
-			// did we succeed with the readout?
-			if (temperature.isReadTemperatureSuccessful())
 			{
-				tf = temperature.getTemperature();
-				stream << "t1: " << (int)tf << xpcc::endl;
+				PT_CALL(temp.readComparatorMode(this, result));
+				float temperature = temp.getTemperature();
+				uint8_t tI = (int) temperature;
+				uint16_t tP = (temperature - tI) * 10000;
+				stream << "T= " << tI << ".";
+				if (tP == 0)
+				{
+					stream << "0000 C";
+				}
+				else if (tP == 625)
+				{
+					stream << "0" << tP << " C";
+				}
+				else
+				{
+					stream << tP << " C";
+				}
+				stream << xpcc::endl;
+				if (result) stream << "Heat me up!" << xpcc::endl;
 			}
-			else {
-				tf = NAN;
-				stream << "t1: NACK"<< xpcc::endl;
-			}
-
 			this->timer.restart(200);
 			PT_WAIT_UNTIL(this->timer.isExpired());
 			LedRed::toggle();
@@ -190,84 +82,13 @@ public:
 	}
 
 private:
-	float tf;
+	bool result;
 	xpcc::Timeout<> timer;
 	uint8_t temperatureData[2];
-	xpcc::Tmp102<MyI2cMaster> temperature;
+	xpcc::Tmp102<MyI2cMaster> temp;
 };
 
-
-class ProtofaceTemperatureTwo : public xpcc::pt::Protothread
-{
-public:
-	ProtofaceTemperatureTwo()
-	:	temperature(temperatureData, 0x48)
-	{
-		// this device will be clocked at 100kHz
-		temperature.attachPeripheralConfiguration(DeviceConfiguration::fast_configuration);
-	}
-
-	bool
-	update()
-	{
-		PT_BEGIN();
-
-		// ping the device until it responds
-		while(true)
-		{
-			// we wait until the task started
-			PT_WAIT_UNTIL(temperature.startPing());
-			// we wait until the task finished
-			PT_WAIT_WHILE(temperature.runPing());
-			// if the device responds to the ping, break
-			if (temperature.isPingSuccessful())
-				break;
-			// otherwise, try again in 100ms
-			this->timer.restart(100);
-			PT_WAIT_UNTIL(this->timer.isExpired());
-		}
-
-		do
-		{
-			// we need to configure the device before we can use it
-			// so we wait until the task started
-			PT_WAIT_UNTIL(temperature.startConfiguration());
-			// and we wait until it finished
-			PT_WAIT_WHILE(temperature.runConfiguration());
-		}
-		while(!temperature.isConfigurationSuccessful());
-
-		while (true)
-		{
-			PT_WAIT_UNTIL(temperature.startReadTemperature());
-			PT_WAIT_WHILE(temperature.runReadTemperature());
-			if (temperature.isReadTemperatureSuccessful())
-			{
-				tf = temperature.getTemperature();
-				stream << "t2: " << (int)tf << xpcc::endl;
-			}
-			else {
-				tf = NAN;
-				stream << "t2: NACK"<< xpcc::endl;
-			}
-
-			this->timer.restart(500);
-			PT_WAIT_UNTIL(this->timer.isExpired());
-			LedGreen::toggle();
-		}
-
-		PT_END();
-	}
-
-private:
-	float tf;
-	xpcc::Timeout<> timer;
-	uint8_t temperatureData[2];
-	xpcc::Tmp102<MyI2cMaster> temperature;
-};
-
-ProtofaceTemperatureOne one;
-ProtofaceTemperatureTwo two;
+ThreadOne one;
 
 // ----------------------------------------------------------------------------
 MAIN_FUNCTION
@@ -287,13 +108,13 @@ MAIN_FUNCTION
 	GpioB8::connect(MyI2cMaster::Scl);
 	GpioB7::configure(Gpio::InputType::PullUp);
 	GpioB8::configure(Gpio::InputType::PullUp);
+	MyI2cMaster::initialize<defaultSystemClock, 100000>();
 
 	stream << "\n\nRESTART\n\n";
 
 	while (1)
 	{
 		one.update();
-		two.update();
 		LedOrange::toggle();
 	}
 
