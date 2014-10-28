7869301143ad3a1aad1c123b96405b91dbe5bcf8
strongly-typed <strongly-typed@nan>
2014-10-28 02:24:14 +0100

    Fix AVR Uart exammple

--- examples/avr/uart/basic/main.cpp
+++ examples/avr/uart/basic/main.cpp
@@ -2,23 +2,28 @@
 #include <xpcc/architecture/platform.hpp>
 
 using namespace xpcc::atmega;
+typedef xpcc::avr::SystemClock clock;
 
 // Create a new UART object and configure it to a baudrate of 115200
-Uart0 uart(115200);
+typedef Uart0 uart;
 
 int
 main()
 {
-	// Enable interrupts, this is needed for every buffered UART
+    GpioOutputD1::connect(Uart0::Tx);
+    GpioInputD0::connect(Uart0::Rx);
+    uart::initialize<clock, 115200>();
+
+    // Enable interrupts, this is needed for every buffered UART
 	sei();
 	
 	// Write some characters
-	uart.write('H');
-	uart.write('e');
-	uart.write('l');
-	uart.write('l');
-	uart.write('o');
-	uart.write('\n');
+    uart::write('H');
+    uart::write('e');
+    uart::write('l');
+    uart::write('l');
+    uart::write('o');
+    uart::write('\n');
 	
 	while (1)
 	{
