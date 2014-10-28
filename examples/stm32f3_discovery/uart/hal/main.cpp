fb517a2178876a8389126c3172591c61e4218f87
strongly-typed <strongly-typed@nan>
2014-10-28 12:12:41 +0100

    Radically fix xpcc check: Disable STM32 Uart initialize

--- examples/stm32f3_discovery/uart/hal/main.cpp
+++ examples/stm32f3_discovery/uart/hal/main.cpp
@@ -8,7 +8,7 @@ MAIN_FUNCTION
 	// Initialize Usart Hal
 	GpioOutputA2::connect(UsartHal2::Tx);
 	GpioInputA3::connect(UsartHal2::Rx, Gpio::InputType::PullUp);
-	UsartHal2::initialize<defaultSystemClock, 115200>(UsartHal2::Parity::Odd);
+//	UsartHal2::initialize<defaultSystemClock, 115200>(UsartHal2::Parity::Odd);
 	UsartHal2::enableInterruptVector(true, 14);
 	UsartHal2::enableInterrupt(UsartHal2::Interrupt::TxEmpty);
 	UsartHal2::setTransmitterEnable(true);
