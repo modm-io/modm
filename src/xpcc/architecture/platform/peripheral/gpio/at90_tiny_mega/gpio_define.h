7402ea46b9af02c0c075c374f634692b48ee0b05
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-11-02 20:25:55 +0100

    AVR: Fix redefines for GPIO and UART.

--- src/xpcc/architecture/platform/peripheral/gpio/at90_tiny_mega/gpio_define.h
+++ src/xpcc/architecture/platform/peripheral/gpio/at90_tiny_mega/gpio_define.h
@@ -8,18 +8,22 @@
  */
 // ----------------------------------------------------------------------------
 
-#ifndef	XPCC_ATTINY_ATMEGA__GPIO_DEFINES_H
-#define	XPCC_ATTINY_ATMEGA__GPIO_DEFINES_H
+#ifndef	XPCC_ATTINY_ATMEGA_GPIO_DEFINES_H
+#define	XPCC_ATTINY_ATMEGA_GPIO_DEFINES_H
 
 // Atmel made a mess with the register names for GPIO between the "old" devices.
 // To avoid to much conditional compiling we try to mimic the new names for
 // the old devices.
 
-#if defined (PCMSK)
+#if defined (PCMSK)// && not defined (PCMSK0)
 #	define PCMSK0	PCMSK
+#endif
+#if defined (PCIE) && not defined (PCIE0)
 #	define PCIE0	PCIE
+#endif
+#if defined (PCIF) && not defined (PCIF0)
 #	define PCIF0	PCIF
 #endif
 
-#endif	// XPCC_ATTINY_ATMEGA__GPIO_DEFINES_H
+#endif	// XPCC_ATTINY_ATMEGA_GPIO_DEFINES_H
 
--- src/xpcc/architecture/platform/peripheral/uart/at90_tiny_mega/uart_defines.h
+++ src/xpcc/architecture/platform/peripheral/uart/at90_tiny_mega/uart_defines.h
@@ -7,33 +7,62 @@
  */
 // ----------------------------------------------------------------------------
 
-#ifndef	XPCC_AT90_ATTINY_ATMEGA__UART_DEFINES_H
-#define	XPCC_AT90_ATTINY_ATMEGA__UART_DEFINES_H
+#ifndef	XPCC_AT90_ATTINY_ATMEGA_UART_DEFINES_H
+#define	XPCC_AT90_ATTINY_ATMEGA_UART_DEFINES_H
 
 // Atmel made a mess with the register names for UART0 between the "old" devices.
 // To avoid to much conditional compiling we try to mimic the new names for
 // the old devices.
-#if defined (UDR) && defined (UDR0)
-#undef UDR0
-#endif
 
-#if defined (UDR)
+#if defined (UDR) && not defined (UDR0)
 #	define UDR0		UDR
+#endif
+#if defined (UBRRL) && not defined (UBRR0L)
 #	define UBRR0L	UBRRL
+#endif
+#if defined (UBRRH) && not defined (UBRR0H)
 #	define UBRR0H	UBRRH
+#endif
+#if defined (UCSRA) && not defined (UCSR0A)
 #	define UCSR0A	UCSRA
+#endif
+#if defined (UCSRB) && not defined (UCSR0B)
 #	define UCSR0B	UCSRB
+#endif
+#if defined (UCSRC) && not defined (UCSR0C)
 #	define UCSR0C	UCSRC
+#endif
+#if defined (DOR) && not defined (DOR0)
 #	define DOR0		DOR
+#endif
+#if defined (FE) && not defined (FE0)
 #	define FE0		FE
+#endif
+#if defined (RXCIE) && not defined (RXCIE0)
 #	define RXCIE0	RXCIE
+#endif
+#if defined (TXCIE) && not defined (TXCIE0)
 #	define TXCIE0	TXCIE
+#endif
+#if defined (RXEN) && not defined (RXEN0)
 #	define RXEN0	RXEN
+#endif
+#if defined (TXEN) && not defined (TXEN0)
 #	define TXEN0	TXEN
+#endif
+#if defined (U2X) && not defined (U2X0)
 #	define U2X0		U2X
+#endif
+#if defined (URSEL) && not defined (URSEL0)
 #	define URSEL0	URSEL
+#endif
+#if defined (UCSZ0) && not defined (UCSZ00)
 #	define UCSZ00	UCSZ0
+#endif
+#if defined (UCSZ1) && not defined (UCSZ01)
 #	define UCSZ01	UCSZ1
+#endif
+#if defined (UDRIE) && not defined (UDRIE0)
 #	define UDRIE0	UDRIE
 #endif
 
@@ -55,5 +84,5 @@
 #	define USART0_TX_vect		USART_TXC_vect
 #endif
 
-#endif	// XPCC_AT90_ATTINY_ATMEGA__UART_DEFINES_H
+#endif	// XPCC_AT90_ATTINY_ATMEGA_UART_DEFINES_H
 
