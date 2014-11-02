9a3ad35b470584ffe70152b1404925a564e7a63d
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-11-02 22:45:36 +0100

    Utils: Add ATTRIBUTE_PACKED for our structs.

--- src/xpcc/architecture/utils.hpp
+++ src/xpcc/architecture/utils.hpp
@@ -37,45 +37,45 @@
 
 	/**
 	 * \brief	Main function definition for microcontroller projects
-	 * 
+	 *
 	 * Inhibits some stack operations at the beginning of main for avr-gcc. May
 	 * save up a few bytes of stack memory.
-	 * 
+	 *
 	 * Typical structure of an microcontroller program:
 	 * \code
 	 * #include <xpcc/architecture/platform.hpp>
-	 * 
+	 *
 	 * MAIN_FUNCTION
 	 * {
 	 *    ...
-	 *    
+	 *
 	 *    while (1)
 	 *    {
 	 *        ...
 	 *    }
 	 * }
 	 * \endcode
-	 * 
+	 *
 	 * \ingroup	platform
 	 */
 	#define	MAIN_FUNCTION
-	
+
 	/**
 	 * \brief	Force inlining
-	 * 
+	 *
 	 * Macro to force inlining on functions if needed. Compiling with -Os  does not
 	 * always inline them when declared only \c inline.
-	 * 
+	 *
 	 * \ingroup	platform
 	 */
 	#define	ALWAYS_INLINE
-	
+
 	/**
 	 * \brief	Convert the argument into a C-String
 	 * \ingroup	platform
 	 */
 	#define	STRINGIFY(s)	#s
-	
+
 	/**
 	 * \brief	Concatenate the two arguments
 	 * \ingroup	platform
@@ -87,15 +87,15 @@
 	#define	STRINGIFY(s)	STRINGIFY_(s)
 	#define	STRINGIFY_(s)	STRINGIFY__(s)
 	#define	STRINGIFY__(s)	#s
-	
+
 	#define	CONCAT(a,b)		CONCAT_(a,b)
 	#define	CONCAT_(a,b)	CONCAT__(a,b)
 	#define	CONCAT__(a,b)	a ## b
-	
+
 	#define	CONCAT3(a,b,c)		CONCAT3_(a,b,c)
 	#define	CONCAT3_(a,b,c)		CONCAT3__(a,b,c)
 	#define	CONCAT3__(a,b,c)	a ## b ## c
-	
+
 	#define	CONCAT4(a,b,c,d)	CONCAT4_(a,b,c,d)
 	#define	CONCAT4_(a,b,c,d)	CONCAT4__(a,b,c,d)
 	#define	CONCAT4__(a,b,c,d)	a ## b ## c ## d
@@ -109,17 +109,19 @@
 	#	define ATTRIBUTE_UNUSED		__attribute__((unused))
 	#	define ATTRIBUTE_WEAK		__attribute__ ((weak))
 	#	define ATTRIBUTE_ALIGNED(n)	__attribute__((aligned(n)))
-	
+	#	define ATTRIBUTE_PACKED		__attribute__((packed))
+
 	// see http://dbp-consulting.com/tutorials/StrictAliasing.html
-	#	define ATTRIBUTE_MAY_ALIAS	__attribute__((__may_alias__))	
+	#	define ATTRIBUTE_MAY_ALIAS	__attribute__((__may_alias__))
 	#else
 	#	define ALWAYS_INLINE  		inline
 	#	define ATTRIBUTE_UNUSED
 	#	define ATTRIBUTE_WEAK
 	#	define ATTRIBUTE_ALIGNED(n)
+	#	define ATTRIBUTE_PACKED
 	#	define ATTRIBUTE_MAY_ALIAS
 	#endif
-	
+
 	#ifdef XPCC__CPU_AVR
 	#	define	MAIN_FUNCTION	int main(void) __attribute__((OS_main)); \
 								int main(void)
