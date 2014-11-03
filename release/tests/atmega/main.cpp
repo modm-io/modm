e5107aff06e1e38363900b37f9367249b3e822a9
strongly-typed <strongly-typed@nan>
2014-11-03 17:24:19 +0100

    Add some test code to check compilability of added ATmega devices.

--- release/tests/atmega/main.cpp
+++ release/tests/atmega/main.cpp
@@ -27,8 +27,25 @@ main(void)
 	Io::setInput();
 	Io::read();
 	
+#if defined(__AVR_ATmega64__)   || \
+	defined(__AVR_ATmega645__)  || \
+	defined(__AVR_ATmega6490__) || \
+	defined(__AVR_ATmega128__) 
+	typedef Uart0 uart;
+	GpioOutputE1::connect(Uart0::Tx);
+	GpioInputE0::connect(Uart0::Rx);
+#elif defined(__AVR_ATmega640__) || \
+	defined(__AVR_ATmega1280__)  || \
+	defined(__AVR_ATmega1281__)
+	typedef Uart1 uart;
+	GpioOutputD3::connect(Uart1::Tx);
+	GpioInputD2::connect(Uart1::Rx);	
+#else
+	typedef Uart0 uart;
 	GpioOutputD1::connect(Uart0::Tx);
 	GpioInputD0::connect(Uart0::Rx);
+#endif
+
 	uart::initialize<clock, 9600>();
 	uart::write('x');
 	
