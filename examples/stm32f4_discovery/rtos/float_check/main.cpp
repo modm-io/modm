37852ecc355d6e961d8ce8d863fb1838bcca3234
strongly-typed <strongly-typed@nan>
2014-11-09 20:19:59 +0100

    Add some documentation to FreeRTOS FPU example

--- examples/stm32f4_discovery/rtos/float_check/main.cpp
+++ examples/stm32f4_discovery/rtos/float_check/main.cpp
@@ -5,40 +5,79 @@
 using namespace xpcc::stm32;
 using namespace xpcc::cortex;
 
+/**
+ * Check FPU support in FreeRTOS for STM32F4.
+ *
+ * FreeRTOS included in xpcc had no out of the box support for FPU.
+ * FPU support was added manually. 
+ *
+ * This example uses four threads to check if task switching still works when
+ * the FPU is used. 
+ *
+ * How to test?
+ * ------------
+ * - Use STM32F4 Discovery Board
+ * - Compile and program (scons program)
+ * - Connect UART at PA2
+ * 
+ * What to expect?
+ * ---------------
+ * - All our LEDs blinking at different rates, about 3 to 4 Hz
+ * - A string at PA2 at 115200 baud
+ *  
+ *  0aA!1bB"2cC#3dD$4eE%5fF&6gG'7hH(8iI9)jJ0*aA1!bB2"cC
+ * 
+ * Each thread prints out a sequence
+ *    0123456789
+ *    abcdefghij
+ *    ABCDEFGHIJ
+ *    !"#$%&'()*
+ * respectivly. 
+ * 
+ * What can fail?
+ * --------------
+ * When FreeRTOS is not compiled without STM32F4XX flag the support for the
+ * extended stack frame is missing in FreeRTOS. Then the device crashes
+ * in the first context switch. So the output '0' or nothing is expected. 
+ * 
+ */
 
 // ----------------------------------------------------------------------------
-template <typename GPIO>
+template <typename Gpio, int SleepTime>
 class P: xpcc::rtos::Thread {
 private:
 	char c;
 	int i;
-	volatile float a = 1;
-	int sleepTime;
+
+	// volatile so the the compiler does not optimise out this operation
+	volatile float a = 1.0;
 public:
-	P(int sleepTime, char c):
+	P(char c):
 	Thread(2,1<<10),
-	c(c),
-	sleepTime(sleepTime)
+	c(c)
 	{
 
 	}
 	void run() {
+		Gpio::setOutput();
 		while (1) {
-			sleep(sleepTime);
+			sleep(SleepTime);
 
-			GPIO::toggle();
+			Gpio::toggle();
 			xpcc::stm32::Usart2::writeBlocking(i + c);
 
 			i = (i+1)%10;
-			a = a*10;
+
+			// Do some float calculation.
+			a = a*10.0;
 		}
 	}
 };
 
-P<GpioD12> p1(260, '0');
-P<GpioD13> p2(260+10, 'a');
-P<GpioD14> p3(260+20, 'A');
-P<GpioD15> p4(260+30, '!');
+P< GpioD12, 260      > p1('0');
+P< GpioD13, 260 + 10 > p2('a');
+P< GpioD14, 260 + 20 > p3('A');
+P< GpioD15, 260 + 30 > p4('!');
 
 
 // ----------------------------------------------------------------------------
@@ -47,11 +86,6 @@ MAIN_FUNCTION
 	typedef Pll<ExternalClock<MHz8>, MHz168, MHz48> clockSource;
 	typedef SystemClock<clockSource> systemClock;
 	systemClock::enable();
-
-	GpioD12::setOutput();
-	GpioD13::setOutput();
-	GpioD14::setOutput();
-	GpioD15::setOutput();
 
 	GpioA2::connect(xpcc::stm32::Usart2::Tx);
 	GpioA3::connect(xpcc::stm32::Usart2::Rx, Gpio::InputType::PullUp);
