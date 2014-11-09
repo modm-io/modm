1edb036ec5b55e7bb6d7d73055d8642e35ff9184
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-11-09 19:27:44 +0100

    UI: Tables: Fix discrepancy between template and generated tables.

--- src/xpcc/ui/led/tables/table16_256.cpp
+++ src/xpcc/ui/led/tables/table16_256.cpp
@@ -1,10 +1,10 @@
 // coding: utf-8
 /* Copyright (c) 2011, Roboterclub Aachen e.V.
-* All Rights Reserved.
-*
-* The file is part of the xpcc library and is released under the 3-clause BSD
-* license. See the file `LICENSE` for the full license governing this code.
-*/
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
+ */
 // ----------------------------------------------------------------------------
 /*
  * WARNING: This file is generated automatically, do not edit!
