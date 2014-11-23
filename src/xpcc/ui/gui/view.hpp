dadc05c8b343b79be4cc8e8e812c6c3006e44442
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-11-23 23:30:29 +0100

    UI: GUI: Add license headers to all GUI files.

--- src/xpcc/ui/gui/view.hpp
+++ src/xpcc/ui/gui/view.hpp
@@ -1,30 +1,9 @@
 // coding: utf-8
-// ----------------------------------------------------------------------------
 /* Copyright (c) 2011, Roboterclub Aachen e.V.
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Roboterclub Aachen e.V. nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL ROBOTERCLUB AACHEN E.V. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
  */
 // ----------------------------------------------------------------------------
 
@@ -45,7 +24,7 @@ namespace gui
 {
 	// forward declaration
 	class GuiViewStack;
-	
+
 	/**
 	 * @brief The View class is the base class for all screens
 	 *        handled by the ViewStack class
@@ -57,7 +36,7 @@ namespace gui
 	class View : public xpcc::AbstractView
 	{
 		friend class GuiViewStack;
-		
+
 	public:
 		/**
 		 * @param stack pointer to the stack, the screen should be displayed on.
@@ -65,7 +44,7 @@ namespace gui
 		 *        displayed on the graphicDisplay
 		 */
 		View(xpcc::gui::GuiViewStack* stack, uint8_t identifier, xpcc::gui::Dimension dimension);
-		
+
 		virtual ~View() = 0;
 
 		/**
@@ -84,13 +63,13 @@ namespace gui
 		postUpdate()
 		{
 		}
-		
+
 		/**
 		 * @brief draw determine the output on the Graphic Display
 		 */
 		virtual void
 		draw();
-		
+
 		/**
 		 *  @brief add widget to view
 		 */
@@ -120,7 +99,7 @@ namespace gui
 
 		void
 		markDrawn();
-		
+
 		inline xpcc::gui::GuiViewStack*
 		getViewStack()
 		{
--- src/xpcc/ui/gui/view_stack.hpp
+++ src/xpcc/ui/gui/view_stack.hpp
@@ -1,30 +1,9 @@
 // coding: utf-8
-// ----------------------------------------------------------------------------
-/* Copyright (c) 2013, Roboterclub Aachen e.V.
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Roboterclub Aachen e.V. nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL ROBOTERCLUB AACHEN E.V. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/* Copyright (c) 2014, Roboterclub Aachen e.V.
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
  */
 // ----------------------------------------------------------------------------
 
@@ -57,9 +36,9 @@ namespace gui
 	{
 	public:
 		GuiViewStack(xpcc::GraphicDisplay* display, xpcc::gui::inputQueue* queue);
-		
+
 		virtual ~GuiViewStack();
-		
+
 		/**
 		 * @brief get the top view from the stack
 		 * @return pointer to view from stack
@@ -70,7 +49,7 @@ namespace gui
 		{
 			return this->stack.get();
 		}
-		
+
 		/**
 		 * @brief push new view on top of stack the new
 		 *        view will be displayed instead of the old
@@ -87,7 +66,7 @@ namespace gui
 			top->draw();
 			this->display->update();
 		}
-		
+
 		/**
 		 * @brief get event input queue from GuiViewStack
 		 */
@@ -104,7 +83,7 @@ namespace gui
 
 		void
 		pop();
-		
+
 		virtual void
 		update();
 
