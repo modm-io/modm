7f6c861e1f8825b0e02a5666260e987714c05634
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-12-04 03:51:58 +0100

    Driver: Add LIS302DL accelerometer driver. This chip can be used with either an I2C or a SPI transport layer.

---
+++ src/xpcc/driver/inertial/lis302.hpp
@@ -0,0 +1,444 @@
+// coding: utf-8
+/* Copyright (c) 2014, Roboterclub Aachen e.V.
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
+ */
+// ----------------------------------------------------------------------------
+
+#ifndef XPCC_LIS302DL_HPP
+#define XPCC_LIS302DL_HPP
+
+#include <xpcc/architecture/peripheral/spi_device.hpp>
+#include <xpcc/architecture/peripheral/i2c_device.hpp>
+#include <xpcc/processing/protothread.hpp>
+#include <xpcc/processing/coroutine.hpp>
+
+namespace xpcc
+{
+
+struct lis302
+{
+	enum class
+	Register : uint8_t
+	{
+		WhoAmI = 0x0F,			///< Device identification (0x3B): r
+
+		CtrlReg1 = 0x20,		///< Control register 1: r/w
+		CtrlReg2 = 0x21,		///< Control register 2: r/w
+		CtrlReg3 = 0x22,		///< Control register 3: r/w
+		HpFilterReset = 0x23,	///< Reading this register momentarily sets all filters to 0
+
+		Status = 0x27,			///< Status Register
+		OutX = 0x29,			///< Output x-axis
+		OutY = 0x2B,			///< Output y-axis
+		OutZ = 0x2D,			///< Output z-axis
+
+		FfWuCfg1 = 0x30,		///< Free fall/wake up configuration register 1
+		FfWuSrc1 = 0x31,		///< Free fall/wake up source register 1
+		FfWuThs1 = 0x32,		///< Free fall/wake up threshold register 1
+		FfWuDuration1 = 0x33,	///< Free fall/wake up duration register 1
+
+		FfWuCfg2 = 0x34,		///< Free fall/wake up configuration register 2
+		FfWuSrc2 = 0x35,		///< Free fall/wake up source register 2
+		FfWuThs2 = 0x36,		///< Free fall/wake up threshold register 2
+		FfWuDuration2 = 0x37,	///< Free fall/wake up duration register 2
+
+		ClickCfg = 0x38,		///< Click configuration register
+		ClickSrc = 0x39,		///< Click source register
+		ClickThsYX = 0x3B,		///< Click threshold register for x- and y-axis
+		ClickThsZ = 0x3C,		///< Click threshold register for z-axis
+		ClickTimeLimit = 0x3D,	///< Click time limit register
+		ClickLatency = 0x3E,	///< Click latency register
+		ClickWindow = 0x3F,		///< Click window register
+	};
+
+public:
+	/// CTRL_REG1 default value is 0x07
+	struct Control1
+	{
+		enum
+		{
+			DR = 0x80,		///< Data rate selection. (0: 100 Hz output data rate; 1: 400 Hz output data rate)
+			PD = 0x40,		///< Power Down Control. (0: power down mode; 1: active mode)
+			FS = 0x20,		///< Full Scale selection. (refer to Table 3 for typical full scale value)
+			STP = 0x10,		///< Self Test Enable. (0: normal mode; 1: self test P enabled)
+			STM = 0x08,		///< Self Test Enable. (0: normal mode; 1: self test M enabled)
+			Zen = 0x04,		///< Z axis enable. (0: Z axis disabled; 1: Z axis enabled)
+			Yen = 0x02,		///< Y axis enable. (0: Y axis disabled; 1: Y axis enabled)
+			Xen = 0x01,		///< X axis enable. (0: X axis disabled; 1: X axis enabled)
+		};
+	};
+
+	/// CTRL_REG2 default value is 0x00
+	struct Control2
+	{
+		enum
+		{
+			SIM = 0x80,			///< SPI Serial Interface Mode selection. (0: 4-wire interface; 1: 3-wire interface)
+			BOOT = 0x40,		///< Reboot memory content. (0: normal mode; 1: reboot memory content)
+
+			FDS = 0x10,			///< Filtered Data Selection. (0: internal filter bypassed; 1: data from internal filter sent to output register)
+			HP_FF_WU2 = 0x08,	///< High Pass filter enabled for FreeFall/WakeUp # 2. (0: filter bypassed; 1: filter enabled)
+			HP_FF_WU1 = 0x04,	///< High Pass filter enabled for Free-Fall/Wake-Up #1. (0: filter bypassed; 1: filter enabled)
+			HP_COEFF2 = 0x02,	///< High pass filter cut-off frequency configuration.
+			HP_COEFF1 = 0x01,	///< High pass filter cut-off frequency configuration.
+		};
+	};
+
+	/// CTRL_REG3 default value is 0x00
+	struct Control3
+	{
+		enum
+		{
+			IHL = 0x80,		///< Interrupt active high, low. (0: active high; 1: active low)
+			PP_OD = 0x40,	///< Push-pull/Open Drain selection on interrupt pad. (0: push-pull; 1: open drain)
+
+			I2CFG2 = 0x20,	///< Data Signal on Int2 pad control bits.
+			I2CFG1 = 0x10,	///< Data Signal on Int2 pad control bits.
+			I2CFG0 = 0x08,	///< Data Signal on Int2 pad control bits.
+
+			I1CFG2 = 0x04,	///< Data Signal on Int1 pad control bits.
+			I1CFG1 = 0x02,	///< Data Signal on Int1 pad control bits.
+			I1CFG0 = 0x01,	///< Data Signal on Int1 pad control bits.
+		};
+	};
+
+	/// STATUS_REG default value is 0x00
+	struct Status
+	{
+		enum
+		{
+			ZYXOR = 0x80,	///< X, Y and Z axis data overrun.
+			ZOR = 0x40,		///< Z axis data overrun.
+			YOR = 0x20,		///< Y axis data overrun.
+			XOR = 0x10,		///< X axis data overrun.
+
+			ZYXDA = 0x08,	///< X, Y and Z axis new data available.
+			ZDA = 0x04,		///< Z axis new data available.
+			YDA = 0x02,		///< Y axis new data available.
+			XDA = 0x01,		///< X axis new data available.
+		};
+	};
+
+	/// FF_WU_CFG default value is 0x00
+	struct FreeFallConfig
+	{
+		enum
+		{
+			AOI = 0x80,		///< And/or combination of Interrupt events. (0: OR combination of interrupt events; 1: AND combination of interrupt events)
+			LIR = 0x40,		///< Latch Interrupt request into FF_WU_SRC reg with the FF_WU_SRC reg cleared by reading FF_WU_SRC_1 reg.
+			ZHIE = 0x20,	///< Enable interrupt generation on Z high event.
+			ZLIE = 0x10,	///< Enable interrupt generation on Z low event.
+			YHIE = 0x08,	///< Enable interrupt generation on Y high event.
+			YLIE = 0x04,	///< Enable interrupt generation on Y low event.
+			XHIE = 0x02,	///< Enable interrupt generation on X high event.
+			XLIE = 0x01,	///< Enable interrupt generation on X low event.
+		};
+	};
+
+	/// FF_WU_SRC default value is 0x00
+	struct FreeFallSource
+	{
+		enum
+		{
+			IA = 0x40,	///< Interrupt Active.
+			ZH = 0x20,	///< Z high.
+			ZL = 0x10,	///< Z low.
+			YH = 0x08,	///< Y high.
+			YL = 0x04,	///< Y low.
+			XH = 0x02,	///< X high.
+			XL = 0x01,	///< X low.
+		};
+	};
+
+	/// FF_WU_THS default value is 0x00
+	struct FreeFallThreshold
+	{
+		enum
+		{
+			DRCM = 0x80,	///< Resetting mode selection.
+			THS = 0x7F,		///< Free-fall / wake-up Threshold.
+		};
+	};
+
+	/// CLOCK_CFG default value is 0x00
+	struct ClickConfig
+	{
+		enum
+		{
+			LIR = 0x40,		///< Latch interrupt request into CLICK_SRC reg with the CLICK_SRC reg refreshed by reading CLICK_SRC reg.
+			Double_Z = 0x20,
+			Single_Z = 0x10,
+			Double_Y = 0x08,
+			Single_Y = 0x04,
+			Double_X = 0x02,
+			Single_X = 0x01,
+		};
+	};
+
+	/// CLOCK_SRC default value is 0x00
+	struct ClickSource
+	{
+		enum
+		{
+			IA = 0x40,		///< Interrupt Active.
+			Double_Z = 0x20,
+			Single_Z = 0x10,
+			Double_Y = 0x08,
+			Single_Y = 0x04,
+			Double_X = 0x02,
+			Single_X = 0x01,
+		};
+	};
+
+public:
+	enum class
+	InterruptSource : uint8_t
+	{
+		GND = 0x00,
+		FF_WU_1 = 0x01,
+		FF_WU_2 = 0x02,
+		FF_WU_1_OR_2 = 0x02,
+		DataReady = 0x04,
+		Click = 0x07,
+	};
+
+	enum class
+	MeasurementRate : uint8_t
+	{
+		Hz100 = 0x00,
+		Hz400 = Control1::DR,
+	};
+
+	enum class
+	Scale : uint8_t
+	{
+		G2 = 0x00,
+		G8 = Control1::FS,
+	};
+
+public:
+	struct __attribute__ ((packed))
+	Data
+	{
+		Data()
+		:	data{0,0,0,0}
+		{}
+		// DATA ACCESS
+		/// @{
+		/// returns the acceleration in g
+		float ALWAYS_INLINE
+		getX() { return data[0] * (data[3] ? 9.2f/128 : 2.3f/128); }
+
+		float ALWAYS_INLINE
+		getY() { return data[1] * (data[3] ? 9.2f/128 : 2.3f/128); }
+
+		float ALWAYS_INLINE
+		getZ() { return data[2] * (data[3] ? 9.2f/128 : 2.3f/128); }
+		/// @}
+
+
+		ALWAYS_INLINE uint8_t
+		operator [](uint8_t index)
+		{ return (index < 3) ? data[index] : 0; }
+
+		ALWAYS_INLINE int8_t*
+		getPointer()
+		{ return data; }
+
+	private:
+		// data 0-2 = x,y,z
+		// data 3 = bool
+		int8_t data[4];
+	};
+
+protected:
+	/// @{
+	/// @private enum class to integer helper functions.
+	static constexpr uint8_t
+	i(Register reg) { return static_cast<uint8_t>(reg); }
+	static constexpr uint8_t
+	i(MeasurementRate rate) { return static_cast<uint8_t>(rate); }
+	static constexpr uint8_t
+	i(Scale scale) { return static_cast<uint8_t>(scale); }
+	/// @}
+}; // struct lis302
+
+/**
+ * LIS302DL 3-Axis accelerometer.
+ *
+ * The LIS302DL is an ultra compact low-power three axes linear accelerometer.
+ * It includes a sensing element and an IC interface able to provide the measured
+ * acceleration to the external world through an I2C/SPI serial interface.
+ *
+ * @tparam	Transport	Either the I2C or SPI Transport Layer.
+ * @see Lis302I2cTransport
+ * @see Lis302SpiTransport
+ *
+ * @ingroup inertial
+ * @author	Niklas Hauser
+ */
+template < class Transport >
+class Lis302 : public lis302, public Transport, protected xpcc::pt::Protothread
+{
+public:
+	/// Constructor, requires a lis302::Data object.
+	/// For I2c this also sets the address to 0x1D (alternative: 0x1C).
+	Lis302(Data &data, uint8_t address=0x1D);
+
+	bool
+	initialize(Scale scale, MeasurementRate rate = MeasurementRate::Hz100)
+	{
+		return CO_CALL_BLOCKING(initialize(this, scale, rate));
+	}
+
+	xpcc::co::Result<bool>
+	initialize(void *ctx, Scale scale, MeasurementRate rate = MeasurementRate::Hz100);
+
+	xpcc::co::Result<bool>
+	writeControlRegister(void *ctx, uint8_t index, uint8_t setMask, uint8_t clearMask = 0xff);
+
+	xpcc::co::Result<bool>
+	readAcceleration(void *ctx);
+
+	uint8_t
+	getStatus()
+	{ return rawBuffer[3]; }
+
+	uint8_t &
+	getControl(uint8_t index)
+	{ return rawBuffer[index]; }
+
+public:
+	/// the data object for this sensor.
+	Data &data;
+
+private:
+	// 0-2: control 0-2
+	// 3: status
+	// 5: out x
+	// 7: out y
+	// 9: out z
+	uint8_t rawBuffer[10];
+};
+
+/**
+ * LIS302DL I2C Transport Layer.
+ *
+ * This class manages communication with the accelerometer via the I2C bus.
+ *
+ * To enable I2C mode in the LIS302, the CS pin must be tied high.
+ * However, it is internally pulled-up, so just leaving the CS pin disconnected should work too.
+ *
+ * The I2C interface is compliant with Fast Mode (up to 400kHz).
+ *
+ * @see Lis302
+ *
+ * @ingroup inertial
+ * @author	Niklas Hauser
+ */
+template < class I2cMaster >
+class Lis302I2cTransport : public xpcc::I2cDevice< I2cMaster >, protected xpcc::co::NestedCoroutine<1>
+{
+public:
+	Lis302I2cTransport(uint8_t address);
+
+	/// pings the sensor
+	xpcc::co::Result<bool>
+	ping(void *ctx);
+
+protected:
+	// RAW REGISTER ACCESS
+	/// write a 8bit value
+	xpcc::co::Result<bool>
+	write(void *ctx, uint8_t reg, uint8_t value);
+
+	/// read a 8bit value
+	xpcc::co::Result<bool> ALWAYS_INLINE
+	read(void *ctx, uint8_t reg, uint8_t &value)
+	{
+		return read(ctx, reg, &value, 1);
+	}
+
+	/// read multiple 8bit values from a start register
+	xpcc::co::Result<bool>
+	read(void *ctx, uint8_t reg, uint8_t *buffer, uint8_t length);
+
+private:
+	enum I2cTask : uint8_t
+	{
+		Idle = 0,
+		Ping = 0x01,
+		// Insert all registers from 0x0F to 0x3F
+	};
+
+	uint8_t buffer[2];
+	volatile uint8_t i2cTask;
+	volatile uint8_t i2cSuccess;
+	I2cTagAdapter<I2cWriteReadAdapter> adapter;
+
+	// increment address or not?
+	static constexpr uint8_t AddressIncrement = 0x80;
+	static constexpr uint8_t AddressStatic = 0x00;
+};
+
+/**
+ * LIS302DL SPI Transport Layer.
+ *
+ * This class manages communication with the accelerometer via the SPI bus.
+ * The SPI interface can be clocked with up to 10MHz.
+ *
+ * @see Lis302
+ *
+ * @tparam	Cs	connected Chip Select Pin
+ *
+ * @ingroup inertial
+ * @author	Niklas Hauser
+ */
+template < class SpiMaster, class Cs >
+class Lis302SpiTransport : public xpcc::SpiDevice< SpiMaster >, protected xpcc::co::NestedCoroutine<1>
+{
+public:
+	Lis302SpiTransport(uint8_t /*address*/);
+
+	/// pings the sensor
+	xpcc::co::Result<bool>
+	ping(void *ctx);
+
+protected:
+	// RAW REGISTER ACCESS
+	/// write a 8bit value
+	xpcc::co::Result<bool>
+	write(void *ctx, uint8_t reg, uint8_t value);
+
+	/// read a 8bit value
+	xpcc::co::Result<bool> ALWAYS_INLINE
+	read(void *ctx, uint8_t reg, uint8_t &value)
+	{
+		return read(ctx, reg, &value, 1);
+	}
+
+	/// read multiple 8bit values from a start register
+	xpcc::co::Result<bool>
+	read(void *ctx, uint8_t reg, uint8_t *buffer, uint8_t length);
+
+private:
+	uint8_t lengthBuffer;
+	uint8_t whoAmI;
+
+	// write read bit on the address
+	static constexpr uint8_t Read = 0x80;
+	static constexpr uint8_t Write = 0x00;
+
+	// increment address or not?
+	static constexpr uint8_t AddressIncrement = 0x40;
+	static constexpr uint8_t AddressStatic = 0x00;
+};
+
+} // namespace xpcc
+
+#include "lis302_impl.hpp"
+
+#endif	// XPCC_LIS302DL_HPP
---
+++ src/xpcc/driver/inertial/lis302_impl.hpp
@@ -0,0 +1,194 @@
+// coding: utf-8
+/* Copyright (c) 2013, Roboterclub Aachen e.V.
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
+ */
+// ----------------------------------------------------------------------------
+
+#ifndef XPCC_LIS302DL_HPP
+#	error  "Don't include this file directly, use 'lis302.hpp' instead!"
+#endif
+
+// ----------------------------------------------------------------------------
+// MARK: LIS302 DRIVER
+template < class Transport >
+xpcc::Lis302<Transport>::Lis302(Data &data, uint8_t address)
+:	Transport(address), data(data), rawBuffer{0x47,0,0,0,0,0,0,0,0,0}
+{
+}
+
+
+template < class Transport >
+xpcc::co::Result<bool>
+xpcc::Lis302<Transport>::initialize(void *ctx, Scale scale, MeasurementRate rate)
+{
+	return writeControlRegister(ctx, 0, i(scale) | i(rate), Control1::DR | Control1::FS);
+}
+
+template < class Transport >
+xpcc::co::Result<bool>
+xpcc::Lis302<Transport>::writeControlRegister(void *ctx, uint8_t index, uint8_t setMask, uint8_t clearMask)
+{
+	CO_BEGIN(ctx);
+
+	if (index < 2)
+	{
+		rawBuffer[index] = (rawBuffer[index] & ~clearMask) | setMask;
+		if (index == 0)
+			data.getPointer()[3] = (rawBuffer[0] & Control1::FS);
+
+		CO_RETURN_CALL(this->write(ctx, i(Register::CtrlReg1) + index, rawBuffer[index]));
+	}
+
+	CO_END_RETURN(false);
+}
+
+template < class Transport >
+xpcc::co::Result<bool>
+xpcc::Lis302<Transport>::readAcceleration(void *ctx)
+{
+	CO_BEGIN(ctx);
+
+	if (CO_CALL(this->read(ctx, i(Register::Status), rawBuffer + 3, 7)))
+	{
+		data.getPointer()[0] = rawBuffer[5];
+		data.getPointer()[1] = rawBuffer[7];
+		data.getPointer()[2] = rawBuffer[9];
+		CO_RETURN(true);
+	}
+
+	CO_END_RETURN(false);
+}
+
+// ----------------------------------------------------------------------------
+// MARK: I2C TRANSPORT
+template < class I2cMaster >
+xpcc::Lis302I2cTransport<I2cMaster>::Lis302I2cTransport(uint8_t address)
+:	i2cTask(I2cTask::Idle), i2cSuccess(0),
+	adapter(address, i2cTask, i2cSuccess)
+{
+}
+
+// MARK: ping
+template < class I2cMaster >
+xpcc::co::Result<bool>
+xpcc::Lis302I2cTransport<I2cMaster>::ping(void *ctx)
+{
+	CO_BEGIN(ctx);
+
+	CO_WAIT_UNTIL(adapter.configurePing() &&
+			(i2cTask = I2cTask::Ping, this->startTransaction(&adapter)));
+
+	CO_WAIT_WHILE(i2cTask == I2cTask::Ping);
+
+	CO_END_RETURN(i2cSuccess == I2cTask::Ping);
+}
+
+// MARK: - register access
+// MARK: write register
+template < class I2cMaster >
+xpcc::co::Result<bool>
+xpcc::Lis302I2cTransport<I2cMaster>::write(void *ctx, uint8_t reg, uint8_t value)
+{
+	CO_BEGIN(ctx);
+
+	buffer[0] = reg | AddressStatic;
+	buffer[1] = value;
+
+	CO_WAIT_UNTIL(
+			adapter.configureWrite(buffer, 2) &&
+			(i2cTask = reg, this->startTransaction(&adapter))
+	);
+
+	CO_WAIT_WHILE(i2cTask == reg);
+
+	CO_END_RETURN(i2cSuccess == reg);
+}
+
+// MARK: read register
+template < class I2cMaster >
+xpcc::co::Result<bool>
+xpcc::Lis302I2cTransport<I2cMaster>::read(void *ctx, uint8_t reg, uint8_t *buffer, uint8_t length)
+{
+	CO_BEGIN(ctx);
+
+	this->buffer[0] = reg | AddressIncrement;
+
+	CO_WAIT_UNTIL(
+			adapter.configureWriteRead(this->buffer, 1, buffer, length) &&
+			(i2cTask = reg, this->startTransaction(&adapter))
+	);
+
+	CO_WAIT_WHILE(i2cTask == reg);
+
+	CO_END_RETURN(i2cSuccess == reg);
+}
+
+// ----------------------------------------------------------------------------
+// MARK: SPI TRANSPORT
+template < class SpiMaster, class Cs >
+xpcc::Lis302SpiTransport<SpiMaster, Cs>::Lis302SpiTransport(uint8_t /*address*/)
+:	lengthBuffer(0), whoAmI(0)
+{
+	Cs::setOutput(xpcc::Gpio::High);
+}
+
+// MARK: ping
+template < class SpiMaster, class Cs >
+xpcc::co::Result<bool>
+xpcc::Lis302SpiTransport<SpiMaster, Cs>::ping(void *ctx)
+{
+	CO_BEGIN(ctx);
+
+	CO_CALL(read(ctx, static_cast<uint8_t>(lis302::Register::WhoAmI), whoAmI));
+
+	CO_END_RETURN(whoAmI == 0x3B);
+}
+
+// MARK: - register access
+// MARK: write register
+template < class SpiMaster, class Cs >
+xpcc::co::Result<bool>
+xpcc::Lis302SpiTransport<SpiMaster, Cs>::write(void *ctx, uint8_t reg, uint8_t value)
+{
+	CO_BEGIN(ctx);
+
+	CO_WAIT_UNTIL(this->aquireMaster(ctx));
+	Cs::reset();
+
+	CO_CALL(SpiMaster::transfer(reg | Write | AddressStatic));
+	CO_CALL(SpiMaster::transfer(value));
+
+	if (this->releaseMaster(ctx))
+		Cs::set();
+
+	CO_END_RETURN(true);
+}
+
+// MARK: read register
+template < class SpiMaster, class Cs >
+xpcc::co::Result<bool>
+xpcc::Lis302SpiTransport<SpiMaster, Cs>::read(void *ctx, uint8_t reg, uint8_t *buffer, uint8_t length)
+{
+	CO_BEGIN(ctx);
+
+	Cs::set();
+	CO_WAIT_UNTIL(this->aquireMaster(ctx));
+	Cs::reset();
+
+	CO_CALL(SpiMaster::transfer(reg | Read | AddressIncrement));
+
+	for (lengthBuffer = 0; lengthBuffer < length; lengthBuffer++)
+	{
+		whoAmI = CO_CALL(SpiMaster::transfer(0));
+		buffer[lengthBuffer] = whoAmI;
+	}
+
+	if (this->releaseMaster(ctx))
+		Cs::set();
+
+	CO_END_RETURN(true);
+}
+
