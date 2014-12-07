236f74aadb1d651ceb22acd424a9ba6a3b19d794
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-12-07 17:55:05 +0100

    Register: Using combined enum class and struct for type. This however does not fully overcome the enum class casting problem.

--- src/xpcc/architecture/driver/test/register_test.cpp
+++ src/xpcc/architecture/driver/test/register_test.cpp
@@ -8,66 +8,38 @@
 // ----------------------------------------------------------------------------
 
 #include "register_test.hpp"
-#include "register_definitions.hpp"
-
-constexpr Control3::Bit Control3::IHL;
-constexpr Control3::Bit Control3::PP_OD;
-
-//constexpr Control3::Group::Config Control3::InterruptSource::GND;
-//constexpr Control3::Group::Config Control3::InterruptSource::Click;
-//constexpr Control3::Group::Config Control3::InterruptSource::DataReady;
-
-//constexpr Control3::Group::Config Control3::InterruptSource::Mask;
-constexpr Control3::Group::Config Control3::Source2::Mask;
-constexpr Control3::Group::Config Control3::Source2::Click;
-constexpr Control3::Group::Config Control3::Source1::Click;
 
 void
-RegisterTest::testExclusiveness()
+RegisterTest::testOperators()
 {
-	TEST_ASSERT_EQUALS(Control3::IHL.value, 0x80);
+	Test_t vD = Test::A | Test::B;
+	TEST_ASSERT_EQUALS(vD.value, 0b11);
 
-	Test valueDirect = Test::A | Test::B;
-	TEST_ASSERT_EQUALS(static_cast<uint8_t>(valueDirect), 0x07);
-	Test valueA = Test::A;
-	Test valueB = Test::B;
-	Test valueIndirect = valueA | valueB | Test::A;
-	TEST_ASSERT_EQUALS(static_cast<uint8_t>(valueIndirect), 0x07);
+	vD = vD | Test::C;
+	TEST_ASSERT_EQUALS(vD.value, 0b111);
 
-	STest vD = STest::A | STest::B;
-	vD = vD | STest::A;
+	vD = Test::D | vD;
+	TEST_ASSERT_EQUALS(vD.value, 0b1111);
 
-//	Control3 control3 = Control3::IHL;
-//	ControlRegister controlRegister = Control3::IHL;
-//
-//	TEST_ASSERT_EQUALS(control3.value, 0x80);
-//	TEST_ASSERT_EQUALS(controlRegister.value, 0x80);
-//
-//	control3 = Control3::Source2::Click;
-//
-//	TEST_ASSERT_EQUALS(control3.value, 0b111000);
-//	TEST_ASSERT_EQUALS(Control3::Source2::Mask.value, 0b111000);
-//
-//	control3 = Control3::Source1::Click;
-//
-//	TEST_ASSERT_EQUALS(control3.value, 0b111);
+	Test_t vI = Test::E;
+	vD = vD | vI;
+	TEST_ASSERT_EQUALS(vD.value, 0b11111);
 
-////	Control1::Source2 source2 = Control3::Source2::GND;
-////	Control3 control3 = source2 | Control3::Source2::GND;
-////	control3 ^= ~Control3::Source2::GND;
-////	Control1 control1 = Control1::DR | Control1::PD | Control1::FS;
-////
-////	Control1 control11 = control1 | Control1::STP;
-//////	control11 |= Control1::STP;
-////
-////	ControlRegister controlRegister1 = control1;
-////	ControlRegister controlRegister3 = Control3::IHL | Control3::Source2::GND;
-////
-////	uint8_t raw = controlRegister1.value;
-////	raw |= Control1::PD.value;
-////	uint8_t bitValue = Control1::PD.value;
-////
-////	bool isZero = control1 & Control1::PD;
-////	bool isSame = control3 == Control3::Source2::GND;
+	// simple raw access is possible
+	uint8_t raw = vD.value;
+	(void) raw;
+
+	// this is possible too
+	Register8 reg = vD;
+	// and then you cannot modify it anymore, which makes sense
+	// since you don't actually know which register this is)
+//	reg = reg | Test::A;
+	(void) reg;
+
+	// but this is *not possible*, but it must be possible, otherwise it makes no sense.
+//	reg = Test::A;
+	// neither is extracting the enum class value directly
+//	uint8_t raw2 = Test::A.value;
+
 }
 
--- src/xpcc/architecture/driver/test/register_test.hpp
+++ src/xpcc/architecture/driver/test/register_test.hpp
@@ -11,49 +11,111 @@
 #define XPCC_UNITTEST_REGISTER_HPP
 
 #include <unittest/testsuite.hpp>
-#include "register_definitions.hpp"
+//#include "register_definitions.hpp"
 
 
-#define REGISTER8(name, ...) \
-	struct name { \
-	uint8_t value; \
-	constexpr STest(uint8_t value) \
-	:	value(value) {} \
-	constexpr operator uint8_t() const \
-	{ return value; } \
-	enum : uint8_t { \
-		__VA_ARGS__ \
-	}; };
+#define REGISTER(type, name, ...) \
+	enum class name : type { \
+		__VA_ARGS__ }; \
+	struct CONCAT(name, _t) : public Register<type> { \
+		constexpr CONCAT(name, _t)(name value) \
+		:	Register8(static_cast<type>(value)) {} \
+		friend constexpr CONCAT(name, _t) operator|(name const &lhs, CONCAT(name, _t) const &rhs) \
+		{ return CONCAT(name, _t)(static_cast<name>(static_cast<type>(lhs) | rhs.value)); } \
+		friend constexpr CONCAT(name, _t) operator|(CONCAT(name, _t) const &lhs, name const &rhs) \
+		{ return CONCAT(name, _t)(static_cast<name>(static_cast<type>(rhs) | lhs.value)); } \
+		friend constexpr CONCAT(name, _t) operator|(CONCAT(name, _t) const &lhs, CONCAT(name, _t) const &rhs) \
+		{ return CONCAT(name, _t)(static_cast<name>(lhs.value | rhs.value)); } \
+	}; \
+	constexpr CONCAT(name, _t) operator|(name const &lhs, name const &rhs) \
+	{ return CONCAT(name, _t)(static_cast<name>(static_cast<type>(lhs) | static_cast<type>(rhs))); }
+
+
+
+template< typename T >
+struct Register
+{
+	typedef T Type;
+	T value{0};
+
+	operator bool() const
+	{ return value != 0; }
+
+	// do NOT cast to anything else
+	template<typename U>
+	operator U() const = delete;
+
+protected:
+	constexpr Register(T value)
+	:	value(value) {}
+};
+typedef Register<uint8_t> Register8;
+
+//REGISTER( uint8_t, Test,
+//	A = 0x01,
+//	B = 0x02,
+//	C = 0x04,
+//	D = 0x08,
+//	E = 0x10,
+//	F = 0x20,
+//	G = 0x40,
+//	H = 0x80,
+//	Mask = 0xff,
+//);
+
+REGISTER( uint8_t, Test2,
+		A = 0x01,
+		B = 0x02,
+		C = 0x04,
+		D = 0x08,
+		E = 0x10,
+		F = 0x20,
+		G = 0x40,
+		H = 0x80,
+		Mask = 0xff,
+);
 
 enum class
 Test : uint8_t
 {
-	A = 0x03,
-	B = 0x04,
-	Mask = 0x07,
+	A = 0x01,
+	B = 0x02,
+	C = 0x04,
+	D = 0x08,
+	E = 0x10,
+	F = 0x20,
+	G = 0x40,
+	H = 0x80,
+	Mask = 0xff,
 };
 
-constexpr Test operator|(Test const &lhs, Test const &rhs)
+struct
+Test_t : public Register8
 {
-	return static_cast<Test>(static_cast<uint8_t>(lhs) | static_cast<uint8_t>(rhs));
+	constexpr Test_t(Test value)
+	:	Register8(static_cast<uint8_t>(value)) {}
+
+	friend constexpr Test_t operator|(Test const &lhs, Test_t const &rhs)
+	{
+		return Test_t(static_cast<Test>(static_cast<uint8_t>(lhs) | rhs.value));
+	}
+
+	friend constexpr Test_t operator|(Test_t const &lhs, Test const &rhs)
+	{
+		return Test_t(static_cast<Test>(static_cast<uint8_t>(rhs) | lhs.value));
+	}
+
+	friend constexpr Test_t operator|(Test_t const &lhs, Test_t const &rhs)
+	{
+		return Test_t(static_cast<Test>(lhs.value | rhs.value));
+	}
+};
+
+constexpr Test_t operator|(Test const &lhs, Test const &rhs)
+{
+	return Test_t(static_cast<Test>(static_cast<uint8_t>(lhs) | static_cast<uint8_t>(rhs)));
 }
 
-struct STest
-{
-	uint8_t value;
-	constexpr STest(uint8_t value)
-	:	value(value) {}
-
-	constexpr operator uint8_t() const
-	{ return value; }
-
-	enum : uint8_t
-	{
-		A = 0x03,
-		B = 0x04,
-		Mask = 0x07
-	};
-};
 
 //REGISTER8(STest,
 //	A = 0x03,
@@ -67,13 +129,10 @@ class RegisterTest : public unittest::TestSuite
 {
 public:
 	void
-	testOperators()
-	{
+	testOperators();
 
-	}
-
-	void
-	testExclusiveness();
+//	void
+//	testExclusiveness();
 
 //	void
 //	testGroup();
