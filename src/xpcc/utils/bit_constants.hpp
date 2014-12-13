5b0aabc11f3f190e3571cc5828516b73d9804c67
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-12-13 20:17:09 +0100

    Utils: Suppress warnings "left shift count >= width of type" for AVR.

--- src/xpcc/utils/bit_constants.hpp
+++ src/xpcc/utils/bit_constants.hpp
@@ -15,38 +15,38 @@
 namespace xpcc
 {
 
-constexpr uint8_t  Bit0  = (1 <<  0);
-constexpr uint8_t  Bit1  = (1 <<  1);
-constexpr uint8_t  Bit2  = (1 <<  2);
-constexpr uint8_t  Bit3  = (1 <<  3);
-constexpr uint8_t  Bit4  = (1 <<  4);
-constexpr uint8_t  Bit5  = (1 <<  5);
-constexpr uint8_t  Bit6  = (1 <<  6);
-constexpr uint8_t  Bit7  = (1 <<  7);
-constexpr uint16_t Bit8  = (1 <<  8);
-constexpr uint16_t Bit9  = (1 <<  9);
-constexpr uint16_t Bit10 = (1 << 10);
-constexpr uint16_t Bit11 = (1 << 11);
-constexpr uint16_t Bit12 = (1 << 12);
-constexpr uint16_t Bit13 = (1 << 13);
-constexpr uint16_t Bit14 = (1 << 14);
-constexpr uint16_t Bit15 = (1 << 15);
-constexpr uint32_t Bit16 = (1 << 16);
-constexpr uint32_t Bit17 = (1 << 17);
-constexpr uint32_t Bit18 = (1 << 18);
-constexpr uint32_t Bit19 = (1 << 19);
-constexpr uint32_t Bit20 = (1 << 20);
-constexpr uint32_t Bit21 = (1 << 21);
-constexpr uint32_t Bit22 = (1 << 22);
-constexpr uint32_t Bit23 = (1 << 23);
-constexpr uint32_t Bit24 = (1 << 24);
-constexpr uint32_t Bit25 = (1 << 25);
-constexpr uint32_t Bit26 = (1 << 26);
-constexpr uint32_t Bit27 = (1 << 27);
-constexpr uint32_t Bit28 = (1 << 28);
-constexpr uint32_t Bit29 = (1 << 29);
-constexpr uint32_t Bit30 = (1 << 30);
-constexpr uint32_t Bit31 = (1 << 31);
+constexpr uint8_t  Bit0  = (1ul <<  0);
+constexpr uint8_t  Bit1  = (1ul <<  1);
+constexpr uint8_t  Bit2  = (1ul <<  2);
+constexpr uint8_t  Bit3  = (1ul <<  3);
+constexpr uint8_t  Bit4  = (1ul <<  4);
+constexpr uint8_t  Bit5  = (1ul <<  5);
+constexpr uint8_t  Bit6  = (1ul <<  6);
+constexpr uint8_t  Bit7  = (1ul <<  7);
+constexpr uint16_t Bit8  = (1ul <<  8);
+constexpr uint16_t Bit9  = (1ul <<  9);
+constexpr uint16_t Bit10 = (1ul << 10);
+constexpr uint16_t Bit11 = (1ul << 11);
+constexpr uint16_t Bit12 = (1ul << 12);
+constexpr uint16_t Bit13 = (1ul << 13);
+constexpr uint16_t Bit14 = (1ul << 14);
+constexpr uint16_t Bit15 = (1ul << 15);
+constexpr uint32_t Bit16 = (1ul << 16);
+constexpr uint32_t Bit17 = (1ul << 17);
+constexpr uint32_t Bit18 = (1ul << 18);
+constexpr uint32_t Bit19 = (1ul << 19);
+constexpr uint32_t Bit20 = (1ul << 20);
+constexpr uint32_t Bit21 = (1ul << 21);
+constexpr uint32_t Bit22 = (1ul << 22);
+constexpr uint32_t Bit23 = (1ul << 23);
+constexpr uint32_t Bit24 = (1ul << 24);
+constexpr uint32_t Bit25 = (1ul << 25);
+constexpr uint32_t Bit26 = (1ul << 26);
+constexpr uint32_t Bit27 = (1ul << 27);
+constexpr uint32_t Bit28 = (1ul << 28);
+constexpr uint32_t Bit29 = (1ul << 29);
+constexpr uint32_t Bit30 = (1ul << 30);
+constexpr uint32_t Bit31 = (1ul << 31);
 
 };	// namespace xpcc
 
