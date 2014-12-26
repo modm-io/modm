d0c1185d0c9dbdd70425f44d167b252ceaa7b33c
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-12-26 17:29:19 +0100

    Unittest: Fix include paths.

--- src/xpcc/communication/xpcc/backend/can/test/fake_can_driver.hpp
+++ src/xpcc/communication/xpcc/backend/can/test/fake_can_driver.hpp
@@ -31,7 +31,6 @@
 #ifndef FAKE_CAN_DRIVER_HPP
 #define FAKE_CAN_DRIVER_HPP
 
-#include <xpcc/communication/can/message.hpp>
 #include <xpcc/container/linked_list.hpp>
 #include <xpcc/architecture/peripheral/can.hpp>
 
