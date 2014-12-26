8ac1d3ed0f26d92f2219483e79dca7ce4eb8c6e5
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-12-26 22:38:11 +0100

    Uart: AVR: Fix redefinition of defines for uart.

--- src/xpcc/architecture/platform/peripheral/uart/at90_tiny_mega/uart_defines.h
+++ src/xpcc/architecture/platform/peripheral/uart/at90_tiny_mega/uart_defines.h
@@ -14,73 +14,87 @@
 // To avoid to much conditional compiling we try to mimic the new names for
 // the old devices.
 
-#if defined (UDR) && not defined (UDR0)
+#if defined (UDR) and not defined (UDR0)
 #	define UDR0		UDR
 #endif
-#if defined (UBRRL) && not defined (UBRR0L)
+#if defined (UBRRL) and not defined (UBRR0L)
 #	define UBRR0L	UBRRL
 #endif
-#if defined (UBRRH) && not defined (UBRR0H)
+#if defined (UBRRH) and not defined (UBRR0H)
 #	define UBRR0H	UBRRH
 #endif
-#if defined (UCSRA) && not defined (UCSR0A)
+#if defined (UCSRA) and not defined (UCSR0A)
 #	define UCSR0A	UCSRA
 #endif
-#if defined (UCSRB) && not defined (UCSR0B)
+#if defined (UCSRB) and not defined (UCSR0B)
 #	define UCSR0B	UCSRB
 #endif
-#if defined (UCSRC) && not defined (UCSR0C)
+#if defined (UCSRC) and not defined (UCSR0C)
 #	define UCSR0C	UCSRC
 #endif
-#if defined (DOR) && not defined (DOR0)
+#if defined (DOR) and not defined (DOR0)
 #	define DOR0		DOR
 #endif
-#if defined (FE) && not defined (FE0)
+#if defined (FE) and not defined (FE0)
 #	define FE0		FE
 #endif
-#if defined (RXCIE) && not defined (RXCIE0)
+#if defined (RXCIE) and not defined (RXCIE0)
 #	define RXCIE0	RXCIE
 #endif
-#if defined (TXCIE) && not defined (TXCIE0)
+#if defined (TXCIE) and not defined (TXCIE0)
 #	define TXCIE0	TXCIE
 #endif
-#if defined (RXEN) && not defined (RXEN0)
+#if defined (RXEN) and not defined (RXEN0)
 #	define RXEN0	RXEN
 #endif
-#if defined (TXEN) && not defined (TXEN0)
+#if defined (TXEN) and not defined (TXEN0)
 #	define TXEN0	TXEN
 #endif
-#if defined (U2X) && not defined (U2X0)
+#if defined (U2X) and not defined (U2X0)
 #	define U2X0		U2X
 #endif
-#if defined (URSEL) && not defined (URSEL0)
+#if defined (URSEL) and not defined (URSEL0)
 #	define URSEL0	URSEL
 #endif
-#if defined (UCSZ0) && not defined (UCSZ00)
+#if defined (UCSZ0) and not defined (UCSZ00)
 #	define UCSZ00	UCSZ0
 #endif
-#if defined (UCSZ1) && not defined (UCSZ01)
+#if defined (UCSZ1) and not defined (UCSZ01)
 #	define UCSZ01	UCSZ1
 #endif
-#if defined (UDRIE) && not defined (UDRIE0)
+#if defined (UDRIE) and not defined (UDRIE0)
 #	define UDRIE0	UDRIE
 #endif
 
 // We want the interrupt names to be "USART0_RX_vect", "USART0_UDRE_vect" and
 // "USART0_TX_vect".
-#if defined(USART0_RX_vect)
-// everything is fine
-#elif defined(USART_RX_vect)
+#if defined(USART_RX_vect) and not defined(USART0_RX_vect)
 #	define USART0_RX_vect		USART_RX_vect
+#endif
+#if defined(USART_UDRE_vect) and not defined(USART0_UDRE_vect)
 #	define USART0_UDRE_vect		USART_UDRE_vect
+#endif
+#if defined(USART_TX_vect) and not defined(USART0_TX_vect)
 #	define USART0_TX_vect		USART_TX_vect
-#elif defined(USARTRXC_vect)
+#endif
+
+#if defined(USARTRXC_vect) and not defined(USART0_RX_vect)
 #	define USART0_RX_vect		USARTRXC_vect
+#endif
+#if defined(USARTUDRE_vect) and not defined(USART0_UDRE_vect)
 #	define USART0_UDRE_vect		USARTUDRE_vect
+#endif
+#if defined(USARTTXC_vect) and not defined(USART0_TX_vect)
 #	define USART0_TX_vect		USARTTXC_vect
-#elif defined(USART_RXC_vect)
+#endif
+
+#if defined(USART_RXC_vect) and not defined(USART0_RX_vect)
 #	define USART0_RX_vect		USART_RXC_vect
+#endif
+#if defined(USART_UDRE_vect) and not defined(USART0_UDRE_vect)
 #	define USART0_UDRE_vect		USART_UDRE_vect
+#endif
+#if defined(USART_TXC_vect) and not defined(USART0_TX_vect)
 #	define USART0_TX_vect		USART_TXC_vect
 #endif
 
