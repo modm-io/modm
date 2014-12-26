00ca8c328044df0c5ff19b79e81991e3aa338c7e
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-12-26 02:21:18 +0100

    Doxygen: Move 1-Wire interface into `peripheral` group.

--- src/xpcc/architecture/peripheral/one_wire.hpp
+++ src/xpcc/architecture/peripheral/one_wire.hpp
@@ -5,7 +5,7 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -28,16 +28,16 @@
  */
 // ----------------------------------------------------------------------------
 /**
- * \ingroup		connectivity
+ * \ingroup		peripheral
  * \defgroup	1-wire		1-Wire
- * 
+ *
  * 1-Wire is a device communications bus system designed by
  * Dallas Semiconductor Corp. that provides low-speed data, signaling and
  * power over a single signal.[1] 1-Wire is similar in concept to I²C, but
  * with lower data rates and longer range. It is typically used to communicate
  * with small inexpensive devices such as digital thermometers and
  * weather instruments.
- * 
+ *
  * One distinctive feature of the bus is the possibility to use only two
  * wires: data and ground. To accomplish this, 1-wire devices include an
  * 800 pF capacitor to store charge, and power the device during periods where
@@ -59,18 +59,18 @@ namespace xpcc
 	{
 		/**
 		 * \brief	ROM Commands
-		 * 
+		 *
 		 * After the bus master has detected a presence pulse, it can issue a
 		 * ROM command. These commands operate on the unique 64-bit ROM codes
 		 * of each slave device and allow the master to single out a specific
 		 * device if many are present on the 1-Wire bus.
-		 * 
+		 *
 		 * These commands also allow the master to determine how many and what
 		 * types of devices are present on the bus or if any device has
 		 * experienced an alarm condition.
-		 * 
+		 *
 		 * There are five ROM commands, and each command is 8 bits long.
-		 * 
+		 *
 		 * \ingroup	1-wire
 		 */
 		enum RomCommand
