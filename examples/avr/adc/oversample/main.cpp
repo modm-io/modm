a4eaf70e070d40a9e2e13bd7da139a5253a43646
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-01-12 00:32:42 +0100

    Examples: Adapt AVR oversample example.

--- examples/avr/adc/oversample/main.cpp
+++ examples/avr/adc/oversample/main.cpp
@@ -1,58 +1,63 @@
 
 #include <xpcc/architecture.hpp>
-#include <xpcc/io/iostream.hpp>
 #include <xpcc/processing.hpp>
 
 using namespace xpcc::atmega;
 
 // Create a new UART object
 typedef xpcc::avr::SystemClock clock;
-xpcc::atmega::Uart0 uart;
+
+#include <xpcc/io/iostream.hpp>
+// Create a IOStream for complex formatting tasks
+xpcc::IODeviceWrapper< Uart0, xpcc::IOBuffer::BlockIfFull > device;
+xpcc::IOStream output(device);
+
 
 // the three sensors are mapped: x = ch1, y = ch2, z = ch0
-uint8_t sensorMapping[3] = {1,2,0};
+Adc::Channel sensorMapping[3] = {1,2,0};
 // 3 channels and averages of 2^5 bits = 32 oversamples
-typedef xpcc::AnalogSensors< 3,5, AdcInterrupt > sensor;
+#include <xpcc/driver/adc/adc_sampler.hpp>
+typedef xpcc::AdcSampler< AdcInterrupt, 3, 5 > sensors;
 // the results are up to 16 bit wide
-uint16_t sensorData[3];
-xpcc::PeriodicTimer<> sensorTimer(100);
+sensors::DataType sensorData[3];
+
+xpcc::Timeout<> timeout(100);
 
 int
 main()
 {
     GpioOutputD1::connect(Uart0::Tx);
     GpioInputD0::connect(Uart0::Rx);
-    uart.initialize<clock, 115200>();
-
-	// Enable interrupts, this is needed for every buffered UART
-	sei();
-
-	// Create a IOStream for complex formatting tasks
-	xpcc::IODeviceWrapper< Uart0, xpcc::IOBuffer::BlockIfFull > device(uart);
-	xpcc::IOStream output(device);
-
-	output << "Welcome!" << xpcc::endl;
+	Uart0::initialize<clock, 115200>();
 
 	// Initialize the analog to digital converter
 	// With the AVR running at 14.7456Mhz and a prescaler of 128 the
 	// ADC is running at 115kHz.
 	Adc::initialize<clock, 115000>();
 	Adc::setReference(Adc::Reference::InternalVcc);
+	Adc::enableInterrupt();
 
-	sensor::initialize(sensorMapping, sensorData);
+	// Enable interrupts, this is needed for every buffered UART
+	enableInterrupts();
+
+	output << "Welcome!" << xpcc::endl;
+
+	sensors::initialize(sensorMapping, sensorData);
+	sensors::startReadout();
 
 	while (1)
 	{
-		if (sensorTimer.isExpired())
+		if (timeout.isExpired() && sensors::isReadoutFinished())
 		{
-			sensor::readSensors();
-		}
-
-		if (sensor::isNewDataAvailable())
-		{
-			uint16_t* data = sensor::readData();
+			uint16_t* data = sensors::getData();
 			// send it via UART
-			output <<"data[0]="  << data[0] << xpcc::endl;
+			output <<"x="  << data[0] << xpcc::endl;
+			output <<"y="  << data[1] << xpcc::endl;
+			output <<"z="  << data[2] << xpcc::endl;
+
+			// start another readout
+			sensors::startReadout();
+			timeout.restart(200);
 		}
 	}
 }
