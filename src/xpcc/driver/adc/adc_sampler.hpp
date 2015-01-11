ad706e650a8fbbfdf55e1a96a9c89ccf875dc8ed
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-01-12 00:32:12 +0100

    Driver: Move AnalogSensors class to AdcSampler and adapt for AdcInterrupt interface.

---
+++ src/xpcc/driver/adc/adc_sampler.hpp
@@ -0,0 +1,115 @@
+// coding: utf-8
+/* Copyright (c) 2011, Roboterclub Aachen e.V.
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
+ */
+// ----------------------------------------------------------------------------
+
+#ifndef XPCC_ADC_SAMPLER_HPP
+#define XPCC_ADC_SAMPLER_HPP
+
+#include <xpcc/architecture/interface/adc_interrupt.hpp>
+#include <xpcc/utils/template_metaprogramming.hpp>
+#include <xpcc/math/utils/misc.hpp>
+
+
+namespace xpcc
+{
+
+/**
+ * Generic ADC wrapper for (over-)sampling analog inputs.
+ *
+ * This template class stores 8, 16 or 32bit ADC values in a buffer and
+ * provides an easy interface for managing that data.
+ *
+ * The code will read the ADC channels you specify in an array.
+ * This means you can remap the ADC channels in the order that they
+ * should appear in the buffer.
+ * The array must obviously have as many elements as channels!
+ * For example, if channels 0,2,7 are connected, but the sensors
+ * are in the wrong order, just remap them with the array {7,0,2}.
+ *
+ * To make use of oversampling and averaging, set `Oversamples > 0`.
+ * This will loop `2^Oversamples` times through the ADC channels, adding
+ * the average of each result to the buffer.
+ *
+ * This class will choose the smallest data width for you.
+ * It decides this by adding the ADC resolution and the oversample size
+ * and choosing the next smallest size (not larger than 32bit though!).
+ *
+ * @warning	The averaging algorithm only works for unsigned ADC data!
+ *
+ * @tparam AdcInterrupt	a class implementing the @see AdcInterrupt interface
+ * @tparam Channels		number of ADC channels connected to sensor(s)
+ * @tparam Oversamples	`2^Oversamples` samples to average for each channel
+ *
+ * @ingroup adc
+ * @author	Niklas Hauser
+ */
+template < class AdcInterrupt, uint8_t Channels, uint8_t Oversamples=0 >
+class AdcSampler
+{
+	typedef typename AdcInterrupt::Channel Channel;
+
+	static constexpr uint32_t totalSamples = xpcc::pow(2, Oversamples) * Channels;
+	typedef typename xpcc::tmp::Select<
+			totalSamples <= xpcc::pow(2, 16),
+			uint16_t,
+			uint32_t >::Result SampleType;
+
+public:
+#ifdef __DOXYGEN__
+	typedef uint32_t DataType;
+#else
+	typedef typename xpcc::tmp::Select<
+			(AdcInterrupt::Resolution + Oversamples) <= 16,
+			typename xpcc::tmp::Select<
+					(AdcInterrupt::Resolution + Oversamples) <= 8,
+					uint8_t,
+					uint16_t >::Result,
+			uint32_t >::Result DataType;
+#endif
+
+public:
+	/**
+	 * @param mapping
+	 * 			array of length `Channels` containing the channel-to-data mapping
+	 * @param data
+	 * 			array of length `Channels` containing the results
+	 */
+	static void
+	initialize(const Channel* mapping, DataType* data);
+
+	/**
+	 * Starts the ADC readout routine and buffers the results
+	 * @return `false` when a readout is in progress, `true` otherwise
+	 */
+	static bool
+	startReadout();
+
+	static bool
+	isReadoutFinished();
+
+	/// @return pointer to first element of 16bit result array
+	static DataType*
+	getData();
+
+private:
+	static void
+	sampleAdc();
+
+	static const Channel* map;
+	static DataType *data;
+
+	static SampleType samples;
+	static uint8_t index;
+	static bool newData;
+};
+
+}	// namespace xpcc
+
+#include "adc_sampler_impl.hpp"
+
+#endif // XPCC_ADC_SAMPLER_HPP
---
+++ src/xpcc/driver/adc/adc_sampler_impl.hpp
@@ -0,0 +1,120 @@
+// coding: utf-8
+/* Copyright (c) 2011, Roboterclub Aachen e.V.
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
+ */
+// ----------------------------------------------------------------------------
+
+#ifndef XPCC_ADC_SAMPLER_HPP
+#	error 	"Don't include this file directly, use 'adc_sampler.hpp' instead!"
+#endif
+
+// ----------------------------------------------------------------------------
+template < class AdcInterrupt, uint8_t Channels, uint8_t Oversamples >
+const typename xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::Channel*
+xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::map;
+
+template < class AdcInterrupt, uint8_t Channels, uint8_t Oversamples >
+typename xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::DataType*
+xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::data;
+
+template < class AdcInterrupt, uint8_t Channels, uint8_t Oversamples >
+typename xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::SampleType
+xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::samples;
+
+template < class AdcInterrupt, uint8_t Channels, uint8_t Oversamples >
+uint8_t
+xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::index;
+
+template < class AdcInterrupt, uint8_t Channels, uint8_t Oversamples >
+bool
+xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::newData;
+
+// ----------------------------------------------------------------------------
+template < class AdcInterrupt, uint8_t Channels, uint8_t Oversamples >
+void
+xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::initialize(const Channel* mapping, DataType* data)
+{
+	map = mapping;
+	AdcSampler::data = data;
+	samples = 0;
+	index = 0;
+	newData = false;
+
+	AdcInterrupt::attachInterruptHandler(sampleAdc);
+}
+
+template < class AdcInterrupt, uint8_t Channels, uint8_t Oversamples >
+void
+xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::sampleAdc()
+{
+	if (Oversamples) {
+
+		// reset array to zero at the beginning of sampling
+		if (samples <= Channels) data[index] = 0;
+
+		data[index] += AdcInterrupt::getValue();
+
+		if (++samples <= totalSamples) {
+			// continue sampling on next index
+			if (++index >= Channels) index = 0;
+			AdcInterrupt::setChannel(map[index]);
+			AdcInterrupt::startConversion();
+		}
+		else {
+			// stop getting values and calculate the average of the 2^N samples
+			for (uint_fast8_t ii=0; ii < Channels; ++ii) {
+				// lazy divide: (value / 2^n) => (value >> n)
+				data[ii] >>= Oversamples;
+			}
+			samples = index = 0;
+			newData = true;
+		}
+
+	} else {
+
+		// just get the raw data
+		data[index] = AdcInterrupt::getValue();
+
+		if (++index < Channels) {
+			AdcInterrupt::setChannel(map[index]);
+			AdcInterrupt::startConversion();
+		} else {
+			index = 0;
+			newData = true;
+		}
+
+	}
+}
+
+template < class AdcInterrupt, uint8_t Channels, uint8_t Oversamples >
+bool
+xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::startReadout()
+{
+	if (samples > 0) return false;
+	newData = false;
+	index = 0;
+
+	AdcInterrupt::setChannel(map[0]);
+	AdcInterrupt::startConversion();
+
+	return true;
+}
+
+template < class AdcInterrupt, uint8_t Channels, uint8_t Oversamples >
+bool
+xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::isReadoutFinished()
+{
+	return newData;
+}
+
+template < class AdcInterrupt, uint8_t Channels, uint8_t Oversamples >
+typename xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::DataType*
+xpcc::AdcSampler<AdcInterrupt,Channels,Oversamples>::getData()
+{
+	return data;
+}
+
+
