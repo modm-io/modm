modm-io/modm

commit a8fda6608d3e1665b07fb974d3daaf5ee6c964d3

move generated_platform to the build folder
 * the new path is libxpcc/generated_platform
 * in addition libxpcc/src is added to include path, where platform.hpp is generated to.

 * the build pathes and include pathes are even more mixed, this should change with comming concept of library builder.
 * Crazy is also, that xpcc_buildpath is different while building different things.
 * since build path depends on generated platform, which depends on build path, the environment variables for pathes of generated platform cannot be set in generate(), but in platform_tools_generate(). We should concider use the variable mechanism in our tools, where the variables are substituded/resolved later while building. The whole path conversion things i think should be done in emmiters and so on. maybe our default_emmiter does all the work already.
 * additional clean step for generated_platform should not be needed. At least my test has shown, that all files, but *.lst have been deleted. Maybe we could think about fixing this later.
 * i've again added automaticall removement of old generated pathes. This should be removed later.

scons/site_tools/platform_tools.py
@@ -135,6 +135,14 @@ def platform_tools_find_device_file(env):
 # env['XPCC_PLATFORM_PATH'] is used for absolute paths
 # architecture_path for relative build paths
 def platform_tools_generate(env, architecture_path):
+# 	# Set some paths used by this file
+	env['XPCC_PLATFORM_GENERATED_PATH_OLD'] = \
+		os.path.join(env['XPCC_LIBRARY_PATH'], 'xpcc', 'architecture', 'generated_platform_' + env['XPCC_DEVICE'])
+ 		
+	env['XPCC_PLATFORM_GENERATED_PATH'] = \
+		os.path.join(env['XPCC_BUILDPATH'], 'generated_platform')
+	
+	
 	device = env['XPCC_DEVICE']
 
 	# Initialize Return Lists/Dicts
@@ -142,8 +150,16 @@ def platform_tools_generate(env, architecture_path):
 	defines = {}
 	# make paths
 	platform_path = os.path.join(architecture_path, 'platform')
-	generated_path = os.path.join(architecture_path, env['XPCC_PLATFORM_GENERATED_DIR'])
+	old_generated_path = env['XPCC_PLATFORM_GENERATED_PATH_OLD']
+	generated_path = env['XPCC_PLATFORM_GENERATED_PATH']
+	path_of_generated_xpcc_source = os.path.join(env['XPCC_BUILDPATH'], 'src')
 
+	
+	#remove the old platform. Delete these lines in a fiew days.
+	#remove also the XPCC_PLATFORM_GENERATED_PATH_OLD
+	if os.path.exists(old_generated_path):
+		Execute(Delete(old_generated_path))
+	
 	dev = env['XPCC_DEVICE_FILE']
 
 	# Parse Properties
@@ -209,15 +225,15 @@ def platform_tools_generate(env, architecture_path):
 	#remove the old platform.hpp. Delete these lines in a fiew days.
 	oldTarget = os.path.join(architecture_path, 'platform.hpp')
 	if os.path.exists(oldTarget):
-		os.remove(oldTarget)
+		Execute(Delete(oldTarget))
 	
 	src = os.path.join(platform_path, 'platform.hpp.in')
-	tar = os.path.join(generated_path, 'include_platform_hack', architecture_path, 'platform.hpp')
-	sub = {'include_path': '../../../drivers.hpp'}
+	tar = os.path.join(path_of_generated_xpcc_source, architecture_path, 'platform.hpp')
+	sub = {'include_path': '../../../generated_platform/drivers.hpp'}
 	env.Template(target = tar, source = src, substitutions = sub)
 	
 	#append and return additional CPPPATH
-	cppIncludes = [os.path.join(generated_path, 'include_platform_hack')]
+	cppIncludes = [path_of_generated_xpcc_source]
 	env.AppendUnique(CPPPATH = cppIncludes)
 
 	# Show SCons how to build the drivers.hpp.in file:
@@ -294,21 +310,11 @@ def filter_letter_to_num(letter):
 # -----------------------------------------------------------------------------
 ###################### Generate Platform Tools ################################
 def generate(env, **kw):
-	# Set some paths used by this file
-	env['XPCC_PLATFORM_PATH'] = \
-		os.path.join(env['XPCC_LIBRARY_PATH'], 'xpcc', 'architecture', 'platform')
-	env['XPCC_PLATFORM_GENERATED_DIR'] = 'generated_platform_' + env['XPCC_DEVICE']
-	env['XPCC_PLATFORM_GENERATED_PATH'] = \
-		os.path.join(env['XPCC_LIBRARY_PATH'], 'xpcc', 'architecture', env['XPCC_PLATFORM_GENERATED_DIR'])
+ 	env['XPCC_PLATFORM_PATH'] = \
+ 		os.path.join(env['XPCC_LIBRARY_PATH'], 'xpcc', 'architecture', 'platform')
 
 	# Create Parameter DB and parse User parameters
 	env['XPCC_PARAMETER_DB'] = ParameterDB(env['XPCC_USER_PARAMETERS'], env.GetLogger()).toDictionary()
-
-	# Remove Generated Folder if Clean Flag is set
-	# Scons does not remove the files on its own
-	if env.GetOption('clean'):
-		env.Info("Removing %s..." % env['XPCC_PLATFORM_GENERATED_PATH'])
-		Execute(Delete(env['XPCC_PLATFORM_GENERATED_PATH']))
 
 	# Add Method to Parse XML Files, and create Template / Copy Dependencies
 	env.AddMethod(platform_tools_generate, 'GeneratePlatform')

scons/site_tools/xpcc.py
@@ -120,9 +120,6 @@ def xpcc_library(env, buildpath=None):
 	# exclude the buildpath from the FileScanner
 	exclude_from_scanner(env['XPCC_BUILDPATH'])
 
-	# exclude the generated path from the FileScanner
-	exclude_from_scanner(env['XPCC_PLATFORM_GENERATED_PATH'])
-
 	# build the library
 	library, defines = env.SConscript(
 			os.path.join(env['XPCC_ROOTPATH'], 'src', 'SConscript'),

commit a71ec83c80a12ade131a5df0ed2bdb815e1d12e2

[scons] remove incorrect whitespaces introduced by a8fda6608d3e1665b07fb974d3daaf5ee6c964d3

scons/site_tools/platform_tools.py
@@ -138,11 +138,11 @@ def platform_tools_generate(env, architecture_path):
 # 	# Set some paths used by this file
 	env['XPCC_PLATFORM_GENERATED_PATH_OLD'] = \
 		os.path.join(env['XPCC_LIBRARY_PATH'], 'xpcc', 'architecture', 'generated_platform_' + env['XPCC_DEVICE'])
- 		
+
 	env['XPCC_PLATFORM_GENERATED_PATH'] = \
 		os.path.join(env['XPCC_BUILDPATH'], 'generated_platform')
-	
-	
+
+
 	device = env['XPCC_DEVICE']
 
 	# Initialize Return Lists/Dicts
@@ -152,12 +152,12 @@ def platform_tools_generate(env, architecture_path):
 	platform_path = os.path.join(architecture_path, 'platform')
 	old_generated_path = env['XPCC_PLATFORM_GENERATED_PATH_OLD']
 	generated_path = env['XPCC_PLATFORM_GENERATED_PATH']
-	
+
 	#remove the old platform. Delete these lines in a fiew days.
 	#remove also the XPCC_PLATFORM_GENERATED_PATH_OLD
 	if os.path.exists(old_generated_path):
 		Execute(Delete(old_generated_path))
-	
+
 	dev = env['XPCC_DEVICE_FILE']
 
 	# Parse Properties
@@ -308,8 +308,8 @@ def filter_letter_to_num(letter):
 # -----------------------------------------------------------------------------
 ###################### Generate Platform Tools ################################
 def generate(env, **kw):
- 	env['XPCC_PLATFORM_PATH'] = \
- 		os.path.join(env['XPCC_LIBRARY_PATH'], 'xpcc', 'architecture', 'platform')
+	env['XPCC_PLATFORM_PATH'] = \
+		os.path.join(env['XPCC_LIBRARY_PATH'], 'xpcc', 'architecture', 'platform')
 
 	# Create Parameter DB and parse User parameters
 	env['XPCC_PARAMETER_DB'] = ParameterDB(env['XPCC_USER_PARAMETERS'], env.GetLogger()).toDictionary()