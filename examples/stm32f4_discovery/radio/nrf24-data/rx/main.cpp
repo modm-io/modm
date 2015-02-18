e40850b02fa761c0e0bd7ade50374c62d418e903
strongly-typed <stronglytyp3d@gmail.com>
2015-02-18 12:00:00 +0100

    Adopt paths for NRF24 examples.

--- examples/stm32f4_discovery/radio/nrf24-data/rx/main.cpp
+++ examples/stm32f4_discovery/radio/nrf24-data/rx/main.cpp
@@ -1,5 +1,5 @@
 #include <xpcc/architecture/platform.hpp>
-#include "../../stm32f4_discovery.hpp"
+#include "../../../stm32f4_discovery.hpp"
 #include <xpcc/debug/logger.hpp>
 #include <xpcc/processing.hpp>
 
