1a5ee0924ab79828c90fe9ed88e7ba9809123d55
strongly-typed <stronglytyp3d@gmail.com>
2015-02-28 15:49:50 +0100

    App: CAN2USB: All LEDs blinks with 1/2 Hz

--- examples/avr/app_can2usb/main.cpp
+++ examples/avr/app_can2usb/main.cpp
@@ -7,22 +7,76 @@
  */
 // ----------------------------------------------------------------------------
 
+/**
+ * CAN2USB Hardware Revision 1.3
+ *
+ * An AT90CAN128 and FT245 based CAN sniffer.
+ *
+ * AT90CAN128 fuse settings:
+ *
+ * lfuse: 0xfe
+ * hfuse: 0xd1
+ * efuse: 0xfd
+ *
+ */
+
 #include <xpcc/architecture/platform.hpp>
 
 using namespace xpcc::at90;
 
-typedef GpioOutputD7 Led;
-// create a wrapper for a output pin
-typedef xpcc::GpioInverted< GpioOutputB1 > LedInverted;
+// LED1 Status
+typedef GpioOutputD7 LedStatus;
+
+// LED2 RX
+typedef GpioOutputG3 LedRxLow;
+typedef GpioOutputG4 LedRxHigh;
+
+// LED3 TX
+typedef GpioOutputB6 LedTxLow;
+typedef GpioOutputB7 LedTxHigh;
+
+// LED4
+typedef GpioOutputF7 Led4Low;
+typedef GpioOutputF4 Led4High;
+
+// LED5
+typedef GpioOutputF6 Led5Low;
+typedef GpioOutputF3 Led5High;
 
 MAIN_FUNCTION
 {
-	Led::setOutput();
-	Led::set();
+	LedStatus::setOutput();
+	LedStatus::set();
+
+	LedRxLow::setOutput();
+	LedRxHigh::setOutput();
+
+	LedTxLow::setOutput();
+	LedTxHigh::setOutput();
+
+	Led4Low::setOutput();
+	Led4High::setOutput();
+
+	Led5Low::setOutput();
+	Led5High::setOutput();
+
+	LedTxHigh::set();
+	LedRxLow::set();
+	Led4High::set();
+	Led5Low::set();
 
 	while (1)
 	{
 		xpcc::delayMilliseconds(1000);
-		Led::toggle();
+		LedStatus::toggle();
+		LedRxHigh::toggle();
+		LedRxLow::toggle();
+		LedTxHigh::toggle();
+		LedTxLow::toggle();
+		Led4Low::toggle();
+		Led4High::toggle();
+		Led5Low::toggle();
+		Led5High::toggle();
+
 	}
 }
