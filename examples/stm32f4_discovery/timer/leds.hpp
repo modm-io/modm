60763b922fd26f0462658388846f7518d8711ca2
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-04-24 13:56:23 +0200

    Examples: STM32F4: Timer Animation: Use lambda functions as callbacks.

--- examples/stm32f4_discovery/timer/leds.hpp
+++ examples/stm32f4_discovery/timer/leds.hpp
@@ -6,34 +6,25 @@
 
 xpcc::accessor::Flash<uint16_t> ledTable(xpcc::ui::table16_256);
 
-void
-setOrange(uint8_t brightness)
+// create the leds with these lambda callbacks
+xpcc::ui::Led orange([](uint8_t brightness)
 {
 	Timer4::setCompareValue(2, ledTable[brightness]);
-}
+});
 
-void
-setRed(uint8_t brightness)
+xpcc::ui::Led red([](uint8_t brightness)
 {
 	Timer4::setCompareValue(3, ledTable[brightness]);
-}
+});
 
-void
-setGreen(uint8_t brightness)
+xpcc::ui::Led green([](uint8_t brightness)
 {
 	Timer4::setCompareValue(1, ledTable[brightness]);
-}
+});
 
-void
-setBlue(uint8_t brightness)
+xpcc::ui::Led blue([](uint8_t brightness)
 {
 	Timer4::setCompareValue(4, ledTable[brightness]);
-}
-
-// create the leds with these callbacks
-xpcc::ui::Led orange(setOrange);
-xpcc::ui::Led red(setRed);
-xpcc::ui::Led green(setGreen);
-xpcc::ui::Led blue(setBlue);
+});
 
 #endif // LEDS_HPP
