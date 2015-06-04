bc26758a91a2453b10e6395268d0e836ecbc2be9
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-06-04 19:36:56 +0200

    LPC: Update Uart implementation for new interface.

---
+++ src/xpcc/architecture/platform/driver/uart/lpc/type_ids.hpp
@@ -0,0 +1,28 @@
+// coding: utf-8
+/* Copyright (c) 2015, Roboterclub Aachen e.V.
+* All Rights Reserved.
+*
+* The file is part of the xpcc library and is released under the 3-clause BSD
+* license. See the file `LICENSE` for the full license governing this code.
+*/
+// ----------------------------------------------------------------------------
+#ifndef XPCC_LPC_UART_TYPE_IDS_HPP
+#define XPCC_LPC_UART_TYPE_IDS_HPP
+
+namespace xpcc
+{
+
+namespace lpc
+{
+
+namespace TypeId
+{
+	typedef struct{} Uart1Rx;
+	typedef struct{} Uart1Tx;
+}
+
+}	// namespace lpc
+
+}	// namespace xpcc
+
+#endif // XPCC_LPC_UART_TYPE_IDS_HPP
