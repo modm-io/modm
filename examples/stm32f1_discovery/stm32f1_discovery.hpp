bb2bd9eb4fe2ca9e16348aab3489871f5c036bd1
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-06-20 03:45:28 +0200

    Example: Add STM32F1 discovery example.

---
+++ examples/stm32f1_discovery/stm32f1_discovery.hpp
@@ -0,0 +1,29 @@
+// coding: utf-8
+/* Copyright (c) 2013, Roboterclub Aachen e.V.
+* All Rights Reserved.
+*
+* The file is part of the xpcc library and is released under the 3-clause BSD
+* license. See the file `LICENSE` for the full license governing this code.
+*/
+// ----------------------------------------------------------------------------
+
+//
+// STM32F1DISCOVERY
+// Discovery kit for STM32 F1 series
+// www.st.com/stm32-discovery
+//
+
+#ifndef XPCC_STM32_F1_DISCOVERY_HPP
+#define XPCC_STM32_F1_DISCOVERY_HPP
+
+using namespace xpcc::stm32;
+
+typedef GpioOutputC9  LedLeft;
+typedef GpioOutputC8  LedRight;
+typedef GpioInputA0   Button;
+
+/// STM32F1 running at 72MHz generated from the external 8MHz clock
+/// supplied by the on-board st-link
+typedef SystemClock<Pll<ExternalClock<MHz8>, MHz72> > defaultSystemClock;
+
+#endif	// XPCC_STM32_F1_DISCOVERY_HPP
