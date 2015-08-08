9b74d79695086e14193accd70604424d4948b839
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-08-08 02:28:36 +0200

    Architecture: Linker: Move stack to Core-Coupled Memory. Add .ccm section with example.

--- examples/stm32f4_discovery/blink/main.cpp
+++ examples/stm32f4_discovery/blink/main.cpp
@@ -1,6 +1,8 @@
 #include <xpcc/architecture/platform.hpp>
 #include "../stm32f4_discovery.hpp"
 
+uint32_t buffer[100] __attribute__((section(".ccm"))) = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1400, 1300, 1200, 1100, 1000, 900, 800, 700, 600, 500, 400, 300, 200, 100};
+
 // ----------------------------------------------------------------------------
 MAIN_FUNCTION
 {
@@ -11,11 +13,15 @@ MAIN_FUNCTION
 	LedRed::setOutput(xpcc::Gpio::High);
 	LedBlue::setOutput(xpcc::Gpio::High);
 
+	int counter(0);
+
 	while (1)
 	{
 		LedBlue::toggle();
 		LedGreen::toggle();
-		xpcc::delayMilliseconds(500);
+		xpcc::delayMilliseconds(buffer[counter]);
+		counter++;
+		counter %= 29;
 	}
 
 	return 0;
