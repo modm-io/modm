d1ac6e44eb602f889fb26825a94aa54015b29f58
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-08-15 22:16:58 +0200

    Examples: STM32F429: Add Logger example.

---
+++ examples/stm32f429_discovery/logger/main.cpp
@@ -0,0 +1,40 @@
+#include "../stm32f429_discovery.hpp"
+#include <xpcc/debug/logger.hpp>
+
+// ----------------------------------------------------------------------------
+// Set the log level
+#undef	XPCC_LOG_LEVEL
+#define	XPCC_LOG_LEVEL xpcc::log::INFO
+
+// Create an IODeviceWrapper around the Uart Peripheral we want to use
+xpcc::IODeviceWrapper< Usart1, xpcc::IOBuffer::BlockIfFull > loggerDevice;
+
+// Set all four logger streams to use the UART
+xpcc::log::Logger xpcc::log::debug(loggerDevice);
+xpcc::log::Logger xpcc::log::info(loggerDevice);
+xpcc::log::Logger xpcc::log::warning(loggerDevice);
+xpcc::log::Logger xpcc::log::error(loggerDevice);
+
+// ----------------------------------------------------------------------------
+MAIN_FUNCTION
+{
+	Board::initialize();
+
+	// initialize Uart2 for XPCC_LOG_
+	GpioOutputA9::connect(Usart1::Tx);
+	GpioInputA10::connect(Usart1::Rx, Gpio::InputType::PullUp);
+	Usart1::initialize<Board::systemClock, 115200>(12);
+
+	// Use the logging streams to print some messages.
+	// Change XPCC_LOG_LEVEL above to enable or disable these messages
+	XPCC_LOG_DEBUG   << "debug"   << xpcc::endl;
+	XPCC_LOG_INFO    << "info"    << xpcc::endl;
+	XPCC_LOG_WARNING << "warning" << xpcc::endl;
+	XPCC_LOG_ERROR   << "error"   << xpcc::endl;
+
+	while (1)
+	{
+	}
+
+	return 0;
+}
