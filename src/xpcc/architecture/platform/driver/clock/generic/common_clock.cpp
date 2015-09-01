82fb12e34b0f31bcad4adbe2bbeaef01c6e71e1c
strongly-typed <stronglytyp3d@gmail.com>
2015-09-01 16:11:54 +0200

    Architecture: Clock: Fix clock error for XPCC__OS_NONE and gcc 5.2

--- src/xpcc/architecture/platform/driver/clock/generic/common_clock.cpp
+++ src/xpcc/architecture/platform/driver/clock/generic/common_clock.cpp
@@ -9,16 +9,20 @@
 
 #include "common_clock.hpp"
 
+#include <xpcc/architecture/detect.hpp>
+
 namespace xpcc
 {
 
 namespace clock
 {
 
+#if defined (XPCC__OS_NONE)
 uint32_t ATTRIBUTE_FASTDATA fcpu(MHz8);
 uint32_t ATTRIBUTE_FASTDATA fcpu_kHz(8000);
 uint16_t ATTRIBUTE_FASTDATA fcpu_MHz(8);
 uint16_t ATTRIBUTE_FASTDATA ns_per_loop asm("ns_per_loop") (125*3);
+#endif
 
 }
 
