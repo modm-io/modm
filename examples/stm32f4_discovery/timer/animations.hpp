d0c724dbb53f659180f6ed7b390966cf28b04b3a
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-09-26 21:33:05 +0100

    Examples: Beautify Animation code.

--- examples/stm32f4_discovery/timer/animations.hpp
+++ examples/stm32f4_discovery/timer/animations.hpp
@@ -5,32 +5,34 @@
 #include <xpcc/ui/animation.hpp>
 
 // apply some animations to the leds
-xpcc::ui::Pulse<uint8_t> pulse(red.animation);
-xpcc::ui::Indicator<uint8_t> indicator(blue.animation);
-xpcc::ui::Strobe<uint8_t> strobe(green.animation);
+xpcc::ui::Pulse<uint8_t> pulse(red);
+xpcc::ui::Indicator<uint8_t> indicator(blue);
+xpcc::ui::Strobe<uint8_t> strobe(green);
 // ----------------------------------------------------------------------------
 
 // Now for some custom animators
+using KeyFrame = xpcc::ui::KeyFrame<uint8_t>;
+using KeyFrameAnimation = xpcc::ui::KeyFrameAnimation<uint8_t>;
 
 // custom keyframes (time, value) for the orange led
-xpcc::ui::KeyFrame<uint8_t> frames[] =
+const KeyFrame frames[] =
 {
-	xpcc::ui::KeyFrame<uint8_t>(1000, 50),
-	xpcc::ui::KeyFrame<uint8_t>(900, 0),
-	xpcc::ui::KeyFrame<uint8_t>(800, 100),
-	xpcc::ui::KeyFrame<uint8_t>(700, 0),
-	xpcc::ui::KeyFrame<uint8_t>(600, 150),
-	xpcc::ui::KeyFrame<uint8_t>(500, 0),
-	xpcc::ui::KeyFrame<uint8_t>(400, 200),
-	xpcc::ui::KeyFrame<uint8_t>(300, 0),
-	xpcc::ui::KeyFrame<uint8_t>(200, 250),
-	xpcc::ui::KeyFrame<uint8_t>(100, 0),
+	KeyFrame(1000, 50),
+	KeyFrame(900, 0),
+	KeyFrame(800, 100),
+	KeyFrame(700, 0),
+	KeyFrame(600, 150),
+	KeyFrame(500, 0),
+	KeyFrame(400, 200),
+	KeyFrame(300, 0),
+	KeyFrame(200, 250),
+	KeyFrame(100, 0)
 };
 // create a new keyframe animator for the orange led
-xpcc::ui::KeyFrameAnimation<uint8_t> animator(frames, XPCC__ARRAY_SIZE(frames), orange.animation);
+KeyFrameAnimation animator(frames, orange);
 
 // animate the period of the red pulse (Aniception?)
-uint16_t period = 500;
+static uint16_t period = 500;
 xpcc::ui::Animation<uint16_t> periodAnimator(period);
 // wrap it in a pulse
 xpcc::ui::Pulse<uint16_t> pulsePeriod(periodAnimator);
