0f4ea63de8381d0649920581558b97172df6389d
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-12-08 22:50:59 +0000

    Example: Update STM32F7 blink and uart example.

--- examples/stm32f7_discovery/blink/main.cpp
+++ examples/stm32f7_discovery/blink/main.cpp
@@ -1,14 +1,29 @@
-#include <xpcc/architecture/platform.hpp>
 #include "../stm32f7_discovery.hpp"
+#include <xpcc/processing.hpp>
+
+using namespace Board;
 
 // ----------------------------------------------------------------------------
 MAIN_FUNCTION
 {
-//	defaultSystemClock::enable();
+	Board::initialize();
+	LedD13::setOutput(xpcc::Gpio::Low);
+
+	// Use the logging streams to print some messages.
+	// Change XPCC_LOG_LEVEL above to enable or disable these messages
+	XPCC_LOG_DEBUG   << "debug"   << xpcc::endl;
+	XPCC_LOG_INFO    << "info"    << xpcc::endl;
+	XPCC_LOG_WARNING << "warning" << xpcc::endl;
+	XPCC_LOG_ERROR   << "error"   << xpcc::endl;
+
+	uint32_t counter(0);
 
 	while (1)
 	{
-		xpcc::delayMilliseconds(500);
+		xpcc::delayMilliseconds(Button::read() ? 250 : 1000);
+		LedD13::toggle();
+
+		XPCC_LOG_INFO << "loop: " << counter++ << xpcc::endl;
 	}
 
 	return 0;
--- examples/stm32f7_discovery/stm32f7_discovery.hpp
+++ examples/stm32f7_discovery/stm32f7_discovery.hpp
@@ -16,13 +16,57 @@
 #ifndef XPCC_STM32_F7_DISCOVERY_HPP
 #define XPCC_STM32_F7_DISCOVERY_HPP
 
+#include <xpcc/architecture/platform.hpp>
+#include <xpcc/debug/logger.hpp>
+
 using namespace xpcc::stm32;
 
-typedef GpioOutputI1 Led;		// User LED 1 (Arduino D13)
+namespace Board
+{
+
+/// STM32F7 running at 16MHz from the internal 16MHz crystal
+// typedef SystemClock< InternalClock<MHz16> > systemClock;
+struct systemClock
+{
+	static constexpr uint32_t Frequency = 16000000;
+	static constexpr uint32_t Usart1 = 16000000;
+
+	static void inline enable()
+	{
+		xpcc::clock::fcpu     = 16000000;
+		xpcc::clock::fcpu_kHz = 16000;
+		xpcc::clock::fcpu_MHz = 16;
+		xpcc::clock::ns_per_loop = 1000/16;
+	}
+};
 
 typedef GpioInputI11 Button;	// User Button
 
-/// STM32F7 running at 200MHz generated from the external on-board 25MHz clock
-//typedef SystemClock<Pll<ExternalClock<MHz25>, MHz200> > defaultSystemClock;
+typedef GpioOutputI1 LedD13;	// User LED 1 (Arduino D13)
+
+namespace stlink
+{
+typedef GpioA9	Rx;
+typedef GpioB7	Tx;
+typedef Usart1 Uart;
+}
+
+inline void
+initialize()
+{
+	systemClock::enable();
+	xpcc::cortex::SysTickTimer::initialize<systemClock>();
+
+	stlink::Rx::connect(stlink::Uart::Tx);
+	stlink::Tx::connect(stlink::Uart::Rx, Gpio::InputType::PullUp);
+	stlink::Uart::initialize<systemClock, 115200>(12);
+
+	Button::setInput();
+	Button::setInputTrigger(Gpio::InputTrigger::RisingEdge);
+	Button::enableExternalInterrupt();
+//	Button::enableExternalInterruptVector(12);
+}
+
+}
 
 #endif	// XPCC_STM32_F7_DISCOVERY_HPP
