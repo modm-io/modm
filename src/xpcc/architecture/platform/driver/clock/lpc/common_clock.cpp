697c9dd995b8474e4f98ac8cac9471630673a4ad
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2016-02-13 00:31:34 +0000

    [platform]: Add LPC11 specific `fcpu` init.

---
+++ src/xpcc/architecture/platform/driver/clock/lpc/common_clock.cpp
@@ -0,0 +1,27 @@
+// coding: utf-8
+/* Copyright (c) 2013, Roboterclub Aachen e.V.
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
+ */
+// ----------------------------------------------------------------------------
+
+#include "../generic/common_clock.hpp"
+
+#include <xpcc/architecture/detect.hpp>
+
+namespace xpcc
+{
+
+namespace clock
+{
+
+uint32_t ATTRIBUTE_FASTDATA fcpu(MHz12);
+uint32_t ATTRIBUTE_FASTDATA fcpu_kHz(12000);
+uint16_t ATTRIBUTE_FASTDATA fcpu_MHz(12);
+uint16_t ATTRIBUTE_FASTDATA ns_per_loop asm("ns_per_loop") (4000/12);
+
+}
+
+}
