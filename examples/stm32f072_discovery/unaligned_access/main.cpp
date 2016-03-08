1a14fe197191611b60a1f8e4d1d10088d4f9c73a
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2016-03-08 22:08:30 +0100

    [examples] Add unaligned access example for F072.

---
+++ examples/stm32f072_discovery/unaligned_access/main.cpp
@@ -0,0 +1,75 @@
+#include <xpcc/architecture/platform.hpp>
+#include <xpcc/architecture/driver/accessor.hpp>
+#include <xpcc/container/smart_pointer.hpp>
+
+using namespace Board;
+
+uint8_t buffer[300] = {0};
+
+uint32_t input = 0x06050403;
+uint32_t output;
+
+void error()
+{
+	while(1)
+	{
+		LedUp::toggle();
+		xpcc::delayMilliseconds(100);
+	}
+}
+
+// ----------------------------------------------------------------------------
+int
+main()
+{
+	LedRight::setOutput(xpcc::Gpio::High);
+	LedLeft::setOutput();
+	LedUp::setOutput();
+	LedDown::setOutput();
+
+	uint8_t offset(0);
+
+	// if an unaligned access is trapped, the blue led will blink
+
+	while (1)
+	{
+		{
+			// "smart" pointer is created on the heap
+			xpcc::SmartPointer p(&input);
+
+			// manually interpreting the pointer
+			output = *reinterpret_cast<uint32_t*>(p.getPointer());
+			if (output != input) error();
+
+			// letting the class interpret it
+			output = p.get<uint32_t>();
+			if (output != input) error();
+		}
+
+		{
+			// `u32` is a pointer to a xpcc::unaligned_t<uint32_t> !
+			auto *u32 = xpcc::asUnaligned<uint32_t>(buffer + offset);
+			// this is short for:
+			// xpcc::unaligned_t<uint32_t> *u32 = reinterpret_cast<xpcc::unaligned_t<uint32_t>*>(buffer + offset);
+			// write to the unaligned location
+			*u32 = input;
+			// read from the unaligned location
+			output = *u32;
+			if (output != input) error();
+
+			// Anonymous form
+			*xpcc::asUnaligned<uint32_t>(buffer + offset) = input;
+			output = *xpcc::asUnaligned<uint32_t>(buffer + offset);
+			if (output != input) error();
+		}
+
+		input += 0x0125197;
+		offset += 1;
+
+		LedRight::toggle();
+		LedLeft::toggle();
+		xpcc::delayMilliseconds(Button::read() ? 250 : 500);
+	}
+
+	return 0;
+}
