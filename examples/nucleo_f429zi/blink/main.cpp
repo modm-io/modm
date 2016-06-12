25046824035ffcfb330c79f30a5ad15deb1058bb
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2016-06-12 17:52:56 +0100

    [examples] Add NUCLEO-F429ZI blinky example.

---
+++ examples/nucleo_f429zi/blink/main.cpp
@@ -0,0 +1,29 @@
+#include <xpcc/architecture/platform.hpp>
+
+using namespace Board;
+
+int
+main()
+{
+	Board::initialize();
+	Leds::setOutput();
+
+	// Use the logging streams to print some messages.
+	// Change XPCC_LOG_LEVEL above to enable or disable these messages
+	XPCC_LOG_DEBUG   << "debug"   << xpcc::endl;
+	XPCC_LOG_INFO    << "info"    << xpcc::endl;
+	XPCC_LOG_WARNING << "warning" << xpcc::endl;
+	XPCC_LOG_ERROR   << "error"   << xpcc::endl;
+
+	uint32_t counter(0);
+
+	while (1)
+	{
+		Leds::write(1 << (counter % Leds::width));
+		xpcc::delayMilliseconds(Button::read() ? 100 : 500);
+
+		XPCC_LOG_INFO << "loop: " << counter++ << xpcc::endl;
+	}
+
+	return 0;
+}
