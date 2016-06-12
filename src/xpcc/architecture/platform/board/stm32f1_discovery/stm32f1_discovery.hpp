26a160bcaedbd97534974d77e5225be76e63748b
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2016-06-12 19:22:39 +0100

    [boards] Replace STM32F1 Discovery SystemClock.

--- src/xpcc/architecture/platform/board/stm32f1_discovery/stm32f1_discovery.hpp
+++ src/xpcc/architecture/platform/board/stm32f1_discovery/stm32f1_discovery.hpp
@@ -24,9 +24,83 @@ using namespace xpcc::stm32;
 namespace Board
 {
 
-/// STM32F100 running at 24MHz generated from the external 8MHz crystal
+
 /// supplied by the on-board st-link
-using systemClock = SystemClock<Pll<ExternalCrystal<MHz8>, MHz24> >;
+/* SystemClock generator is only available for selected STM32F1 devices.
+ * The idea is that it is generated automatically for you like the rest of the
+ * HAL, however, xpcc does not have this capability yet. See PR #36.
+ */
+// using systemClock = SystemClock<Pll<ExternalCrystal<MHz8>, MHz24> >;
+
+// Instead this manual implementation of the system clock is used:
+/// STM32F100 running at 24MHz generated from the external 8MHz crystal
+struct systemClock {
+	static constexpr uint32_t Frequency = 24 * MHz1;
+	static constexpr uint32_t Ahb = Frequency;
+	static constexpr uint32_t Apb1 = Frequency;
+	static constexpr uint32_t Apb2 = Frequency;
+
+	static constexpr uint32_t Adc = Apb2;
+
+	static constexpr uint32_t Can1   = Apb1;
+	static constexpr uint32_t Can2   = Apb1;
+
+	static constexpr uint32_t Spi1   = Apb2;
+	static constexpr uint32_t Spi2   = Apb1;
+	static constexpr uint32_t Spi3   = Apb1;
+
+	static constexpr uint32_t Usart1 = Apb2;
+	static constexpr uint32_t Usart2 = Apb1;
+	static constexpr uint32_t Usart3 = Apb1;
+	static constexpr uint32_t Uart4  = Apb1;
+	static constexpr uint32_t Uart5  = Apb1;
+
+	static constexpr uint32_t I2c1   = Apb1;
+	static constexpr uint32_t I2c2   = Apb1;
+
+	static constexpr uint32_t Apb1Timer = Apb1 * 1;
+	static constexpr uint32_t Apb2Timer = Apb2 * 1;
+	static constexpr uint32_t Timer1  = Apb2Timer;
+	static constexpr uint32_t Timer2  = Apb1Timer;
+	static constexpr uint32_t Timer3  = Apb1Timer;
+	static constexpr uint32_t Timer4  = Apb1Timer;
+	static constexpr uint32_t Timer5  = Apb1Timer;
+	static constexpr uint32_t Timer6  = Apb1Timer;
+	static constexpr uint32_t Timer7  = Apb1Timer;
+	static constexpr uint32_t Timer9  = Apb2Timer;
+	static constexpr uint32_t Timer12 = Apb1Timer;
+	static constexpr uint32_t Timer13 = Apb1Timer;
+	static constexpr uint32_t Timer14 = Apb1Timer;
+	static constexpr uint32_t Timer15 = Apb2Timer;
+	static constexpr uint32_t Timer16 = Apb2Timer;
+	static constexpr uint32_t Timer17 = Apb2Timer;
+
+	static bool inline
+	enable()
+	{
+		ClockControl::enableExternalCrystal();	// 8MHz
+		ClockControl::enablePll(
+			ClockControl::PllSource::ExternalCrystal,
+			3,
+			1
+		);
+		// set flash latency for 24MHz
+		ClockControl::setFlashLatency(Frequency);
+		// switch system clock to PLL output
+		ClockControl::enableSystemClock(ClockControl::SystemClockSource::Pll);
+		ClockControl::setAhbPrescaler(ClockControl::AhbPrescaler::Div1);
+		// APB1 has max. 24MHz
+		ClockControl::setApb1Prescaler(ClockControl::Apb1Prescaler::Div1);
+		ClockControl::setApb2Prescaler(ClockControl::Apb2Prescaler::Div1);
+		// update frequencies for busy-wait delay functions
+		xpcc::clock::fcpu     = Frequency;
+		xpcc::clock::fcpu_kHz = Frequency / 1000;
+		xpcc::clock::fcpu_MHz = Frequency / 1000000;
+		xpcc::clock::ns_per_loop = ::round(3000 / (Frequency / 1000000));
+
+		return true;
+	}
+};
 
 
 using Button = GpioInputA0;	// Blue PushButton
