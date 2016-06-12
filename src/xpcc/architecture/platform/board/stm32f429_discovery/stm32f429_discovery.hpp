f43d1ebc75bf4518f5c3f86383270fdc72eb9b30
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2016-06-12 18:07:21 +0100

    [boards] Let STM32F429 run at 180MHz.

--- src/xpcc/architecture/platform/board/stm32f429_discovery/stm32f429_discovery.hpp
+++ src/xpcc/architecture/platform/board/stm32f429_discovery/stm32f429_discovery.hpp
@@ -23,9 +23,79 @@ using namespace xpcc::stm32;
 namespace Board
 {
 
-/// STM32F429 running at 168MHz (USB Clock qt 48MHz) generated from the
-/// external on-board 8MHz crystal
-using systemClock = SystemClock<Pll<ExternalCrystal<MHz8>, MHz168, MHz48> >;
+/// STM32F429 running at 180MHz from the external 8MHz crystal
+struct systemClock
+{
+	static constexpr uint32_t Frequency = MHz180;
+	static constexpr uint32_t Apb1 = Frequency / 4;
+	static constexpr uint32_t Apb2 = Frequency / 2;
+
+	static constexpr uint32_t Adc = Apb2;
+
+	static constexpr uint32_t Spi1 = Apb2;
+	static constexpr uint32_t Spi2 = Apb1;
+	static constexpr uint32_t Spi3 = Apb1;
+	static constexpr uint32_t Spi4 = Apb2;
+	static constexpr uint32_t Spi5 = Apb2;
+	static constexpr uint32_t Spi6 = Apb2;
+
+	static constexpr uint32_t Usart1 = Apb2;
+	static constexpr uint32_t Usart2 = Apb1;
+	static constexpr uint32_t Usart3 = Apb1;
+	static constexpr uint32_t Uart4  = Apb1;
+	static constexpr uint32_t Uart5  = Apb1;
+	static constexpr uint32_t Usart6 = Apb2;
+	static constexpr uint32_t Uart7  = Apb1;
+	static constexpr uint32_t Uart8  = Apb1;
+
+	static constexpr uint32_t Can1 = Apb1;
+	static constexpr uint32_t Can2 = Apb1;
+
+	static constexpr uint32_t I2c1 = Apb1;
+	static constexpr uint32_t I2c2 = Apb1;
+	static constexpr uint32_t I2c3 = Apb1;
+	static constexpr uint32_t I2c4 = Apb1;
+
+	static constexpr uint32_t Apb1Timer = Apb1 * 2;
+	static constexpr uint32_t Apb2Timer = Apb2 * 2;
+	static constexpr uint32_t Timer1  = Apb2Timer;
+	static constexpr uint32_t Timer2  = Apb1Timer;
+	static constexpr uint32_t Timer3  = Apb1Timer;
+	static constexpr uint32_t Timer4  = Apb1Timer;
+	static constexpr uint32_t Timer5  = Apb1Timer;
+	static constexpr uint32_t Timer6  = Apb1Timer;
+	static constexpr uint32_t Timer7  = Apb1Timer;
+	static constexpr uint32_t Timer8  = Apb2Timer;
+	static constexpr uint32_t Timer10 = Apb2Timer;
+	static constexpr uint32_t Timer11 = Apb2Timer;
+	static constexpr uint32_t Timer12 = Apb1Timer;
+	static constexpr uint32_t Timer13 = Apb1Timer;
+	static constexpr uint32_t Timer14 = Apb1Timer;
+
+	static bool inline
+	enable()
+	{
+		ClockControl::enableExternalCrystal(); // 8 MHz
+		ClockControl::enablePll(
+			ClockControl::PllSource::ExternalCrystal,
+			4,      // 8MHz / N=4 -> 2MHz
+			180,    // 2MHz * M=180 -> 360MHz
+			2,      // 360MHz / P=2 -> 180MHz = F_cpu
+			7       // 360MHz / Q=7 -> ~51MHz = F_usb => bad for USB
+		);
+		ClockControl::setFlashLatency(Frequency);
+		ClockControl::enableSystemClock(ClockControl::SystemClockSource::Pll);
+		ClockControl::setApb1Prescaler(ClockControl::Apb1Prescaler::Div4);
+		ClockControl::setApb2Prescaler(ClockControl::Apb2Prescaler::Div2);
+		// update clock frequencies
+		xpcc::clock::fcpu     = Frequency;
+		xpcc::clock::fcpu_kHz = Frequency / 1000;
+		xpcc::clock::fcpu_MHz = Frequency / 1000000;
+		xpcc::clock::ns_per_loop = ::round(3000 / (Frequency / 1000000));
+
+		return true;
+	}
+};
 
 
 using Button = GpioInputA0;
@@ -185,7 +255,7 @@ initializeL3g()
 	l3g::Mosi::connect(l3g::SpiMaster::Mosi);
 	l3g::Miso::connect(l3g::SpiMaster::Miso);
 
-	l3g::SpiMaster::initialize<systemClock, MHz10>();
+	l3g::SpiMaster::initialize<systemClock, 11250000ul>();
 	l3g::SpiMaster::setDataMode(l3g::SpiMaster::DataMode::Mode3);
 }
 
