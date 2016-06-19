73d5d2ab267a72ec2a7e6e90c215a536ee53614b
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2016-06-20 00:36:18 +0100

    [examples] Update TLSF example for F469 Discovery.

--- examples/stm32f469_discovery/tlsf-allocator/main.cpp
+++ examples/stm32f469_discovery/tlsf-allocator/main.cpp
@@ -1,46 +1,32 @@
 #include <xpcc/architecture/platform.hpp>
-#include <tlsf.h>
-
-extern tlsf_t tlsf_heap0;
-extern uint32_t __heap1_start;
 
 int main()
 {
 	Board::initialize();
 	Board::Leds::setOutput();
 
-	// 200 pointers to allocated memories
-	void *d[200];
+	// 600 pointers to allocated memories
+	uint8_t *d[600];
 	int free_ii = 0;
 
-	for (int ii=0; ii < 200; ii++)
+	for (int ii=0; ii < 600; ii++)
 	{
 		d[ii] = nullptr;
-		uint32_t size = rand() % (1024*20);
+		uint32_t size = rand() % (1024 * 100);
 
-		if ((rand() % 100) >= 75) {
-			d[ii] = tlsf_malloc(tlsf_heap0, size);
-			// explicit allocation in CCM
-			XPCC_LOG_INFO << ".heap0  ";
-		} else {
-			d[ii] = malloc(size);
-			if (d[ii] && d[ii] < &__heap1_start) {
-				// allocation on SRAM has failed and fallback uses CCM!
-				XPCC_LOG_INFO << ".heap0FB";
-			}
-			else XPCC_LOG_INFO << ".heap123";
-		}
+		d[ii] = new (xpcc::MemoryFastData) uint8_t[size];
 
 		// print what size we requested and if it succeeded
-		XPCC_LOG_INFO.printf(" malloc(%2ukB) = ", size/1024);
+		XPCC_LOG_INFO.printf(" malloc(%3ukB) = ", size/1024);
 		if (d[ii]) XPCC_LOG_INFO << d[ii];
 		else   XPCC_LOG_INFO << "NO MEMORY ";
 
 		if ((rand() % 100) >= 50) {
 			// only some memory is returned to the system
-			void* df = d[free_ii++];
+			uint8_t *df;
+			while ((df = d[free_ii++]) == nullptr and free_ii <= ii) ;
 			XPCC_LOG_INFO << "  ...freeing " << df;
-			free(df);
+			delete df;
 		}
 		XPCC_LOG_INFO << xpcc::endl;
 	}
