b3278777868a4549863ddbefc1d9e31484e7d8f2
Raphael Lehmann <raphael@rleh.de>
2016-08-24 22:42:56 +0200

    [examples] STM32F4 Discovery: Improve SPI example.

--- examples/stm32f4_discovery/spi/main.cpp
+++ examples/stm32f4_discovery/spi/main.cpp
@@ -1,22 +1,33 @@
 #include <xpcc/architecture/platform.hpp>
-#include <xpcc/architecture/platform.hpp>
 
+// If you use a different SPI instance, you may have to also choose different
+// GPIOs to connect to.
+using Sck = GpioOutputB13;
+using Mosi = GpioOutputB15;
+using Miso = GpioInputB14;
+using SpiMaster = SpiMaster2;
+// Note that you can also use a bit-banged SPI driver as a drop-in replacement
+// using SpiMaster = xpcc::SoftwareSpiMaster<Sck, Mosi, Miso>;
 
 int
 main()
 {
 	Board::initialize();
 
-	// Enable SPI 2
-	GpioOutputB12::connect(SpiMaster2::Nss);
-	GpioOutputB15::connect(SpiMaster2::Mosi);
-	GpioInputB14::connect(SpiMaster2::Miso);
-	GpioOutputB13::connect(SpiMaster2::Sck);
-	SpiMaster2::initialize<Board::systemClock, MHz20>();
+	// Connect the GPIOs to the SPIs alternate function
+	Sck::connect(SpiMaster::Sck);
+	Mosi::connect(SpiMaster::Mosi);
+	Miso::connect(SpiMaster::Miso);
+	// Initialize the SPI with a 20MHz clock
+	SpiMaster::initialize<Board::systemClock, MHz20>();
 
+	uint8_t data = 0;
 	while (1)
 	{
-		SpiMaster1::transferBlocking(0xF0);
+		// Connect Mosi to Miso to create a loopback
+		data = SpiMaster::transferBlocking(data);
+		data++;
+		// This will be spamming the bus
 	}
 
 	return 0;
