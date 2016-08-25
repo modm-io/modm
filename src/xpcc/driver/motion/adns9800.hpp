e0137371f2cfa8ee85cebbefc26fe2252389e9e3
strongly-typed <stronglytyp3d@gmail.com>
2016-08-25 21:19:24 +0200

    [driver] ADNS9800 optical motion sensor.

---
+++ src/xpcc/driver/motion/adns9800.hpp
@@ -0,0 +1,104 @@
+// coding: utf-8
+/* Copyright (c) 2016, Roboterclub Aachen e. V.
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
+ */
+// ----------------------------------------------------------------------------
+
+/**
+ * Based on work of Alexander Entinger, MSc / LXRobotics
+ * Based on https://github.com/mrjohnk/ADNS-9800
+ */
+
+#ifndef XPCC_ADNS9800_HPP
+#define XPCC_ADNS9800_HPP
+
+namespace xpcc
+{
+
+struct adns9800
+{
+public:
+	/// The addresses of the Configuration and Data Registers
+	enum class
+	Register : uint8_t
+	{
+		Product_ID                   = 0x00,
+		Revision_ID                  = 0x01,
+		Motion                       = 0x02,
+		Delta_X_L                    = 0x03,
+		Delta_X_H                    = 0x04,
+		Delta_Y_L                    = 0x05,
+		Delta_Y_H                    = 0x06,
+		SQUAL                        = 0x07,
+		Pixel_Sum                    = 0x08,
+		Maximum_Pixel                = 0x09,
+		Minimum_Pixel                = 0x0a,
+		Shutter_Lower                = 0x0b,
+		Shutter_Upper                = 0x0c,
+		Frame_Period_Lower           = 0x0d,
+		Frame_Period_Upper           = 0x0e,
+		Configuration_I              = 0x0f,
+		Configuration_II             = 0x10,
+		Frame_Capture                = 0x12,
+		SROM_Enable                  = 0x13,
+		Run_Downshift                = 0x14,
+		Rest1_Rate                   = 0x15,
+		Rest1_Downshift              = 0x16,
+		Rest2_Rate                   = 0x17,
+		Rest2_Downshift              = 0x18,
+		Rest3_Rate                   = 0x19,
+		Frame_Period_Max_Bound_Lower = 0x1a,
+		Frame_Period_Max_Bound_Upper = 0x1b,
+		Frame_Period_Min_Bound_Lower = 0x1c,
+		Frame_Period_Min_Bound_Upper = 0x1d,
+		Shutter_Max_Bound_Lower      = 0x1e,
+		Shutter_Max_Bound_Upper      = 0x1f,
+		LASER_CTRL0                  = 0x20,
+		Observation                  = 0x24,
+		Data_Out_Lower               = 0x25,
+		Data_Out_Upper               = 0x26,
+		SROM_ID                      = 0x2a,
+		Lift_Detection_Thr           = 0x2e,
+		Configuration_V              = 0x2f,
+		Configuration_IV             = 0x39,
+		Power_Up_Reset               = 0x3a,
+		Shutdown                     = 0x3b,
+		Inverse_Product_ID           = 0x3f,
+		Motion_Burst                 = 0x50,
+		SROM_Load_Burst              = 0x62,
+		Pixel_Burst                  = 0x64,
+	};
+};
+
+template < typename Spi, typename Cs >
+class Adns9800 : public adns9800
+{
+public:
+	static bool
+	initialise();
+
+	static bool
+	isNewMotionDataAvailable();
+
+	static void
+	getDeltaXY(int16_t &delta_x, int16_t &delta_y);
+
+protected:       
+	static uint8_t
+	readReg(Register const reg);
+
+	static void
+	writeReg(Register const reg, uint8_t const data);
+
+	static void
+	uploadFirmware();
+};
+
+} // xpcc namespace
+
+#include "adns9800_impl.hpp"
+
+#endif // XPCC__ADNS9800_HPP
---
+++ src/xpcc/driver/motion/adns9800_impl.hpp
@@ -0,0 +1,218 @@
+// coding: utf-8
+/* Copyright (c) 2016, Roboterclub Aachen e. V.
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
+ */
+// ----------------------------------------------------------------------------
+
+/**
+ * Based on work of Alexander Entinger, MSc / LXRobotics
+ * Based on https://github.com/mrjohnk/ADNS-9800
+ */
+
+#ifndef XPCC_ADNS9800_HPP
+#error  "Don't include this file directly, use 'adns9800.hpp' instead!"
+#endif
+
+#include <xpcc/debug/logger.hpp>
+
+#include "adns9800.hpp"
+
+// #include "adns9800_srom_a4.hpp"
+// #include "adns9800_srom_a4b.hpp"
+// #include "adns9800_srom_a5.hpp"
+#include "adns9800_srom_a6.hpp"
+
+// Set the log level
+#undef  XPCC_LOG_LEVEL
+#define XPCC_LOG_LEVEL xpcc::log::DEBUG
+
+template < typename Spi, typename Cs >
+bool
+xpcc::Adns9800< Spi, Cs>::isNewMotionDataAvailable()
+{
+	uint8_t const motion_reg = readReg(Register::Motion);
+	bool const new_data_available = (motion_reg & 0x80) > 0;
+	return new_data_available;
+}
+
+template < typename Spi, typename Cs >
+void
+xpcc::Adns9800< Spi, Cs>::getDeltaXY(int16_t &delta_x, int16_t &delta_y)
+{
+	Cs::reset();
+	xpcc::delayMicroseconds(100); // tSRAD
+
+	Spi::transferBlocking(static_cast<uint8_t>(Register::Motion_Burst));
+
+	// Delay tSRAD
+	xpcc::delayMicroseconds(100);
+
+	static constexpr uint8_t buf_size = 6;
+	uint8_t tx_buf[buf_size];
+	uint8_t rx_buf[buf_size];
+
+	Spi::transferBlocking(tx_buf, rx_buf, buf_size);
+
+	xpcc::delayNanoseconds(120); // tSCLK-NCS for read operation is 120ns
+	Cs::set();
+
+	uint8_t delta_x_l = rx_buf[2];
+	uint8_t delta_x_h = rx_buf[3];
+	uint8_t delta_y_l = rx_buf[4];
+	uint8_t delta_y_h = rx_buf[5];
+
+	delta_x = delta_x_h << 8 | delta_x_l;
+	delta_y = delta_y_h << 8 | delta_y_l;
+}
+
+template < typename Spi, typename Cs >
+uint8_t
+xpcc::Adns9800< Spi, Cs>::readReg(Register const reg)
+{
+	Cs::reset();
+
+	uint8_t address = static_cast<uint8_t>(reg);
+
+	// send adress of the register, with MSBit = 0 to indicate it's a read
+	Spi::transferBlocking(address & 0x7f);
+	xpcc::delayMicroseconds(100); // tSRAD
+	
+	// read data
+	uint8_t data = Spi::transferBlocking(0);
+
+	xpcc::delayNanoseconds(120); // tSCLK-NCS for read operation is 120ns
+	Cs::set();
+	xpcc::delayMicroseconds(19); // tSRW/tSRR (=20us) minus tSCLK-NCS
+
+	return data; 
+}
+
+template < typename Spi, typename Cs >
+void
+xpcc::Adns9800< Spi, Cs>::writeReg(Register const reg, uint8_t const data)
+{
+	Cs::reset();
+
+	uint8_t address = static_cast<uint8_t>(reg);
+
+	//send adress of the register, with MSBit = 1 to indicate it's a write
+	Spi::transferBlocking(address | 0x80);
+
+	//send data
+	Spi::transferBlocking(data);
+
+	xpcc::delayMicroseconds(20); // tSCLK-NCS for write operation
+	Cs::set();
+	xpcc::delayMicroseconds(100); // tSWW/tSWR (=120us) minus tSCLK-NCS. Could be shortened, but is looks like a safe lower bound 
+}
+
+template < typename Spi, typename Cs >
+void
+xpcc::Adns9800< Spi, Cs >::uploadFirmware()
+{
+	// set the configuration_IV register in 3k firmware mode
+	writeReg(Register::Configuration_IV, 0x02); // bit 1 = 1 for 3k mode, other bits are reserved 
+
+	// write 0x1d in SROM_enable reg for initializing
+	writeReg(Register::SROM_Enable, 0x1d); 
+
+	// wait for more than one frame period
+	xpcc::delayMilliseconds(10); // assume that the frame rate is as low as 100fps... even if it should never be that low
+
+	// write 0x18 to SROM_enable to start SROM download
+	writeReg(Register::SROM_Enable, 0x18); 
+
+	// write the SROM file (=firmware data) 
+	Cs::reset();
+
+	// write burst destination address
+	uint8_t address = static_cast<uint8_t>(Register::SROM_Load_Burst) | 0x80;
+	Spi::transferBlocking(address);
+	xpcc::delayMicroseconds(15);
+
+	// send all bytes of the firmware
+	for(int ii = 0; ii < firmware_length; ++ii)
+	{ 
+		Spi::transferBlocking(firmware_data[ii]);
+		xpcc::delayMicroseconds(15);
+	}
+
+	Cs::set(); 
+}
+
+template < typename Spi, typename Cs >
+bool
+xpcc::Adns9800< Spi, Cs >::initialise()
+{
+	bool success = true;
+
+	// ensure that the serial port is reset
+	Cs::set(); 
+	Cs::reset();
+	Cs::set();
+
+	writeReg(Register::Power_Up_Reset, 0x5a); // force reset
+	xpcc::delayMilliseconds(50); // wait for it to reboot
+
+	// Read Product ID
+	uint8_t id = readReg(Register::Product_ID);
+	static constexpr uint8_t id_expected = 0x33;
+	if (id != id_expected)
+	{
+		XPCC_LOG_ERROR.printf("Product Id = %02x. Expected %02x\n", id, id_expected);
+		success = false;
+	}
+
+	static constexpr uint8_t rev_expected = 0x03;
+	id = readReg(Register::Revision_ID);
+	if (id != rev_expected)
+	{
+		XPCC_LOG_DEBUG.printf("Revision Id = %02x. Expected %02x\n", id, rev_expected);
+		success = false;
+	}
+
+	id = readReg(Register::Inverse_Product_ID);
+	if (id != ~id_expected)
+	{
+		XPCC_LOG_DEBUG.printf("Inverse Product Id = %02x. Expected %02x\n", id, ~id_expected);
+		success = false;
+	}
+
+	// read registers 0x02 to 0x06 (and discard the data)
+	readReg(Register::Motion);
+	readReg(Register::Delta_X_L);
+	readReg(Register::Delta_X_H);
+	readReg(Register::Delta_Y_L);
+	readReg(Register::Delta_Y_H);
+
+	uploadFirmware();
+	xpcc::delayMilliseconds(10);
+
+	// enable laser(bit 0 = 0b), in normal mode (bits 3,2,1 = 000b)
+	// reading the actual value of the register is important because the real
+	// default value is different from what is said in the datasheet, and if you
+	// change the reserved bytes (like by writing 0x00...) it would not work.
+
+	// Laser always on
+	// Do not read Motion
+
+	uint8_t laser_ctrl0 = readReg(Register::LASER_CTRL0);
+	writeReg(Register::LASER_CTRL0, (laser_ctrl0 & 0xf0) | (0b0100) );
+
+	// Configure for robotics
+	// 8200 cpi
+	writeReg(Register::Configuration_I, 0xa4);
+
+	// Fixed frame rate
+	writeReg(Register::Configuration_II, 0b00001000);
+
+	writeReg(Register::Frame_Period_Max_Bound_Lower, 0x20);
+	writeReg(Register::Frame_Period_Max_Bound_Upper, 0x1b);
+
+	xpcc::delayMilliseconds(1);
+
+	return success;
+}
