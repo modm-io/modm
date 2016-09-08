ca611b6503ce4ae84cc6d645e675b7c6cc5b6306
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2016-09-08 02:59:17 +0100

    [cortex] Add assertion handler implementation.

---
+++ src/xpcc/architecture/platform/driver/core/cortex/assert.cpp
@@ -0,0 +1,50 @@
+// coding: utf-8
+/* Copyright (c) 2016, Roboterclub Aachen e.V.
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
+ */
+// ----------------------------------------------------------------------------
+
+#include <xpcc/architecture/interface/assert.hpp>
+
+extern "C" void exit(int);
+
+using xpcc::AssertionHandler;
+using xpcc::Abandonment;
+
+extern AssertionHandler __assertion_table_start;
+extern AssertionHandler __assertion_table_end;
+
+extern "C"
+{
+
+void xpcc_assert_evaluate(bool condition, const char * identifier)
+{
+	if (!condition)
+	{
+		uint8_t state(uint8_t(Abandonment::DontCare));
+		const char * module = identifier;
+		const char * location = module + strlen(module) + 1;
+		const char * failure = location + strlen(location) + 1;
+
+		AssertionHandler * handler = &__assertion_table_start;
+		for (; handler < &__assertion_table_end; handler++)
+		{
+			state |= (uint8_t) (*handler)(module, location, failure);
+		}
+
+		if (state == (uint8_t) Abandonment::DontCare or
+			state & (uint8_t) Abandonment::Fail)
+		{
+			xpcc_abandon(module, location, failure);
+			exit(1);
+		}
+	}
+}
+
+void xpcc_abandon(const char *, const char *, const char *) __attribute__((weak));
+void xpcc_abandon(const char *, const char *, const char *) {}
+
+}
