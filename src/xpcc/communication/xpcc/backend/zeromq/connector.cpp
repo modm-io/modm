e9ddd63a7dd79f25a2e930f3d3911a04d054d053
Christopher Durand <christopher.durand@rwth-aachen.de>
2017-01-22 19:29:47 +0100

    [XPCC] Move implementation of ZeroMQConnector to cpp file

---
+++ src/xpcc/communication/xpcc/backend/zeromq/connector.cpp
@@ -0,0 +1,122 @@
+// coding: utf-8
+/* Copyright (c) 2017, Roboterclub Aachen e. V.
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
+ */
+// ----------------------------------------------------------------------------
+
+#include "connector.hpp"
+
+namespace xpcc
+{
+
+ZeroMQConnector::ZeroMQConnector(std::string endpointIn, std::string endpointOut, Mode mode) :
+	socketIn (context, (mode == Mode::SubPush ? zmqpp::socket_type::sub  : zmqpp::socket_type::pull)),
+	socketOut(context, (mode == Mode::SubPush ? zmqpp::socket_type::push : zmqpp::socket_type::pub)),
+	reader(socketIn)
+{
+	switch(mode)
+	{
+		case
+		Mode::SubPush:
+			this->socketIn.connect(endpointIn);
+			this->socketIn.subscribe("");
+
+			this->socketOut.connect(endpointOut);
+		break;
+
+		case
+		Mode::PubPull:
+			this->socketIn.bind(endpointIn);
+			this->socketOut.bind(endpointOut);
+		break;
+	}
+
+	this->reader.start();
+}
+
+// ----------------------------------------------------------------------------
+ZeroMQConnector::~ZeroMQConnector()
+{
+	this->reader.stop();
+	this->socketIn.unsubscribe("");
+	this->socketOut.unsubscribe("");
+	this->socketIn.close();
+	this->socketOut.close();
+}
+
+// ----------------------------------------------------------------------------
+void
+ZeroMQConnector::sendPacket(const Header &header, SmartPointer payload)
+{
+	// Publish the reassembled message with zeromq
+	zmqpp::message message;
+
+	// Manual serialisation of XPCC Header and Payload into a byte buffer
+	// The mapping of type, ack, dest, src and id into a uint32_t from
+	// CanConnectorBase is used.
+
+	// 5 bytes for header
+	uint16_t buf_size = 5 + payload.getSize();
+	uint8_t buf[buf_size];
+
+	buf[0] = static_cast<uint8_t>(header.type);
+	buf[1] = header.isAcknowledge;
+	buf[2] = header.destination;
+	buf[3] = header.source;
+	buf[4] = header.packetIdentifier;
+
+	memcpy(buf + 5, payload.getPointer(), payload.getSize());
+
+#	if ZMQPP_VERSION_MAJOR >= 4
+	/**
+	* Breaking change in 4.1.1:
+	* Removed message::add(pointer, size_t) as there were situations it conflicts with the new easier 
+	* to use templated add. This has been replaced with a message::add_raw(pointer, size_t) method.
+	* https://github.com/zeromq/zmqpp/blob/develop/CHANGES.md
+	*/
+	message.add_raw(buf, buf_size);
+#	else
+	message.add(buf, buf_size);
+#	endif
+
+	socketOut.send(message, /* dont_block = */ true);
+}
+
+// ----------------------------------------------------------------------------
+bool
+ZeroMQConnector::isPacketAvailable() const
+{
+	return this->reader.isPacketAvailable();
+}
+
+// ----------------------------------------------------------------------------
+const Header&
+ZeroMQConnector::getPacketHeader() const
+{
+	return this->reader.getPacket().header;
+};
+
+// ----------------------------------------------------------------------------
+const xpcc::SmartPointer
+ZeroMQConnector::getPacketPayload() const
+{
+	return this->reader.getPacket().payload;
+};
+
+// ----------------------------------------------------------------------------
+void
+ZeroMQConnector::dropPacket()
+{
+	this->reader.dropPacket();
+}
+
+// ----------------------------------------------------------------------------
+void
+ZeroMQConnector::update()
+{
+}
+
+} // xpcc namespace
