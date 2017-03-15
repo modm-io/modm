07493feadbe152af03abca67b21dee827cbc86e1
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2017-03-15 23:19:48 +0000

    [stm32] Update drivers for minor changes in device files.

--- scons/site_tools/xpcc.py
+++ scons/site_tools/xpcc.py
@@ -509,11 +509,13 @@ def generate(env, **kw):
 	elif env['ARCHITECTURE'].startswith('hosted'):
 		env['HOSTED_DEVICE'] = device
 		env.Tool('hosted')
-	elif env['ARCHITECTURE'] in ['cortex-m0', 'cortex-m3', 'cortex-m4', 'cortex-m4f', 'cortex-m7', 'cortex-m7f', 'cortex-m7fd']:
+	elif env['ARCHITECTURE'] in ['cortex-m0', 'cortex-m0+', 'cortex-m3', 'cortex-m4', 'cortex-m4f', 'cortex-m7', 'cortex-m7f', 'cortex-m7fd']:
 		if env['ARCHITECTURE'] in ['cortex-m7f', 'cortex-m7fd']:
 			env['ARM_ARCH'] = 'cortex-m7'
 		elif env['ARCHITECTURE'] == 'cortex-m4f':
 			env['ARM_ARCH'] = 'cortex-m4'
+		elif env['ARCHITECTURE'] == 'cortex-m0+':
+			env['ARM_ARCH'] = 'cortex-m0p'
 		else:
 			env['ARM_ARCH'] = env['ARCHITECTURE']
 		env['ARM_DEVICE'] = device
