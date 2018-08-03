cf1cb44e06ae46700bf35593933181a5b2601408
Christopher Durand <christopher.durand@rwth-aachen.de>
2018-08-03 10:11:07 +0200

    [build.scons] Fix qtcreator project generator

--- tools/build_script_generator/scons/site_tools/qtcreator.py
+++ tools/build_script_generator/scons/site_tools/qtcreator.py
@@ -3,7 +3,8 @@
 #
 # Copyright (c) 2016, Kevin Läufer
 # Copyright (c) 2017, Michael Thies
-# Copyright (c) 2017, Niklas Hauser
+# Copyright (c) 2017-2018, Niklas Hauser
+# Copyright (c) 2018, Christopher Durand
 #
 # This file is part of the modm project.
 #
@@ -18,67 +19,70 @@
 This tool makes it possible to automatically generate a QtCreator project for
 applications using modm.
 
-You need to add something like this to your application's `SConstruct`:
+You need to add something like this to your application"s `SConstruct`:
 
 ~~~{.py}
 
-files = env.FindFiles('.')
+files = env.FindFiles(".")
 
 # [...]
 
-env.Alias('qtcreator', env.QtCreatorProject(files))
+env.Alias("qtcreator", env.QtCreatorProject(files))
 
 # [...]
 
-env.Alias('all', ['build', 'qtcreator', 'size'])
+env.Alias("all", ["build", "qtcreator", "size"])
 
 ~~~
 """
 
+from os.path import join, abspath, relpath
 
-import os
-
-def collect_defines(defines, source):
-	if isinstance(source, str):
-		defines[source] = None
-	elif isinstance(source, dict):
-		defines.update(source)
-	elif isinstance(source, list):
-		[collect_defines(defines, mm) for mm in source]
-
-
-def qt_creator_project_method(env, file_scanner, additional_sources=[]):
-	# collect values
-	project_name = env['MODM_PROJECT_NAME']
-	project_path = env['MODM_BASEPATH']
+def qt_creator_project_method(env, sources, with_modm=False):
+	cwd = env.Dir("#").abspath
+	project_name = env["CONFIG_PROJECT_NAME"]
+	# preprocessor defines
 	defines = {}
-	collect_defines(defines, env['CPPDEFINES'])
-	collect_defines(defines, file_scanner.defines)
-	files = [os.path.relpath(str(ff), project_path)
-	         for ff in file_scanner.header + file_scanner.sources + additional_sources]
-	includes = env['CPPPATH']
+	for define in env.get("CPPDEFINES", []):
+		if isinstance(define, str):
+			define = define.split("=")
+		defines[define[0]] = define[1] if len(define) > 1 else ""
+	# include paths
+	includes = [relpath(i, cwd) for i in env["CPPPATH"]]
+	# header files
+	headers = []
+	if with_modm:
+		headers = [relpath(h, cwd) for h in env.FindHeaderFiles("modm")]
+		sources += [relpath(s, cwd) for s in env.FindSourceFiles("modm")]
+
+	headers = list(map(str, headers))
+	sources = list(map(str, sources))
 
 	# generate project files
-	proj_path = os.path.join(project_path, project_name)
-	temp_path = os.path.join(env['MODM_ROOTPATH'], 'templates', 'qtcreator', 'project')
+	proj_path = join(cwd, project_name)
+	temp_path = abspath(join(env["BASEPATH"], "scons", "site_tools", "qtcreator", "project"))
 	return [
 		env.Jinja2Template(
-			target = proj_path + '.creator', source = temp_path + '.creator.in',
+			target = proj_path + ".creator",
+		    source = temp_path + ".creator.in",
 			substitutions = {}),
 		env.Jinja2Template(
-			target = proj_path + '.config', source = temp_path + '.config.in',
-			substitutions = {'defines': defines}),
+			target = proj_path + ".config",
+			source = temp_path + ".config.in",
+			substitutions = {"defines": defines}),
 		env.Jinja2Template(
-			target = proj_path + '.files', source = temp_path + '.files.in',
-			substitutions = {'files': files}),
+			target = proj_path + ".files",
+			source = temp_path + ".files.in",
+			substitutions = {"files": sorted(list(set(headers + sources)))}),
 		env.Jinja2Template(
-			target = proj_path + '.includes', source = temp_path + '.includes.in',
-			substitutions = {'includes': includes})
+			target = proj_path + ".includes",
+			source = temp_path + ".includes.in",
+			substitutions = {"includes": includes})
 	]
 
 
 def generate(env):
-	env.AddMethod(qt_creator_project_method, 'QtCreatorProject')
+	env.AddMethod(qt_creator_project_method, "QtCreatorProject")
 
 def exists(env):
 	return 1
