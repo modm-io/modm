1aa8fd70ce7059b1fb32ad64d7ce7fca73f4723d
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2018-08-07 02:40:15 +0200

    [build] Add common .gdbinit config file
    This adds a GDB config file to improve debugging with arm-none-eabi-gdb.

--- tools/build_script_generator/scons/site_tools/gdb.py
+++ tools/build_script_generator/scons/site_tools/gdb.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2009, Martin Rosekeit
-# Copyright (c) 2009-2012, Fabian Greif
-# Copyright (c) 2012, Georgi Grinshpun
-# Copyright (c) 2012, Sascha Schade
-# Copyright (c) 2012, 2016-2017, Niklas Hauser
-# Copyright (c) 2016, Daniel Krebs
-# Copyright (c) 2017, Michael Thies
+# Copyright (c) 2018, Niklas Hauser
 #
 # This file is part of the modm project.
 #
@@ -16,28 +10,35 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # -----------------------------------------------------------------------------
 
-import platform
+import os
 from SCons.Script import *
+import subprocess
+import signal
 
-# -----------------------------------------------------------------------------
-# Start the gnu debugger
-# -----------------------------------------------------------------------------
+def run_openocd_gdb(target, source, env):
+	# We have to start openocd in its own session ID, so that Ctrl-C in GDB
+	# does not kill OpenOCD. See https://github.com/RIOT-OS/RIOT/pull/3619.
+	openocd = subprocess.Popen("openocd -f openocd.cfg -c \"log_output /dev/null\"", cwd=os.getcwd(), shell=True,
+	                           preexec_fn=os.setsid)
+	# This call is blocking
+	subprocess.call("arm-none-eabi-gdb -x gdbinit {}".format(source[0]), cwd=os.getcwd(), shell=True)
+	# Then kill just the background OpenOCD process
+	os.killpg(os.getpgid(openocd.pid), signal.SIGTERM)
+	return 0
 
-def gdb_debug(env, source, alias='gdb_debug'):
-	actionString = '$GDB $SOURCE -ex "target extended-remote :$GDB_PORT"'
-	action = Action(actionString, cmdstr="$GDB_COMSTR")
+def gdb_arm_none_eabi_debug(env, source, alias="gdb_arm_none_eabi_debug"):
+	action = Action(run_openocd_gdb, cmdstr="$OPENOCDGDBSTR")
 	return env.AlwaysBuild(env.Alias(alias, source, action))
 
 # -----------------------------------------------------------------------------
 def generate(env, **kw):
 	# build messages
-	if not ARGUMENTS.get('verbose'):
-		env['GDB_COMSTR'] = "GDB: debugging $SOURCE"
+	if not ARGUMENTS.get("verbose"):
+		env["OPENOCDGDBSTR"] = "%s.------GDB----- %s$SOURCE\n" \
+	                           "%s'----OpenOCD--> %s$CONFIG_DEVICE_NAME%s" % \
+	                           ("\033[;0;32m", "\033[;0;33m", "\033[;0;32m", "\033[;1;33m", "\033[;0;0m")
 
-	env['GDB'] = "arm-none-eabi-gdb -tui"
-	env['GDB_PORT'] = '3333'
-
-	env.AddMethod(gdb_debug, 'GdbDebug')
+	env.AddMethod(gdb_arm_none_eabi_debug, "OpenocdGdb")
 
 def exists(env):
-	return env.Detect('gdb')
+	return env.Detect("openocd") and env.Detect("arm-none-eabi-gdb")
