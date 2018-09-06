a3b0d6c29fdca24f75b2b802585f844060e6238d
Christopher Durand <christopher.durand@rwth-aachen.de>
2018-09-06 02:22:44 +0200

    [stdc++] Add unittests for header compilation and std::vector

---
+++ test/stdc++/vector/vector_test.hpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2018, Christopher Durand
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#include <unittest/testsuite.hpp>
+
+class VectorTest : public unittest::TestSuite
+{
+public:
+	void
+	testInsertion();
+
+	void
+	testIteration();
+
+	void
+	testEmplace();
+
+	void
+	testRemove();
+};
