c581e8d59a3c0994405ba11ea3c76f829efcd27d
Sascha Schade (strongly-typed) <stronglytyp3d@gmail.com>
2018-09-29 12:03:54 +0200

    [examples] Generic ROS CAN bridge with modm.

---
+++ examples/generic/ros/can_bridge/README.md
@@ -0,0 +1,54 @@
+# CAN Bridge between modm and ROS
+
+## Package summary
+
+Conversion node for messages from modm CAN to a ROS Topic and vice versa.
+Strongly inspired by [`socketcan_bridge`](http://wiki.ros.org/socketcan_bridge) from ROS. From a ROS point of view this is a complete replacement of a USB CAN adaptor (like Peak PCAN-USB) connected to a Linux computer running `socketcan_bridge`. It allows you to receive all CAN messages from the bus in ROS and send arbitrary messages to the CAN bus from ROS.
+
+## Overview
+
+The packages provides functionality to expose CAN frames from modm to a ROS Topic. Internally it uses modm to receive and send messages to and from the CAN bus controller of a microcontroller.
+
+### Hardware setup
+
+A modm compatible microcontroller with CAN controller is required. In most cases an external CAN transceiver must be connected between the CAN bus and the CAN controller of the microcontroller. Example setup is
+
+* STM32F103 Blue Pill Board
+* SN65HVD252 CAN Transceiver
+  * `CAN_TX` connected to pin `B9` of microcontroller
+  * `CAN_RX` connected to pin `B8` of microcontroller
+* USB UART bridge
+  * `TX` connected to pin `A3`
+  * `RX` connected to pin `A2`
+
+Nucleo boards with integrated USB UART bridge also work. LPC11C24 might be added later.
+
+On-board LED toggles once per second and for every message received or transmitted.
+
+## Nodes
+
+The functionality is offered in the form of a single node running on the microcontroller. The microcontroller uses [rosserial_python](http://wiki.ros.org/rosserial_python) to forward and receive messages to and from the ROS core on the host computer.
+
+### Subscribed Topics
+
+`sent_messages` [can_msgs/Frame](http://docs.ros.org/api/can_msgs/html/msg/Frame.html)
+
+* Messages received here will be sent to the CAN bus.
+
+### Published Topics
+
+`received_messages` [can_msgs/Frame](http://docs.ros.org/api/can_msgs/html/msg/Frame.html)
+
+* Frames received on the CAN bus are published in this topic.
+
+## Examples
+
+Build and program the binary to the microcontroller:
+
+    lbuild build && scons program
+
+To run the node with a different port and baud rate, for example on `/dev/ttyACM0`, you must specify the `~port` and `~baud` parameters on the command line. 
+
+    rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0 _baud:=250000
+
+Baud rate is the baud rate of the serial link and is set to 250 kBps. It is not the baud rate of the CAN bus, which is also hardcoded to 250 kBps.
---
+++ examples/generic/ros/can_bridge/main.cpp
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2018, Sascha Schade
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <modm/board.hpp>
+#include <modm/processing/timer.hpp>
+
+#include <can_msgs/Frame.h>
+#include <ros/node_handle.h>
+#include <modm/communication/ros.hpp>
+
+namespace modm {
+namespace can {
+bool
+messageRosToModm(const can_msgs::Frame& source, modm::can::Message& destination) {
+	if (source.dlc > 8) {
+		return false;
+	}
+
+	destination.setIdentifier(source.id);
+	destination.setLength(source.dlc);
+	if (source.is_extended) { destination.setExtended(true); }
+	if (source.is_rtr)      { destination.setRemoteTransmitRequest(true);	}
+	memcpy(destination.data, source.data, source.dlc);
+
+	return true;
+}
+
+bool
+messageModmToRos(const modm::can::Message& source, can_msgs::Frame& destination) {
+	destination.id = source.getIdentifier();
+	destination.dlc = source.getLength();
+	destination.is_rtr = source.isRemoteTransmitRequest();
+	destination.is_extended = source.isExtended();
+	memcpy(destination.data, source.data, source.getLength());
+	return true;
+}
+} // can namespace
+} // modm namespace
+
+// Set the log level
+#undef	MODM_LOG_LEVEL
+#define	MODM_LOG_LEVEL modm::log::DISABLED
+
+using RosSerialUart = Usart2;
+
+namespace ros
+{
+	using modmHardware = ModmHardware<RosSerialUart>;
+	using ModmNodeHandle = NodeHandle_<modmHardware>;
+}
+
+ros::ModmNodeHandle nh;
+
+// For publisher
+can_msgs::Frame can_msg;
+ros::Publisher pub_can("/can/received_messages", &can_msg);
+
+static void
+publishMessage(const modm::can::Message& message) {
+	can_msgs::Frame can_msg;
+	if (modm::can::messageModmToRos(message, can_msg)) {
+		can_msg.header.stamp = nh.now();
+		pub_can.publish(&can_msg);
+		Board::LedGreen::toggle();
+	}
+}
+
+// For subscriber
+static void
+message_cb_can(const can_msgs::Frame& ros_can_msg) {
+	modm::can::Message modm_can_msg;
+	if (messageRosToModm(ros_can_msg, modm_can_msg)) {
+		Can::sendMessage(modm_can_msg);
+		Board::LedGreen::toggle();
+	}
+}
+
+
+// ----------------------------------------------------------------------------
+int
+main()
+{
+	Board::initialize();
+
+	// Initialize Usart
+	RosSerialUart::connect<GpioOutputA2::Tx>();
+	RosSerialUart::connect<GpioInputA3::Rx>();
+
+	// Reinit onboard UART to 1 Mbps
+	// Do not use it for logging because this will destroy ROS serial messages.
+	RosSerialUart::initialize<Board::systemClock, modm::Uart::Baudrate::kBps250>(12);
+
+	CanFilter::setStartFilterBankForCan2(14);
+
+	// Initialize Can1
+	if (false) {
+		Can::connect<GpioInputA11::Rx, GpioOutputA12::Tx>(Gpio::InputType::PullUp);
+	} else {
+		Can::connect<GpioInputB8::Rx, GpioOutputB9::Tx>(Gpio::InputType::PullUp);
+	}
+	Can::initialize<Board::systemClock, Can::Bitrate::kBps250>(9);
+
+	// Receive every message
+	CanFilter::setFilter(0, CanFilter::FIFO0,
+			CanFilter::ExtendedIdentifier(0),
+			CanFilter::ExtendedFilterMask(0));
+
+	CanFilter::setFilter(1, CanFilter::FIFO0,
+			CanFilter::StandardIdentifier(0),
+			CanFilter::StandardFilterMask(0));
+
+	nh.initNode();
+
+	ros::Subscriber<can_msgs::Frame> sub_can ("/can/sent_messages", &message_cb_can);
+	nh.subscribe(sub_can);
+	nh.advertise(pub_can);
+
+	modm::ShortPeriodicTimer heartbeat(1000);
+	while (true)
+	{
+		if (Can::isMessageAvailable())
+		{
+			modm::can::Message message;
+			Can::getMessage(message);
+			publishMessage(message);
+		}
+
+		nh.spinOnce();
+		if (heartbeat.execute()) {
+			Board::LedGreen::toggle();
+		}
+	}
+
+	return 0;
+}
