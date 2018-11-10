efa41ffa9c764cd0bf13f95b43b0daf675cb1afe
Christopher Durand <christopher.durand@rwth-aachen.de>
2018-11-10 20:35:19 +0100

    [stm32] Add NUCLEO-L432KC SPI USART example

---
+++ examples/nucleo_l432kc/uart_spi/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2013, Kevin Läufer
+ * Copyright (c) 2013-2018, Niklas Hauser
+ * Copyright (c) 2018, Christopher Durand
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#include <modm/board.hpp>
+
+
+int
+main()
+{
+	Board::initialize();
+
+	// Enable Uart SPI 1
+	UartSpiMaster1::connect<GpioA8::Ck, GpioA9::Tx, GpioA10::Rx>();
+	UartSpiMaster1::initialize<Board::systemClock, 1'000'000, modm::Tolerance::Exact>();
+
+	while (1)
+	{
+		UartSpiMaster1::transferBlocking(0xF0);
+	}
+
+	return 0;
+}
