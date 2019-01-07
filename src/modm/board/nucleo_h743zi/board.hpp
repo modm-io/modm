dc0505fcbaf58be6c6db673de5c89969d74c717e
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2019-01-07 17:04:54 +0100

    [board] Add Nucleo-H743ZI BSP

---
+++ src/modm/board/nucleo_h743zi/board.hpp
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2019, Niklas Hauser
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#ifndef MODM_STM32_NUCLEO_H743ZI_HPP
+#define MODM_STM32_NUCLEO_H743ZI_HPP
+
+#include <modm/platform.hpp>
+#include <modm/architecture/interface/clock.hpp>
+#include <modm/debug/logger.hpp>
+/// @ingroup modm_board_nucleo_h743zi
+#define MODM_BOARD_HAS_LOGGER
+
+using namespace modm::platform;
+
+/// @ingroup modm_board_nucleo_h743zi
+namespace Board
+{
+
+/// STM32H743 running at 400MHz from the external 8MHz STLink clock
+struct systemClock
+{
+	static constexpr uint32_t Frequency = MHz180;
+	static constexpr uint32_t Apb1 = Frequency / 4;
+	static constexpr uint32_t Apb2 = Frequency / 2;
+
+	static constexpr uint32_t Adc = Apb2;
+
+	static constexpr uint32_t Spi1 = Apb2;
+	static constexpr uint32_t Spi2 = Apb1;
+	static constexpr uint32_t Spi3 = Apb1;
+	static constexpr uint32_t Spi4 = Apb2;
+	static constexpr uint32_t Spi5 = Apb2;
+	static constexpr uint32_t Spi6 = Apb2;
+
+	static constexpr uint32_t Usart1 = Apb2;
+	static constexpr uint32_t Usart2 = Apb1;
+	static constexpr uint32_t Usart3 = Apb1;
+	static constexpr uint32_t Uart4  = Apb1;
+	static constexpr uint32_t Uart5  = Apb1;
+	static constexpr uint32_t Usart6 = Apb2;
+	static constexpr uint32_t Uart7  = Apb1;
+	static constexpr uint32_t Uart8  = Apb1;
+
+	static constexpr uint32_t Can1 = Apb1;
+	static constexpr uint32_t Can2 = Apb1;
+
+	static constexpr uint32_t I2c1 = Apb1;
+	static constexpr uint32_t I2c2 = Apb1;
+	static constexpr uint32_t I2c3 = Apb1;
+	static constexpr uint32_t I2c4 = Apb1;
+
+	static constexpr uint32_t Apb1Timer = Apb1 * 2;
+	static constexpr uint32_t Apb2Timer = Apb2 * 2;
+	static constexpr uint32_t Timer1  = Apb2Timer;
+	static constexpr uint32_t Timer2  = Apb1Timer;
+	static constexpr uint32_t Timer3  = Apb1Timer;
+	static constexpr uint32_t Timer4  = Apb1Timer;
+	static constexpr uint32_t Timer5  = Apb1Timer;
+	static constexpr uint32_t Timer6  = Apb1Timer;
+	static constexpr uint32_t Timer7  = Apb1Timer;
+	static constexpr uint32_t Timer8  = Apb2Timer;
+	static constexpr uint32_t Timer10 = Apb2Timer;
+	static constexpr uint32_t Timer11 = Apb2Timer;
+	static constexpr uint32_t Timer12 = Apb1Timer;
+	static constexpr uint32_t Timer13 = Apb1Timer;
+	static constexpr uint32_t Timer14 = Apb1Timer;
+
+	static bool inline
+	enable()
+	{
+		ClockControl::enableExternalClock(); // 8 MHz
+		ClockControl::enablePll(
+			ClockControl::PllSource::ExternalClock,
+			4,      // 8MHz / N=4 -> 2MHz
+			180,    // 2MHz * M=180 -> 360MHz
+			2,      // 360MHz / P=2 -> 180MHz = F_cpu
+			7       // 360MHz / Q=7 -> ~51MHz = F_usb => bad for USB
+		);
+		ClockControl::setFlashLatency(Frequency);
+		ClockControl::enableSystemClock(ClockControl::SystemClockSource::Pll);
+		ClockControl::setApb1Prescaler(ClockControl::Apb1Prescaler::Div4);
+		ClockControl::setApb2Prescaler(ClockControl::Apb2Prescaler::Div2);
+		// update clock frequencies
+		modm::clock::fcpu     = Frequency;
+		modm::clock::fcpu_kHz = Frequency / 1000;
+		modm::clock::fcpu_MHz = Frequency / 1000000;
+		modm::clock::ns_per_loop = ::round(3000.f / (Frequency / 1000000));
+
+		return true;
+	}
+};
+
+// Arduino Footprint
+#include "nucleo144_arduino.hpp"
+
+using Button = GpioInputC13;
+
+using LedGreen = GpioOutputB0;	// LED1 [Green]
+using LedBlue = GpioOutputB7;	// LED2 [Blue]
+using LedRed = GpioOutputB14;	// LED3 [Red]
+using Leds = SoftwareGpioPort< LedRed, LedBlue, LedGreen >;
+
+namespace stlink
+{
+using Tx = GpioOutputD8;
+using Rx = GpioInputD9;
+using Uart = Usart3;
+}
+
+using LoggerDevice = modm::IODeviceWrapper< stlink::Uart, modm::IOBuffer::BlockIfFull >;
+
+
+inline void
+initialize()
+{
+    systemClock::enable();
+    modm::cortex::SysTickTimer::initialize<systemClock>();
+
+    stlink::Uart::connect<stlink::Tx::Tx, stlink::Rx::Rx>();
+    stlink::Uart::initialize<systemClock, 115200>();
+
+    LedGreen::setOutput(modm::Gpio::Low);
+    LedBlue::setOutput(modm::Gpio::Low);
+    LedRed::setOutput(modm::Gpio::Low);
+
+    Button::setInput();
+    Button::setInputTrigger(Gpio::InputTrigger::RisingEdge);
+    Button::enableExternalInterrupt();
+//  Button::enableExternalInterruptVector(12);
+}
+
+}
+
+#endif  // MODM_STM32_NUCLEO_F429ZI_HPP
