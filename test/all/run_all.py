3fb1928bcd7fbffc1faf54b220855d207dda82ae
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2019-01-22 06:04:53 +0100

    [test] Write to common CacheDir for CI

--- test/all/run_all.py
+++ test/all/run_all.py
@@ -20,6 +20,7 @@ import tempfile
 import logging
 import subprocess
 import multiprocessing
+from pathlib import Path
 
 LOGGER = logging.getLogger("run")
 LBUILD_COMMAND = ["lbuild"]
@@ -32,7 +33,7 @@ class CommandException(Exception):
 def run_command(cmdline):
     try:
         cmdline = " ".join(cmdline)
-        p = subprocess.run(cmdline, shell=True, stdout=subprocess.PIPE, universal_newlines = True)
+        p = subprocess.run(cmdline, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
         return (p.stdout, p.returncode)
     except KeyboardInterrupt:
         raise multiprocessing.ProcessError()
@@ -46,20 +47,14 @@ def run_lbuild(command):
     return output
 
 def run_scons(tempdir):
-    cmdline = ["scons", "-C", tempdir]
+    cmdline = ["scons", "-C", tempdir, "--random", "--cache-show"]
     return run_command(cmdline)
 
 def build_code(tempdir):
-    count = 0
-    while count < 20:
-        output, retval = run_scons(tempdir)
-        if retval != 0:
-            if "Cannot allocate memory" in output:
-                # CircleCI is running out of memory
-                count += 1
-                continue
-            raise CommandException(output)
-        return output
+    output, retval = run_scons(tempdir)
+    if retval != 0:
+        raise CommandException(output)
+    return output
 
 
 class TestRunResult(enum.Enum):
@@ -69,11 +64,12 @@ class TestRunResult(enum.Enum):
 
 
 class TestRun:
-    def __init__(self, device):
+    def __init__(self, device, cache_dir=None):
         self.device = device
         self.output = ""
         self.result = TestRunResult.FAIL_BUILD
         self.time = None
+        self.cache_dir = cache_dir
 
     def run(self):
         start_time = time.time()
@@ -85,6 +81,9 @@ class TestRun:
             elif self.device.startswith("stm32"):
                 lbuild_command = ["-c", "stm32.xml"]
                 shutil.copyfile("stm32.cpp", os.path.join(tempdir, "main.cpp"))
+
+            if self.cache_dir:
+                lbuild_command.append("-D:::cache_dir={}".format(self.cache_dir))
 
             lbuild_command.extend(["-D:target={}".format(self.device),
                                    "-p", str(tempdir),
@@ -145,11 +144,15 @@ def main():
     devices = [d for d in devices if d not in ignored_devices]
 
     os.makedirs("log", exist_ok=True)
+    cache_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../build/cache"))
+    Path(cache_dir).mkdir(exist_ok=True, parents=True)
+    if "no-cache" in sys.argv:
+        cache_dir = None
 
     try:
         #devices = [device for device in devices if device.startswith("atx")]
         with multiprocessing.Pool(cpus) as pool:
-            test_runs = pool.map(build_device, [TestRun(x) for x in devices])
+            test_runs = pool.map(build_device, [TestRun(x, cache_dir) for x in devices])
 
         succeded = []
         failed = []
