21af57b793f37db71de3e417e6f712b54b0d1f70
Raphael Lehmann <raphael@rleh.de>
2019-02-24 20:19:38 +0100

    [examples] Encoder output examle for STM32F4_Discovery

---
+++ examples/stm32f4_discovery/encoder_output/main.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020, Raphael Lehmann
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#include <modm/board.hpp>
+#include <modm/driver/encoder/encoder_output.hpp>
+
+using namespace Board;
+
+// ----------------------------------------------------------------------------
+int
+main()
+{
+
+	initialize();
+
+	LedBlue::set();
+	LedGreen::set();
+
+	// period=100000 // 100000us => 10Hz (visible)
+	using TestEncoder = modm::EncoderOutput<LedBlue, LedGreen, int32_t, modm::PrecisePeriodicTimer, 100'000>;
+	TestEncoder testEncoder{0};
+
+	testEncoder.setPosition(1 << 31);
+	while (1)
+	{
+		testEncoder.update();
+		LedRed::toggle();
+	}
+
+	return 0;
+}
