7f0a619f3a94dc0aa266ae6b8398c06ecf136f99
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2020-03-11 04:17:19 +0100

    [example] Add C++ exceptions and RTTI example

---
+++ examples/stm32f469_discovery/exceptions_rtti/main.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2020, Niklas Hauser
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#include <modm/board.hpp>
+#include <typeinfo>
+
+using namespace Board;
+
+// ----------------------------------------------------------------------------
+int
+main()
+{
+	Board::initialize();
+	LedD13::setOutput(modm::Gpio::Low);
+	uint32_t counter(0);
+
+	MODM_LOG_INFO << "TypeId for LedD13: " << typeid(LedD13).name() << modm::endl;
+
+	while (true)
+	{
+		LedBlue::toggle();
+		modm::delayMilliseconds(500);
+
+		try {
+			throw counter++;
+		}
+		catch (uint32_t code) {
+			MODM_LOG_ERROR << "Caught exception #" << code << modm::endl;
+		}
+	}
+
+	return 0;
+}
