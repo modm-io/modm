94671d5d7790991bf39a19c54bd990d7d949cae3
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2020-03-13 02:00:35 +0100

    [examples] Add recursive static initialization example

---
+++ examples/stm32f469_discovery/threadsafe_statics/main.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2020, Niklas Hauser
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#include <modm/board.hpp>
+
+void constructDummy();
+class Dummy
+{
+public:
+	Dummy()
+	{
+		MODM_LOG_INFO << "Dummy class constructed" << modm::endl;
+		constructDummy(); // recursive initialization
+	}
+};
+
+void
+constructDummy()
+{
+	static Dummy dummy;
+	MODM_LOG_INFO << "constructDummy() called" << modm::endl;
+}
+
+int
+main()
+{
+	Board::initialize();
+	uint32_t counter(0);
+
+	while (true)
+	{
+		Board::LedGreen::toggle();
+		modm::delayMilliseconds(Board::Button::read() ? 125 : 500);
+
+		MODM_LOG_INFO << "loop: " << counter++ << modm::endl;
+		constructDummy();
+	}
+
+	return 0;
+}
