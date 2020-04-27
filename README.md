79e04904e89858b98ef202a3965007553cf9e487
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2020-04-27 19:44:25 +0200

    [ext] Update submodules

--- README.md
+++ README.md
@@ -70,7 +70,7 @@ git clone --recurse-submodules https://github.com/modm-io/modm.git
 
 ## Targets
 
-modm can generate code for <!--avrcount-->530<!--/avrcount--> AVR and <!--stmcount-->1857<!--/stmcount-->
+modm can generate code for <!--avrcount-->530<!--/avrcount--> AVR and <!--stmcount-->1959<!--/stmcount-->
 STM32 devices, however, there are different levels of support and testing.
 
 <center>
--- examples/nucleo_f429zi/cmsis_dsp/linear_interp/main.cpp
+++ examples/nucleo_f429zi/cmsis_dsp/linear_interp/main.cpp
@@ -17,10 +17,6 @@
 #define main arm_cmsis_dsp_example
 #include "../../../../ext/arm/cmsis/CMSIS/DSP/Examples/ARM/arm_linear_interp_example/math_helper.h"
 #include "../../../../ext/arm/cmsis/CMSIS/DSP/Examples/ARM/arm_linear_interp_example/math_helper.c"
-/* `float arm_linear_interep_table[188495]` is 736kB large and MUST be placed in Flash
- * memory NOT SRAM otherwise it won't fit on the device. Since we don't want (=cannot)
- * to change the example, we need to take drastic measures... */
-#define float const float
 #include "../../../../ext/arm/cmsis/CMSIS/DSP/Examples/ARM/arm_linear_interp_example/arm_linear_interp_data.c"
 #define while return status; void
 #include "../../../../ext/arm/cmsis/CMSIS/DSP/Examples/ARM/arm_linear_interp_example/arm_linear_interp_example_f32.c"
