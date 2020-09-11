c2193b28d91870144941db06610bb0a91a11937c
Sascha Schade (strongly-typed) <stronglytyp3d@gmail.com>
2020-09-11 22:39:31 +0200

    [examples] Add DevEBox STM32F4XX Logger example

---
+++ examples/stm32f407vet6_devebox/logger/main.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2020, Sascha Schade
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#include <modm/board.hpp>
+#include <modm/debug/logger.hpp>
+
+using namespace Board;
+
+modm::IODeviceWrapper< Usart1, modm::IOBuffer::BlockIfFull > loggerDevice;
+
+// Set all four logger streams to use the UART
+modm::log::Logger modm::log::debug(loggerDevice);
+modm::log::Logger modm::log::info(loggerDevice);
+modm::log::Logger modm::log::warning(loggerDevice);
+modm::log::Logger modm::log::error(loggerDevice);
+
+// Set the log level
+#undef	MODM_LOG_LEVEL
+#define	MODM_LOG_LEVEL modm::log::DEBUG
+
+
+int
+main()
+{
+	Board::initialize();
+
+	Usart1::connect<GpioInputA9::Tx>();
+	Usart1::initialize<Board::SystemClock, 115200_Bd>();
+
+	// Use the logging streams to print some messages.
+	// Change MODM_LOG_LEVEL above to enable or disable these messages
+	MODM_LOG_DEBUG   << "debug"   << modm::endl;
+	MODM_LOG_INFO    << "info"    << modm::endl;
+	MODM_LOG_WARNING << "warning" << modm::endl;
+	MODM_LOG_ERROR   << "error"   << modm::endl;
+
+	uint32_t counter(0);
+
+	while(true)
+	{
+		LedGreen::toggle();
+		modm::delay(Button::read() ? 100ms : 500ms);
+
+		MODM_LOG_DEBUG << "loop count: " << counter++ << modm::endl;
+	}
+
+	return 0;
+}
