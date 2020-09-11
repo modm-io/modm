3bebdb5c6d07f405a31f1d6b175cc130684c64f7
Sascha Schade (strongly-typed) <stronglytyp3d@gmail.com>
2020-09-11 22:39:06 +0200

    [examples] Add DevEBox STM32F4XX GPIO example

---
+++ examples/stm32f407vet6_devebox/blinky/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020, Sascha Schade
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#include <modm/board.hpp>
+
+using namespace Board;
+
+
+int
+main()
+{
+	Board::initialize();
+
+	while(true)
+	{
+		LedGreen::toggle();
+		modm::delay(Button::read() ? 100ms : 500ms);
+	}
+
+	return 0;
+}
