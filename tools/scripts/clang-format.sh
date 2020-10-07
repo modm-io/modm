debb4309522571316628e774136853bdb2bd8d25
Erik Henriksson <erikhenrikssn@gmail.com>
2020-10-07 02:26:56 +0200

    [tools] Add clang-format style

---
+++ tools/scripts/clang-format.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright (c) 2020, Erik Henriksson
+#
+# This file is part of the modm project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#----------------------------------------------------------------------------
+
+git diff --name-only --diff-filter=A -C -M develop | grep -e "\.\(c\|h\|hpp\|cpp\)\$" | xargs clang-format -i "$@"
