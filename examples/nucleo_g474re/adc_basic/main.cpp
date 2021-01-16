22275c0d58b1a515a0e19743c3e17146f9084392
Raphael Lehmann <raphael@rleh.de>
2021-01-16 17:15:08 +0100

    [examples] Nucleo_G474RE: Add basic ADC example

---
+++ examples/nucleo_g474re/adc_basic/main.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021, Raphael Lehmann
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <modm/board.hpp>
+
+#undef	MODM_LOG_LEVEL
+#define	MODM_LOG_LEVEL modm::log::INFO
+
+int
+main()
+{
+	Board::initialize();
+
+	MODM_LOG_INFO << "STM32G4 ADC basic example" << modm::endl;
+	MODM_LOG_INFO << " running on Nucleo-G474RE" << modm::endl << modm::endl;
+
+	MODM_LOG_INFO << "Configuring ADC ...";
+	Adc1::initialize(
+		Adc1::ClockMode::SynchronousPrescaler1,
+		Adc1::ClockSource::SystemClock,
+		Adc1::Prescaler::Disabled,
+		Adc1::CalibrationMode::SingleEndedInputsMode,
+		true);
+	Adc1::connect<GpioA0::In1>();
+	Adc1::setPinChannel<GpioA0>(Adc1::SampleTime::Cycles13);
+
+	while (true)
+	{
+		Adc1::startConversion();
+		while(!Adc1::isConversionFinished)
+			;
+		int adcValue = Adc1::getValue();
+		MODM_LOG_INFO << "adcValue=" << adcValue;
+		float voltage = adcValue * 3.3 / 0xfff;
+		MODM_LOG_INFO << " voltage=";
+		MODM_LOG_INFO.printf("%.3f\n", voltage);
+		modm::delay(500ms);
+	}
+
+	return 0;
+}
