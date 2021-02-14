0c663f4b0cb07a939968f747096b4aa1357447a2
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2021-02-14 20:15:54 +0100

    [scons] Import simplified DLR scons tools

---
+++ tools/build_script_generator/scons/site_tools/gcc_retarget.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016-2017, German Aerospace Center (DLR)
+# Copyright (c) 2018, Niklas Hauser
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Authors:
+# - 2016-2017, Fabian Greif (DLR RY-AVS)
+# - 2018, Niklas Hauser
+
+import os
+
+from SCons.Script import *
+
+def strip_binary(env, target, source, options="--strip-unneeded"):
+    return env.Command(target,
+                       source,
+                       Action("$STRIP {} -o $TARGET $SOURCE".format(options),
+                              cmdstr="$STRIPCOMSTR"))
+
+def list_symbols(env, source, alias='__symbols'):
+    action = Action("$NM $SOURCE -S -C --size-sort -td",
+                    cmdstr="$SYMBOLSCOMSTR")
+    return env.AlwaysBuild(env.Alias(alias, source, action))
+
+
+def generate(env, **kw):
+    env.Tool('gcc')
+    env.Tool('g++')
+    env.Tool('gnulink')
+    env.Tool('ar')
+    env.Tool('as')
+    env.Tool('utils')
+
+    # Define executable name of the compiler
+    path = env.get('COMPILERPATH', '')
+    prefix = env.get('COMPILERPREFIX', '')
+    suffix = env.get('COMPILERSUFFIX', '')
+    if suffix != '' and not suffix.startswith('-'):
+        suffix = '-' + suffix
+
+    prefix = path + prefix
+    env['CC'] = prefix + 'gcc' + suffix
+    env['CXX'] = prefix + 'g++' + suffix
+    env['AR'] = prefix + 'ar'
+    env['RANLIB'] = prefix + 'ranlib'
+    if suffix == '':
+        env['AS'] = prefix + 'as'
+        env['NM'] = prefix + 'nm'
+    else:
+        env['AS'] = prefix + 'gcc' + suffix
+        env['NM'] = prefix + 'gcc-nm' + suffix
+        if sys.platform != "darwin":
+            env['AR'] = prefix + 'gcc-ar' + suffix
+            env['RANLIB'] = prefix + 'gcc-ranlib' + suffix
+
+    env['OBJCOPY'] = prefix + 'objcopy'
+    env['OBJDUMP'] = prefix + 'objdump'
+    env['SIZE'] = prefix + 'size'
+    env['STRIP'] = prefix + 'strip'
+
+    env['LINK'] = env['CXX']
+
+    builder_hex = Builder(
+        action=Action("$OBJCOPY -O ihex $SOURCE $TARGET",
+        cmdstr="$HEXCOMSTR"),
+        suffix=".hex",
+        src_suffix="")
+
+    builder_bin = Builder(
+        action=Action("$OBJCOPY -O binary $SOURCE $TARGET",
+        cmdstr="$BINCOMSTR"),
+        suffix=".bin",
+        src_suffix="")
+
+    builder_listing = Builder(
+        action=Action("$OBJDUMP -x -S -l -w $SOURCE > $TARGET",
+        cmdstr="$LSSCOMSTR"),
+        suffix=".lss",
+        src_suffix="")
+
+    env.Append(BUILDERS={
+        'Hex': builder_hex,
+        'Bin': builder_bin,
+        'Listing': builder_listing
+    })
+
+    env.AddMethod(strip_binary, 'Strip')
+    env.AddMethod(list_symbols, 'Symbols')
+
+
+def exists(env):
+    return True
+
---
+++ tools/build_script_generator/scons/site_tools/utils.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2013-2014, 2016-2017, German Aerospace Center (DLR)
+# Copyright (c) 2018, Niklas Hauser
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Authors:
+# - 2013-2014, 2016-2017, Fabian Greif (DLR RY-AVS)
+# - 2018, Niklas Hauser
+
+import os
+import os.path
+
+from SCons.Script import *
+
+def _listify(obj):
+    if obj is None:
+        return list()
+    if isinstance(obj, SCons.Node.NodeList):
+        return obj
+    if isinstance(obj, (list, tuple, set)):
+        return list(obj)
+    if hasattr(obj, "__iter__") and not hasattr(obj, "__getitem__"):
+        return list(obj)
+    return [obj, ]
+
+
+def listify(env, *objs):
+    return [entry for obj in objs for entry in _listify(obj)]
+
+
+def remove_from_list(env, identifier, to_remove):
+    """
+    Remove strings from a list.
+
+    E.g.
+    env.RemoveFromList('CXXFLAGS_warning', ['-Wold-style-cast'])
+    """
+    if identifier.startswith('$'):
+        raise Exception("Identifier '%s' must not start with '$'!" % identifier)
+
+    l = env.subst('$' + identifier)
+    if isinstance(l, str):
+        l = l.split(' ')
+    for r in _listify(to_remove):
+        if r in l:
+            l.remove(r)
+    env[identifier] = l
+
+
+def filtered_glob(env, pattern, omit=None, ondisk=True, source=False, strings=False):
+    if omit is None:
+        omit = []
+
+    results = []
+    for p in _listify(pattern):
+        results.extend(filter(lambda f: os.path.basename(f.path) not in omit,
+                              env.Glob(p)))
+    return results
+
+
+def run_program(env, program):
+    return env.Command('thisfileshouldnotexist',
+                       program,
+                       '@"%s"' % program[0].abspath)
+
+
+def phony_target(env, **kw):
+    for target, action in kw.items():
+        env.AlwaysBuild(env.Alias(target, [], action))
+
+
+# -----------------------------------------------------------------------------
+def generate(env, **kw):
+    env.Append(ENV={'PATH': os.environ['PATH']})
+
+    env.AddMethod(remove_from_list, 'RemoveFromList')
+    env.AddMethod(filtered_glob, 'FilteredGlob')
+
+    env.AddMethod(run_program, 'Run')
+    env.AddMethod(phony_target, 'Phony')
+
+    env.AddMethod(listify, 'Listify')
+
+
+def exists(env):
+    return True
+
