2bb1445bdb84513cd77226480cc701247879c25d
Thomas Sommer <thomas@tomsaw.de>
2021-03-11 11:51:30 +0100

    Support IOStream for modm::color::HsvT

--- src/modm/ui/color_impl.hpp
+++ src/modm/ui/color_impl.hpp
@@ -75,3 +75,11 @@ modm::color::operator << ( modm::IOStream& os, const modm::color::RgbT<Underlyin
 	os << color.red << "\t" << color.green << "\t" << color.blue;
 	return os;
 }
+
+template <typename UnderlyingType>
+modm::IOStream&
+modm::color::operator << ( modm::IOStream& os, const modm::color::HsvT<UnderlyingType>& color)
+{
+	os << color.hue << "\t" << color.saturation << "\t" << color.value;
+	return os;
+}
