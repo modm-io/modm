7b06716288057c07642174c26f76a687d71b15b2
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2021-03-24 18:40:40 +0100

    [tools] Add job splitting to example compilation

--- tools/scripts/examples_compile.py
+++ tools/scripts/examples_compile.py
@@ -10,104 +10,127 @@
 import os
 import sys
 import re
+import argparse
 import platform
 import subprocess
 import multiprocessing
 from pathlib import Path
 
-is_running_in_ci = 	os.getenv("CIRCLECI") is not None or \
-					os.getenv("TRAVIS") is not None or \
-					os.getenv("GITHUB_ACTIONS") is not None
-cpus = 4 if is_running_in_ci else os.cpu_count()
+is_running_in_ci = (os.getenv("CIRCLECI") is not None or
+                    os.getenv("TRAVIS") is not None or
+                    os.getenv("GITHUB_ACTIONS") is not None)
 build_dir = (Path(os.path.abspath(__file__)).parents[2] / "build")
 cache_dir = build_dir / "cache"
 global_options = {}
 if is_running_in_ci:
-	global_options["::build.path"] = "build/"
-	global_options[":::cache_dir"] = str(cache_dir)
+    global_options["::build.path"] = "build/"
+    global_options[":::cache_dir"] = str(cache_dir)
 
 def run_command(where, command, all_output=False):
-	result = subprocess.run(command, shell=True, cwd=where, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	output = ""
-	if result.returncode or all_output:
-		output += result.stdout.decode("utf-8").strip(" \n")
-	output += result.stderr.decode("utf-8").strip(" \n")
-	return (result.returncode, output)
+    result = subprocess.run(command, shell=True, cwd=where, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = ""
+    if result.returncode or all_output:
+        output += result.stdout.decode("utf-8").strip(" \n")
+    output += result.stderr.decode("utf-8").strip(" \n")
+    return (result.returncode, output)
 
 def generate(project):
-	path = project.parent
-	output = ["=" * 90, "Generating: {}".format(path)]
-	options = " ".join("-D{}={}".format(k, v) for k,v in global_options.items())
-	# Compile Linux examples under macOS with hosted-darwin target
-	if "hosted-linux" in project.read_text():
-		options += " -D:target=hosted-{}".format(platform.system().lower())
-	rc, ro = run_command(path, "lbuild {} build".format(options))
-	print("\n".join(output + [ro]))
-	return None if rc else project
+    path = project.parent
+    output = ["=" * 90, "Generating: {}".format(path)]
+    options = " ".join("-D{}={}".format(k, v) for k,v in global_options.items())
+    # Compile Linux examples under macOS with hosted-darwin target
+    if "hosted-linux" in project.read_text():
+        options += " -D:target=hosted-{}".format(platform.system().lower())
+    rc, ro = run_command(path, "lbuild {} build".format(options))
+    print("\n".join(output + [ro]))
+    return None if rc else project
 
 def build(project):
-	path = project.parent
-	project_cfg = project.read_text()
-	commands = []
-	if ":build:scons" in project_cfg:
-		if is_running_in_ci:
-			commands.append("scons build --cache-show --random")
-		else:
-			commands.append("python3 `which scons` build --cache-show --random")
-	if ":build:cmake" in project_cfg:
-		commands.append("make cmake && make build")
+    path = project.parent
+    project_cfg = project.read_text()
+    commands = []
+    if ":build:scons" in project_cfg:
+        commands.append("scons build --cache-show --random")
+    if ":build:cmake" in project_cfg:
+        commands.append("make cmake && make build")
 
-	rcs = 0
-	for command in commands:
-		output = ["=" * 90, "Building: {} with {}".format(
-		          path / "main.cpp", "SCons" if "scons" in command else "CMake")]
-		rc, ro = run_command(path, command)
-		rcs += rc
-		print("\n".join(output + [ro]))
+    rcs = 0
+    for command in commands:
+        output = ["=" * 90, "Building: {} with {}".format(
+                  path / "main.cpp", "SCons" if "scons" in command else "CMake")]
+        rc, ro = run_command(path, command)
+        rcs += rc
+        print("\n".join(output + [ro]))
 
-	return None if rcs else project
+    return None if rcs else project
 
 def run(project):
-	path = project.parent
-	if is_running_in_ci:
-		command = "scons run"
-	else:
-		command = "python3 `which scons` run"
-	output = ["=" * 90, "Running: {} with {}".format(path / "main.cpp", "SCons" if "scons" in command else "CMake")]
-	rc, ro = run_command(path, command, all_output=True)
-	print("\n".join(output + [ro]))
-	if "CI: run fail" in project.read_text():
-		return None if not rc else project
-	return None if rc else project
+    path = project.parent
+    command = "scons run"
+    output = ["=" * 90, "Running: {} with {}".format(path / "main.cpp", "SCons" if "scons" in command else "CMake")]
+    rc, ro = run_command(path, command, all_output=True)
+    print("\n".join(output + [ro]))
+    if "CI: run fail" in project.read_text():
+        return None if not rc else project
+    return None if rc else project
 
-def compile_examples(paths):
-	print("Using {}x parallelism".format(cpus))
-	# Create build folder to prevent process race
-	cache_dir.mkdir(exist_ok=True, parents=True)
-	(cache_dir / "config").write_text('{"prefix_len": 2}')
-	# Find all project files
-	projects = [p for path in paths for p in Path(path).glob("**/project.xml")]
-	# first generate all projects
-	with multiprocessing.Pool(cpus) as pool:
-		projects = pool.map(generate, projects)
-	results = projects.count(None)
+def compile_examples(paths, jobs, split, part):
+    print("Using {}x parallelism".format(jobs))
+    # Create build folder to prevent process race
+    cache_dir.mkdir(exist_ok=True, parents=True)
+    (cache_dir / "config").write_text('{"prefix_len": 2}')
+    # Find all project files
+    projects = [p for path in paths for p in Path(path).glob("**/project.xml")]
+    projects.sort()
+    # Split projects up into parts
+    if split > 1:
+        chunk_size = math.ceil(len(projects) / args.split)
+        projects = projects[chunk_size*args.part:min(chunk_size*(args.part+1), len(projects))]
+    # first generate all projects
+    with multiprocessing.Pool(jobs) as pool:
+        projects = pool.map(generate, projects)
+    results = projects.count(None)
 
-	# Filter projects for successful generation
-	projects = [p for p in projects if p is not None]
-	# Then build the successfully generated ones
-	with multiprocessing.Pool(cpus) as pool:
-		projects = pool.map(build, projects)
-	results += projects.count(None)
+    # Filter projects for successful generation
+    projects = [p for p in projects if p is not None]
+    # Then build the successfully generated ones
+    with multiprocessing.Pool(jobs) as pool:
+        projects = pool.map(build, projects)
+    results += projects.count(None)
 
-	# Filter projects for successful compilation and runablity
-	projects = [p for p in projects if p is not None and "CI: run" in p.read_text()]
-	# Then run the successfully compiled ones
-	with multiprocessing.Pool(cpus) as pool:
-		projects = pool.map(run, projects)
-	results += projects.count(None)
+    # Filter projects for successful compilation and runablity
+    projects = [p for p in projects if p is not None and "CI: run" in p.read_text()]
+    # Then run the successfully compiled ones
+    with multiprocessing.Pool(jobs) as pool:
+        projects = pool.map(run, projects)
+    results += projects.count(None)
 
-	return results
+    return results
 
 
 if __name__ == '__main__':
-	exit(compile_examples(sys.argv[1:]))
+    parser = argparse.ArgumentParser(description='Run platform tests')
+    parser.add_argument(
+            "paths",
+            nargs="+",
+            help="Path to search examples in")
+    parser.add_argument(
+            "--jobs",
+            dest="jobs",
+            default=os.cpu_count(),
+            type=int,
+            help="Number of parallel jobs")
+    parser.add_argument(
+            "--part",
+            dest="part",
+            default=0,
+            type=int,
+            help="Execute this part of the splitting.")
+    parser.add_argument(
+            "--split",
+            dest="split",
+            default=1,
+            type=int,
+            help="Split the examples into this many parts.")
+    args = parser.parse_args()
+    exit(compile_examples(args.paths, args.jobs, args.split, args.part))
