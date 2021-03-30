af2b726032c9fe65af03871219ebf85019e3b86d
Raphael Lehmann <raphael@rleh.de>
2021-03-30 15:10:03 +0200

    [examples] Add Nucleo-L452RE blink example

---
+++ examples/nucleo_l452re/blink/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021, Raphael Lehmann
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <modm/board.hpp>
+
+using namespace Board;
+
+int
+main()
+{
+	initialize();
+
+	while (true)
+	{
+		LedGreen::toggle();
+		modm::delay(Button::read() ? 250ms : 500ms);
+	}
+
+	return 0;
+}
