8297cf350ae2410d4c74f280075e7fdfd763e4ff
andryblack <blackicebox@gmail.com>
2022-01-20 13:52:04 +0100

    [rp] Add USB module and TinyUSB port

---
+++ ext/rp/irq.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022, Andrey Kunitsyn
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+/**
+ * Pico SDK compatibility for tinyusb
+ */
+#include "hardware/regs/intctrl.h"
+
+static inline void
+irq_set_enabled(int irqn, bool enable)
+{
+	if (enable) NVIC_EnableIRQ(irqn);
+	else NVIC_DisableIRQ(irqn);
+}
+
+static inline void
+irq_set_exclusive_handler(int irqn, void (*handler)())
+{
+	(void)irqn;
+	(void)handler;
+	// do nothing, irq implemented at modm
+}
