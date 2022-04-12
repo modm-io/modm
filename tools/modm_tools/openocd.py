f975d7ea3842f42ab7b92a75b96a5a3a6b0716c4
cocasema <dev@cocasema.dev>
2022-04-12 12:12:49 -0700

    [openocd] Add support for custom OpenOCD path
    Based on discussion #845
    
    Raspberry Pi Pico provides its own version of OpenOCD
    which can be used with Raspberry Pi Picoprobe.
    
    Assuming you have RPi Pico stuff stored under ~/projects/pico/...:
    
     1. Build and install OpenOCD
    
      cd ~/projects/pico
      git clone https://github.com/raspberrypi/openocd.git
      cd openocd
      ./bootstrap
      mkdir build
      cd build
      ../configure --prefix=$(realpath ../install)
      make install
    
     2. Use it in your build terminal
    
       export MODM_OPENOCD_BINARY=$HOME/projects/pico/openocd/install/bin/openocd
       scons program
    
    scons: Reading SConscript files ...
    scons: done reading SConscript files.
    scons: Building targets ...
    ╭────────────── build/scons-release/pico-blinky.elf
    ╰───OpenOCD───> rp2040
    Open On-Chip Debugger 0.11.0-g4f2ae61 (2022-04-12-12:01)
    Licensed under GNU GPL v2
    For bug reports, read
            http://openocd.org/doc/doxygen/bugs.html
    Info : only one transport option; autoselect 'swd'
    Info : Hardware thread awareness created
    Info : Hardware thread awareness created
    Info : RP2040 Flash Bank Command
    Info : clock speed 5000 kHz
    Info : SWD DPIDR 0x0bc12477
    Info : SWD DLPIDR 0x00000001
    Info : SWD DPIDR 0x0bc12477
    Info : SWD DLPIDR 0x10000001
    Info : rp2040.core0: hardware has 4 breakpoints, 2 watchpoints
    Info : rp2040.core1: hardware has 4 breakpoints, 2 watchpoints
    Info : starting gdb server for rp2040.core0 on 3333
    Info : Listening on port 3333 for gdb connections
    target halted due to debug-request, current mode: Thread
    xPSR: 0xf1000000 pc: 0x000000ee msp: 0x20041f00
    target halted due to debug-request, current mode: Thread
    xPSR: 0xf1000000 pc: 0x000000ee msp: 0x20041f00
    ** Programming Started **
    Info : RP2040 B0 Flash Probe: 2097152 bytes @10000000, in 512 sectors
    
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    Info : Writing 4096 bytes starting at 0x0
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    ** Programming Finished **
    ** Verify Started **
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    target halted due to debug-request, current mode: Thread
    xPSR: 0x01000000 pc: 0x0000012a msp: 0x20041f00
    ** Verified OK **
    shutdown command invoked
    scons: done building targets

--- tools/modm_tools/openocd.py
+++ tools/modm_tools/openocd.py
@@ -25,6 +25,14 @@ You can also reset the target:
 python3 modm/modm_tools/openocd.py -f modm/openocd.cfg --reset
 ```
 
+You can use a different OpenOCD binary by setting the `MODM_OPENOCD_BINARY`
+environment variable before calling this script. This can be useful when
+using a custom OpenOCD build for specific targets.
+
+```sh
+export MODM_OPENOCD_BINARY=/path/to/other/openocd
+```
+
 (\* *only ARM Cortex-M targets*)
 """
 
@@ -78,7 +86,10 @@ def call(commands=None, config=None, search=None, blocking=True, silent=False, v
     # See http://openocd.org/doc/html/Running.html
     # os.environ.get("OPENOCD_SCRIPTS", "")
 
-    command_openocd = "openocd {} {} {}".format(
+    binary = os.environ.get("MODM_OPENOCD_BINARY", "openocd")
+
+    command_openocd = "{} {} {} {}".format(
+        binary,
         " ".join(map('-s "{}"'.format, search)),
         " ".join(map('-f "{}"'.format, config)),
         " ".join(map('-c "{}"'.format, commands))
