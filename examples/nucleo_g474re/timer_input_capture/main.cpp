3e8adf5c23981e3f011bed5ce1ac00880dbbb778
Steven Macías <ed.steven.macias@gmail.com>
2022-08-13 12:38:31 +0200

    Removed tailing spaces.

--- examples/nucleo_g474re/timer_input_capture/main.cpp
+++ examples/nucleo_g474re/timer_input_capture/main.cpp
@@ -6,14 +6,14 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- * 
- * 
+ *
+ *
  * ---------------------- Input Capture Example -----------------------------
- * 
+ *
  * 	Connect a jumper cable between PB7 and PC2 in your Nucleo G474RE.
  * 	PB7 will generate a PWM signal that will be monitored by PC2. 
  * 	The main goal of this example is to calculate the period of the input signal.
- * 
+ *
  * --------------------------------------------------------------------------
  */
 
@@ -37,7 +37,7 @@ constexpr float 	pwm_gen_pulse_width_ms 	= 1.5f;		// Milliseconds
 // -------------------- PWM Generator Timer Calculations --------------------
 // Desired PWM Freq = Clock Freq / Timer Prescaler / Overflow Value
 // Timer Prescaler = Clock Freq / (Desired PWM Freq * Overflow Value)
-constexpr uint16_t 	pwm_gen_prescaler 			= SystemClock::Frequency / ( pwm_gen_frequency_hz * pwm_gen_overflow ); 
+constexpr uint16_t 	pwm_gen_prescaler 			= SystemClock::Frequency / ( pwm_gen_frequency_hz * pwm_gen_overflow );
 constexpr float 	pwm_gen_period_ms			= ( 1.0 / pwm_gen_frequency_hz ) * 1000;
 constexpr uint16_t 	pwm_gen_ticks_per_period 	= pwm_gen_overflow / pwm_gen_period_ms;
 constexpr uint16_t 	pwm_pulse_width_in_ticks 	= pwm_gen_ticks_per_period * pwm_gen_pulse_width_ms;
@@ -46,7 +46,7 @@ constexpr uint16_t 	pwm_pulse_width_in_ticks 	= pwm_gen_ticks_per_period * pwm_g
 // Variables used to determine the period of the input signal
 uint16_t latest_input_timer_value 	= 0;
 uint16_t old_input_timer_value 		= 0;
-uint16_t ticks_between_interrupts 	= 0; 
+uint16_t ticks_between_interrupts 	= 0;
 
 // Interrupt Configuration
 Timer20::InputCapturePrescaler interrupt_prescaler = Timer20::InputCapturePrescaler::Div8;
@@ -57,7 +57,7 @@ uint8_t interrupt_priority 			= 10u;
  * @brief Function executed when a Capute Compare interruption is triggered.
  * Every time there is a rising edge in the input timer pin this code
  * will be executed (If InputCapturePrescaler is set to Div1). 
- * By saving the value of the capture compare register between interruptions 
+ * By saving the value of the capture compare register between interruptions
  * the period of the signal can be calculated.
  */
 MODM_ISR(TIM20_CC)
@@ -71,25 +71,25 @@ MODM_ISR(TIM20_CC)
 	temp_ticks_between_interrups = (latest_input_timer_value-old_input_timer_value);
 	
 	// Ingore negative values (overflow of counter register)
-	if ( temp_ticks_between_interrups > 0)	
+	if ( temp_ticks_between_interrups > 0)
 	{
 		ticks_between_interrupts = temp_ticks_between_interrups/interrupt_prescaler_value;
-	}	
+	}
 }
 
 /**
  * @brief Get the period of the input signal in milliseconds.
- * 
- * @return float 
+ *
+ * @return float
  */
 float getPeriodOfSignalInMs()
 {
-	return ticks_between_interrupts * input_capture_ms_per_tick; 
+	return ticks_between_interrupts * input_capture_ms_per_tick;
 }
 
 /**
  * @brief Setup of the timer used to generate the PWM Signal.
- * 
+ *
  */
 void generatePwm()
 {
@@ -106,17 +106,17 @@ void generatePwm()
 
 /**
  * @brief Setup of the timer used to measure an input signal.
- * 
+ *
  */
 void inputTimerConfig()
 {
 	Timer20::connect<GpioC2::Ch2>();
-	Timer20::enable(); 
+	Timer20::enable();
 	Timer20::setMode(Timer20::Mode::UpCounter);
 	Timer20::setPrescaler(input_capture_prescaler);
-	Timer20::setOverflow(input_capture_overflow); 
+	Timer20::setOverflow(input_capture_overflow);
 	Timer20::configureInputChannel(input_capture_channel, Timer20::InputCaptureMapping::InputOwn,
-									interrupt_prescaler, Timer20::InputCapturePolarity::Rising, 0, false); 
+									interrupt_prescaler, Timer20::InputCapturePolarity::Rising, 0, false);
 	Timer20::enableInterruptVector(Timer20::Interrupt::CaptureCompare2, true, interrupt_priority);
 	Timer20::enableInterrupt(Timer20::Interrupt::CaptureCompare2);
 	Timer20::applyAndReset();
@@ -138,18 +138,18 @@ int main()
 	MODM_LOG_INFO << modm::endl << "-----------------------------";
 	MODM_LOG_INFO << modm::endl << modm::endl;
 
-	MODM_LOG_INFO << "-- Setup Information --" << modm::endl ;
+	MODM_LOG_INFO << "-- Setup Information --" << modm::endl;
 	MODM_LOG_INFO << "Clock Frequency: " << SystemClock::Frequency << "Hz" << modm::endl;
 	MODM_LOG_INFO << modm::endl;
 
-	MODM_LOG_INFO << "-- PWM Generator Info --" << modm::endl ;
+	MODM_LOG_INFO << "-- PWM Generator Info --" << modm::endl;
 	MODM_LOG_INFO.printf("PWM Gen Frequency: %.2fHz\n", (double) pwm_gen_frequency_hz);
 	MODM_LOG_INFO.printf("PWM Gen Pulse Width: %.2fms\n", (double) pwm_gen_pulse_width_ms);
 	MODM_LOG_INFO << "PWM Gen Overflow: " << pwm_gen_overflow << modm::endl;
 	MODM_LOG_INFO << "PWM Gen Prescaler: " << pwm_gen_prescaler << modm::endl;
 	MODM_LOG_INFO << modm::endl;
 
-	MODM_LOG_INFO << "-- Input Capture Info --" << modm::endl ;
+	MODM_LOG_INFO << "-- Input Capture Info --" << modm::endl;
 	MODM_LOG_INFO.printf("Timer Frequency: %.2fHz\n", (double) input_capture_freq_hz);
 	MODM_LOG_INFO.printf("Timer Period: %.2fms\n", (double) input_capture_ms_per_tick);
 	MODM_LOG_INFO << "Timer Overflow: " << input_capture_overflow << modm::endl;
@@ -165,7 +165,7 @@ int main()
 	{
 		modm::delay(100ms);
 		
-		// If USER button is pressed: 
+		// If USER button is pressed:
 		if (!Button::read())
 		{
 			float periof_of_signal_ms = getPeriodOfSignalInMs();
