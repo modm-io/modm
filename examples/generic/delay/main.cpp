725a69ec2bc0b19d004fe4c51654c4e151cbda39
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2023-04-30 21:43:06 +0200

    [driver] Add cycle counter for AVR and Cortex-M

--- examples/generic/delay/main.cpp
+++ examples/generic/delay/main.cpp
@@ -10,6 +10,7 @@
 // ----------------------------------------------------------------------------
 
 #include <modm/board.hpp>
+#include <modm/driver/time/cycle_counter.hpp>
 
 using namespace Board;
 
@@ -18,6 +19,7 @@ using namespace Board;
 modm::IODeviceWrapper<UsbUart0, modm::IOBuffer::DiscardIfFull> usb_io_device;
 modm::log::Logger modm::log::info(usb_io_device);
 #endif
+modm_fastdata modm::CycleCounter counter;
 #ifdef TCNT1
 constexpr uint32_t SystemCoreClock = F_CPU;
 #endif
@@ -28,38 +30,18 @@ run_delay_ns(uint32_t ns)
 #ifdef CFG_TUSB_MCU
 	tud_task();
 #endif
-#ifdef TCNT1
-	uint16_t start, stop;
-#else
-	uint32_t start, stop;
-#endif
 	{
 		modm::atomic::Lock _;
-#ifdef TCNT1
-		start = TCNT1;
-#elif defined DWT
-		start = DWT->CYCCNT;
-#else
-		SysTick->CTRL = SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_CLKSOURCE_Msk;
-		SysTick->LOAD = (1ul << 23);
-		SysTick->VAL = 0;
-		stop = SysTick->VAL;
-#endif
+		counter.start();
 		modm::delay_ns(ns);
-#ifdef TCNT1
-		stop = TCNT1;
-#elif defined DWT
-		stop = DWT->CYCCNT;
-#else
-		start = SysTick->VAL;
-#endif
+		counter.stop();
 	}
 #ifdef CFG_TUSB_MCU
 	tud_task();
 #endif
-	const uint32_t cycles = (stop - start) - 4;
+	const uint32_t cycles = counter.cycles();
 	const uint32_t expected = uint64_t(SystemCoreClock) * ns / 1'000'000'000ull;
-	const uint32_t real = cycles * 1'000'000'000ull / SystemCoreClock;
+	const uint32_t real = counter.nanoseconds();
 	MODM_LOG_INFO.printf("%8lu | %7lu | %7lu | %8lu      %c\n", ns, expected, cycles, real,
 	    (cycles < expected*1.2f ? (cycles > expected*0.8f ? ' ' : '<') : '>')) << modm::flush;
 #ifdef CFG_TUSB_MCU
@@ -73,38 +55,18 @@ run_delay_us(uint32_t us)
 #ifdef CFG_TUSB_MCU
 	tud_task();
 #endif
-#ifdef TCNT1
-	uint16_t start, stop;
-#else
-	uint32_t start, stop;
-#endif
 	{
 		modm::atomic::Lock _;
-#ifdef TCNT1
-		start = TCNT1;
-#elif defined DWT
-		start = DWT->CYCCNT;
-#else
-		SysTick->CTRL = SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_CLKSOURCE_Msk;
-		SysTick->LOAD = (1ul << 23);
-		SysTick->VAL = 0;
-		stop = SysTick->VAL;
-#endif
+		counter.start();
 		modm::delay_us(us);
-#ifdef TCNT1
-		stop = TCNT1;
-#elif defined DWT
-		stop = DWT->CYCCNT;
-#else
-		start = SysTick->VAL;
-#endif
+		counter.stop();
 	}
 #ifdef CFG_TUSB_MCU
 	tud_task();
 #endif
-	const uint32_t cycles = (stop - start) - 4;
+	const uint32_t cycles = counter.cycles();
 	const uint32_t expected = uint64_t(SystemCoreClock) * us / 1'000'000ull;
-	const uint32_t real = cycles * 1'000'000ull / SystemCoreClock;
+	const uint32_t real = counter.microseconds();
 	MODM_LOG_INFO.printf("%8lu | %8lu | %8lu | %8lu      %c\n", us, expected, cycles, real,
 	    (cycles < expected*1.2f ? (cycles > expected*0.8f ? ' ' : '<') : '>')) << modm::flush;
 #ifdef CFG_TUSB_MCU
@@ -174,9 +136,7 @@ run_test_us(bool short_test=false)
 int main()
 {
 	Board::initialize();
-
-	TCCR1A = 0;
-	TCCR1B = (1 << CS10);
+	counter.initialize();
 
 	run_test_ns();
 	run_test_us();
@@ -192,6 +152,7 @@ int main()
 {
 	Board::initialize();
 	Board::initializeUsbFs();
+	counter.initialize(true);
 	tusb_init();
 
 	MODM_LOG_INFO << "Hello World\n";
@@ -229,11 +190,13 @@ int main()
 {
 	Board::stlink::Uart::connect<Board::stlink::Tx::Tx>();
 	Board::stlink::Uart::initialize<BootClock, 115200_Bd, 5_pct>();
+	counter.initialize(true);
 
 	run_test_ns();
 	run_test_us();
 
 	Board::initialize();
+	counter.initialize(true);
 
 	run_test_ns();
 	run_test_us();
