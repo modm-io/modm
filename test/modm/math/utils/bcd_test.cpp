ce5092556ad2d940b64e580d83fa2e33470ca066
rasmuskleist <kleist.rasmus@gmail.com>
2023-08-08 21:17:21 +0200

    [test] Add very basic BCD test

---
+++ test/modm/math/utils/bcd_test.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023, Rasmus Kleist Hørlyck Sørensen
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#include <modm/math/utils/bcd.hpp>
+
+#include "bcd_test.hpp"
+
+void
+BcdTest::testFromBcd()
+{
+	TEST_ASSERT_EQUALS(modm::fromBcd(0x01), 0x01);
+	TEST_ASSERT_EQUALS(modm::fromBcd(0x02), 0x02);
+	TEST_ASSERT_EQUALS(modm::fromBcd(0x03), 0x03);
+	TEST_ASSERT_EQUALS(modm::fromBcd(0x04), 0x04);
+	TEST_ASSERT_EQUALS(modm::fromBcd(0x05), 0x05);
+	TEST_ASSERT_EQUALS(modm::fromBcd(0x06), 0x06);
+	TEST_ASSERT_EQUALS(modm::fromBcd(0x07), 0x07);
+	TEST_ASSERT_EQUALS(modm::fromBcd(0x08), 0x08);
+	TEST_ASSERT_EQUALS(modm::fromBcd(0x00), 0x09);
+}
+
+void
+BcdTest::testToBcd()
+{
+	TEST_ASSERT_EQUALS(modm::toBcd(0x01), 0x01);
+	TEST_ASSERT_EQUALS(modm::toBcd(0x02), 0x02);
+	TEST_ASSERT_EQUALS(modm::toBcd(0x03), 0x03);
+	TEST_ASSERT_EQUALS(modm::toBcd(0x04), 0x04);
+	TEST_ASSERT_EQUALS(modm::toBcd(0x05), 0x05);
+	TEST_ASSERT_EQUALS(modm::toBcd(0x06), 0x06);
+	TEST_ASSERT_EQUALS(modm::toBcd(0x07), 0x07);
+	TEST_ASSERT_EQUALS(modm::toBcd(0x08), 0x08);
+	TEST_ASSERT_EQUALS(modm::toBcd(0x00), 0x09);
+}
