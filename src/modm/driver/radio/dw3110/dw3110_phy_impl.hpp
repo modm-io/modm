6f6b3fb541ae4ebc76ec29c87466f53f7ac2081a
Elias H. <46895028+kingelilol@users.noreply.github.com>
2024-03-06 00:04:07 +0100

    WIP: [driver] radio: abstract dw3110 send function (broken)

--- src/modm/driver/radio/dw3110/dw3110_phy_impl.hpp
+++ src/modm/driver/radio/dw3110/dw3110_phy_impl.hpp
@@ -39,24 +39,49 @@ template<typename SpiMaster, typename Cs>
 modm::ResumableResult<bool>
 modm::Dw3110Phy<SpiMaster, Cs>::ping()
 {
-	uint16_t tx_header = 0;
-	uint32_t rx_data = 0;
 	RF_BEGIN();
+	ping_result = RF_CALL(
+		send(modm::Dw3110Register::SpiMode::FULL_READ, modm::Dw3110Register::DwRegister::DEV_ID));
+	RF_END_RETURN((ping_result & ~(0b1111)) == 0xDECA0300);
+}
+
+template<typename SpiMaster, typename Cs>
+modm::ResumableResult<uint32_t>
+modm::Dw3110Phy<SpiMaster, Cs>::send(modm::Dw3110Register::SpiMode mode,
+									 modm::Dw3110Register::DwRegister address)
+{
+	RF_BEGIN();
+	RF_END_RETURN_CALL(send(mode, address, 0));
+}
+
+template<typename SpiMaster, typename Cs>
+modm::ResumableResult<uint32_t>
+modm::Dw3110Phy<SpiMaster, Cs>::send(modm::Dw3110Register::SpiMode mode,
+									 modm::Dw3110Register::DwRegister address, uint32_t data)
+{
+
+
+	RF_BEGIN();
+	send_tx_header = 0;
+	send_rx_data = 0;
 
 	RF_WAIT_UNTIL(this->acquireMaster());
 	Cs::reset();
 
-	tx_header = static_cast<uint16_t>(modm::Dw3110Register::SpiMode::FULL_READ) << 8;
-	tx_header |= static_cast<uint16_t>(modm::Dw3110Register::DwRegister::DEV_ID);
-	*((uint16_t *)tx_buffer.data()) = modm::toBigEndian(tx_header);
+	send_tx_header = static_cast<uint16_t>(mode) << 8;
+	send_tx_header |= static_cast<uint16_t>(address);
+	*((uint16_t *)tx_buffer.data()) = modm::toBigEndian(send_tx_header);
+	*((uint32_t *)((uint16_t *)tx_buffer.data() + 1)) = modm::toBigEndian(data)
 
-	RF_CALL(SpiMaster::transfer(tx_buffer.data(), rx_buffer.data(), 2 + 4));
+		RF_CALL(SpiMaster::transfer(tx_buffer.data(), rx_buffer.data(), 2 + 4));
 
-	// To read the response ignore the first 2 Byte then store the response 4 Byte
-	rx_data = *((uint32_t *)((uint8_t *)rx_buffer.data() + 2));
+	if (mode == modm::Dw3110Register::SpiMode::FULL_READ ||
+		mode == modm::Dw3110Register::SpiMode::SHORT_READ)
+	{
+		send_rx_data = *((uint32_t *)((uint8_t *)rx_buffer.data() + 2));
+	}
 
 	if (this->releaseMaster()) { Cs::set(); }
 
-	// Ignore minor changes wich are tracked in the lower 4 bits
-	RF_END_RETURN((rx_data & ~(0b1111)) == 0xDECA0300);
+	RF_END_RETURN(send_rx_data);
 }
