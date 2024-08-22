123c0d4b2850ae2a5a0452c1d5780b1120ff8491
Michael Jossen <michael.jossen@rwth-aachen.de>
2024-08-22 15:33:35 +0200

    [examples] nucleo_f401re DW3110 Update to fix compile errors

--- examples/nucleo_f401re/dw3110-communication/main.cpp
+++ examples/nucleo_f401re/dw3110-communication/main.cpp
@@ -49,7 +49,7 @@ public:
 			txdata[txdata.size() - 1]++;
 			timeout.restart(Button::read() ? 500ms : 10ms);
 			PT_WAIT_UNTIL(timeout.execute());
-			if (PT_CALL(radio.transmit(txdata, true)))
+			if (PT_CALL(radio.transmit(txdata, true)) == modm::Dw3110::Error::None)
 			{
 				sentCount++;
 			} else
