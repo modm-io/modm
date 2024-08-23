63fc406035f0f07942ee0a830422bdc820b821df
Michael Jossen <michael.jossen@rwth-aachen.de>
2024-08-23 12:53:06 +0200

    [driver] radio: dw3110 Improve docs

--- src/modm/driver/radio/dw3110/dw3110_phy.hpp
+++ src/modm/driver/radio/dw3110/dw3110_phy.hpp
@@ -27,7 +27,7 @@ namespace modm
 {
 
 /**
- * Hardware abstraction layer for DW3110
+ * Hardware abstraction layer for DW3110 \n
  * Unsupported Features: AES encryption, Double buffering, GPIO, Temperature and
  * Voltage, Pulse Generator calibration, RX antenna delay temp compensation,
  * Soft reset, Sleep, Sniff mode, Delayed transmission, STS
@@ -42,288 +42,290 @@ class Dw3110Phy : public modm::SpiDevice<SpiMaster>, protected modm::NestedResum
 public:
 	Dw3110Phy();
 
-	// Set the UWB channel used
+	/// Set the UWB channel used
 	modm::ResumableResult<void>
 	setChannel(Dw3110::Channel channel);
 
-	// Change Header format to non-standard to allow 1021 Byte payloads instead of
-	// default 125
+	/// Change Header format to non-standard to allow 1021 Byte payloads instead of
+	/// default 125
 	modm::ResumableResult<void>
 	setEnableLongFrames(bool value);
 
-	// Send the PHR at 6.8Mbps
-	// By default the PHR is always sent at 850kb/s
+	/// Send the PHR at 6.8Mbps. \n
+	/// By default the PHR is always sent at 850kb/s
 	modm::ResumableResult<void>
 	setSendHeaderFast(bool value);
 
-	// Set the timeout to wait on arriving packets after startReceive()
-	// 0 = No Timeout
-	// Register resolution is ~1µs
+	/// Set the timeout to wait on arriving packets after startReceive() \n
+	/// 0 = No Timeout \n
+	/// Register resolution is ~1µs
 	modm::ResumableResult<void>
 	setReceiveWaitTimeout(modm::chrono::micro_clock::duration duration);
 
-	// Set whether to stay in receive mode after receive failure
-	// TODO test
+	/// Set whether to stay in receive mode after receive failure
+	/// TODO test
 	modm::ResumableResult<void>
 	setReenableOnRxFailure(bool value);
 
-	// Set the time spent listening for competing transmissions on CCA commands
-	// Unit is in counts of PAC symbols
+	/// Set the time spent listening for competing transmissions on CCA commands \n
+	/// Unit is in counts of PAC symbols
 	modm::ResumableResult<void>
 	setCCATimeout(uint16_t timeout);
 
-	// Enable a faster TX/RX turnaround.
-	// The Frame will be set ready before computation of ToA data has finished and
-	// any ACKs will be sent Time of Arrival may not be ready when the frame is
-	// made available
-	// TODO test
+	/// Enable a faster TX/RX turnaround. \n
+	/// The Frame will be set ready before computation of ToA data has finished and
+	/// any ACKs will be sent. \n
+	/// Time of Arrival may not be ready when the frame is
+	/// made available
 	modm::ResumableResult<void>
 	setEnableFastTurnaround(bool value);
 
-	// Read the value of the internal chip clock
+	/// Read the value of the internal chip clock
 	modm::ResumableResult<uint32_t>
 	readChipTime();
 
-	// Set the number of preamble symbols sent with each transmission
-	// Each symbol takes ~1µs to send
-	// Note: Fine tuning of preamble length is currently unsupported by this
-	// driver
+	/// Set the number of preamble symbols sent with each transmission \n
+	/// Each symbol takes ~1µs to send \n
+	/// @warning While further fine tuning of preamble length is possible it is currently
+	/// unsupported by this driver
 	modm::ResumableResult<void>
 	setPreambleLength(Dw3110::PreambleLength len);
 
-	// Get the timestamp of the last arrived packet.
-	// This timestamp already has various correction factors applied to it.
-	// It is given in ~15.65 picoseconds per unit
+	/// Get the timestamp of the last arrived packet. \n
+	/// This timestamp already has various correction factors applied to it. \n
+	/// It is given in ~15.65 picoseconds per unit
 	modm::ResumableResult<uint64_t>
 	getReceiveTimestamp();
 
-	// Analogous to getReceiveTimestamp
+	/// Analogous to getReceiveTimestamp
 	modm::ResumableResult<uint64_t>
 	getTransmitTimestamp();
 
-	// Set the time between RX of a packet and the TX of the acknowledgement
-	// Specified in number of preamble symbols, so time depends on the PRF
+	/// Set the time between RX of a packet and the TX of the acknowledgement \n
+	/// Specified in number of preamble symbols, so time depends on the PRF
 	modm::ResumableResult<void>
 	setAcknowledgeTurnaround(uint8_t time);
 
-	// Set the time between a transmission and the start of RX on any of the RX
-	// after TX commands This can be used to delay turning on of the receiver
-	// after transmission to save on power.
+	/// Set the time between a transmission and the start of RX on any of the RX
+	/// after TX commands \n
+	/// This can be used to delay turning on of the receiver after transmission
+	/// to save on power.
 	modm::ResumableResult<void>
 	setWaitForResponseTime(modm::PreciseClock::duration time);
 
-	// Set the start frame delimiter used by the chip
+	/// Set the start frame delimiter used by the chip
 	modm::ResumableResult<void>
 	setSFD(Dw3110::StartFrameDelimiter sfd);
 
-	// Set the preamble code used and looked for.
-	// Note that this implicitly sets the PRF as codes >8 use the 64MHz PRF
-	// instead of 16MHz
+	/// Set the preamble code used and looked for.
+	/// @warning This implicitly sets the PRF as codes >8 use the 64MHz PRF
+	/// instead of 16MHz
 	modm::ResumableResult<void>
 	setPreambleCode(Dw3110::PreambleCode rx, Dw3110::PreambleCode tx);
 
-	/**
-	 * MAC and Frame filtering
-	 *
-	 * Does minimal filtering and processing and AUTO-ACK.
-	 * Proper IEEE802.15.4 frames have to be provided for this to work.
-	 *
-	 */
-
-	// Get devices unique id
+	/// Get devices unique id
 	modm::ResumableResult<uint64_t>
 	getDeviceUID();
 
-	// Set devices unique id
+	/// Set devices unique id
 	modm::ResumableResult<void>
 	setDeviceUID(uint64_t uid);
 
-	// Get devices pan id
+	/// Get devices pan id
 	modm::ResumableResult<uint16_t>
 	getPanUID();
 
-	// Get devices short id
+	/// Get devices short id
 	modm::ResumableResult<uint16_t>
 	getShortUID();
 
-	// Set devices pan id
+	/// Set devices pan id
 	modm::ResumableResult<void>
 	setPanUID(uint16_t pid);
 
-	// Set devices short id
+	/// Set devices short id
 	modm::ResumableResult<void>
 	setShortUID(uint16_t pid);
 
-	// Enable or disable the frame filtering
+	/// Enable or disable the frame filtering
 	modm::ResumableResult<void>
 	setFrameFilterEnabled(bool value);
 
-	// Get current filtering config
+	/// Get current filtering config
 	modm::ResumableResult<Dw3110::FilterConfig_t>
 	getFilterConfig();
 
-	// Set frame filtering config
+	/// Set frame filtering config
 	modm::ResumableResult<void>
 	setFilterConfig(Dw3110::FilterConfig_t fc);
 
-	// Enable auto acknowledgment
-	// Will automatically respond to valid 802.5.14 Frames with the ACK request
-	// bit set Only active when Frame filtering is enabled
+	/// Enable auto acknowledgment \n
+	/// Will automatically respond to valid 802.5.14 Frames with the ACK request
+	/// bit set \n
+	/// Only active when Frame filtering is enabled
 	modm::ResumableResult<void>
 	setAutoAckEnabled(bool value);
 
-	//-----------------------------------
-
-	// Read the reported state of the chip
+	/// Read the reported state of the chip
 	modm::ResumableResult<Dw3110::SystemState>
 	getChipState();
 
-	// Runs the RX calibration, needs to be done after startup, wake and after 20C
-	// temperature change Needs to be run in IDLE_PLL state Automatically run in
-	// initialize
+	/// Runs the RX calibration. \n
+	/// Needs to be done after startup, wake and after 20C temperature change \n
+	/// Needs to be run when chip is in IDLE_PLL state \n
+	/// Automatically run by @ref initialize()
 	modm::ResumableResult<bool>
 	calibrate();
 
-	// Initialize the DW3000, make ready to receive/transmit
-	// Call ideally after the IRQ Pin has gone high, signalling SPIRDY
+	/// Initialize the DW3000, make ready to receive/transmit \n
+	/// Call ideally after the IRQ Pin has gone high, signalling SPIRDY
 	modm::ResumableResult<bool>
 	initialize(Dw3110::Channel channel = Dw3110::Channel::Channel5,
 			   Dw3110::PreambleCode pcode = Dw3110::PreambleCode::Code_64Mhz_9,
 			   Dw3110::PreambleLength plen = Dw3110::PreambleLength::Preamble_64,
 			   Dw3110::StartFrameDelimiter sfd = Dw3110::StartFrameDelimiter::IEEE802_15_4z_8);
 
-	// Set the chip into receive mode
+	/// Set the chip into receive mode
 	modm::ResumableResult<bool>
 	startReceive();
 
-	// Check if a packet has been successfully received
+	/// Check if a packet has been successfully received
 	modm::ResumableResult<bool>
 	packetReady();
 
-	// Check if the chip is currently in RX mode
+	/// Check if the chip is currently in RX mode
 	modm::ResumableResult<bool>
 	isReceiving();
 
-	// Copy received packet into the provided payload buffer, clear packet
-	// received flags
+	/// Copy received packet into the provided payload buffer, clear packet
+	/// received flags
+	/// @param payload Region of memory the payload will be written to
+	/// @param payload_len Contains the length of the received payload
 	modm::ResumableResult<bool>
 	fetchPacket(std::span<uint8_t> payload, size_t &payload_len);
 
-	// Transmit a given package using the current configuration using a given transmission mode
-	// ranging parameter  decides whether or not to set the ranging bit in the header
-	// fast parameter decides if the data portion is sent at 850kbps or 6.8Mbps
+	/// Transmit a given package using the current configuration using a given transmission mode
+	/// @param payload Span to the desired payload
+	/// @param ranging Decides whether or not to set the ranging bit in the header
+	/// @param fast Decides if the data portion is sent at 850kbps or 6.8Mbps
 	template<Dw3110::TXMode tmode = Dw3110::TXMode::Default>
 	modm::ResumableResult<Dw3110::Error>
 	transmit(const std::span<const uint8_t> payload, bool ranging = true, bool fast = true);
 
-	// Set the reference time value
-	// Used as the reference in DelayMode::DelayWRTRef
+	/// Set the reference time value \n
+	/// Used as the reference in @ref transmitDelayed() when using DelayTXMode::DelayWRTRef and
+	/// DelayTXMode::DelayWRTRefAndReceive
 	modm::ResumableResult<void>
 	setReferenceTime(uint32_t time);
 
-	// Transmit a given package using the current configuration using a given delayed transmission
-	// mode ranging parameter  decides whether or not to set the ranging bit in the header fast
-	// parameter decides if the data portion is sent at 850kbps or 6.8Mbps parameter time is
-	// dependent on the DelayTXMode provided but is in units of 4ns
-	// Note that this transmission mode does not respect the clear channel assessment
+	/// Transmit a given package using the current configuration using a given delayed transmission
+	/// mode
+	/// @param payload Span to the desired payload
+	/// @param ranging Decides whether or not to set the ranging bit in the header
+	/// @param fast Decides if the data portion is sent at 850kbps or 6.8Mbps
+	/// @param time Either the transmission time or a delay in units of 4ns dependent on the
+	/// DelayTXMode provided
+	/// @warning This transmission mode does not respect the clear channel assessment
 	template<Dw3110::DelayTXMode dmode = Dw3110::DelayTXMode::AtTime>
 	modm::ResumableResult<Dw3110::Error>
 	transmitDelayed(uint32_t time, const std::span<const uint8_t> payload, bool ranging = true,
 					bool fast = true);
 
-	// Read the current system status register
+	/// Read the current system status register
 	modm::ResumableResult<Dw3110::SystemStatus_t>
 	getStatus();
 
-	// Clear the given bits in the status register
+	/// Clear the given bits in the status register
 	modm::ResumableResult<void>
 	clearStatusBits(Dw3110::SystemStatus_t mask);
 
-private:
-	// Transmit a given package using the current configuration and a specific command
-	// fast parameter decides if the data portion is sent at 850kbps or 6.8Mbps
-	// command fast parameter decides if the data portion is sent at 850kbps
-	// or 6.8Mbps
+protected:
+	/// Transmit a given package using the current configuration and a specific command
+	/// @param payload Span to the desired payload
+	/// @param ranging Decides whether or not to set the ranging bit in the header
+	/// @param fast Decides if the data portion is sent at 850kbps or 6.8Mbps
+	/// command
 	template<modm::Dw3110::FastCommand Cmd>
 	modm::ResumableResult<Dw3110::Error>
 	transmitGeneric(const std::span<const uint8_t> payload, bool ranging, bool fast);
 
-	// Only load configuration independent stuff, everything else should be
-	// initialized when changing those parts
+	/// Only load configuration independent stuff, everything else should be
+	/// initialized when changing those parts
 	modm::ResumableResult<void>
 	loadOTP();
 
-	// Check the device ID over SPI
+	/// Check the device ID over SPI
 	modm::ResumableResult<bool>
 	checkDevID();
 
-	// Helper function checking equality of two spans
+	/// Helper function checking equality of two spans
 	template<size_t Len>
 	bool
 	checkResult(std::span<const uint8_t, Len> expected, std::span<const uint8_t, Len> got);
 
-	// Send a command to the chip
+	/// Send a command to the chip
 	template<Dw3110::FastCommand Cmd>
 	modm::ResumableResult<void>
 	sendCommand();
 
-	// Recompute the SFD TOC
+	/// Recompute the SFD TOC
 	modm::ResumableResult<void>
 	setRX_SFD_TOC();
 
-	// Update the local system_status variable
+	/// Update the local system_status variable
 	modm::ResumableResult<void>
 	fetchSystemStatus();
 
-	// Update the local chip_state variable
+	/// Update the local chip_state variable
 	modm::ResumableResult<void>
 	fetchChipState();
 
-	// Due to an errata the check is a bit more complicated
-	// Relies on system_status and chip_state being up to date
+	/// Due to an errata the check is a bit more complicated \n
+	/// Relies on system_status and chip_state being up to date
 	modm::ResumableResult<modm::Dw3110::Error>
 	checkTXFailed();
 
-	// Read a variable from the OTP memory
+	/// Read a variable from the OTP memory
 	template<Dw3110::OTPAddr Addr>
 	modm::ResumableResult<void>
 	readOTPMemory(std::span<uint8_t, 4> out);
 
-	// Read a variable from a register
+	/// Read a variable from a register
 	template<Dw3110::Register Reg, size_t Len, size_t Offset = 0>
 	modm::ResumableResult<void>
 	readRegister(std::span<uint8_t, Len> out);
 
-	// Read a number of bytes from a register bank, useful for RX buffers and
-	// other large read transfers
+	/// Read a number of bytes from a register bank, useful for RX buffers and
+	/// other large read transfers
 	template<Dw3110::RegisterBank Reg>
 	modm::ResumableResult<void>
 	readRegisterBank(std::span<uint8_t> out, size_t len);
 
-	// Write a variable to a register
+	/// Write a variable to a register
 	template<Dw3110::Register Reg, size_t Len, size_t Offset = 0>
 	modm::ResumableResult<void>
 	writeRegister(const std::span<const uint8_t, Len> val);
 
-	// Simple implementation of read modify write using and and or masks
-	// Use on registers that do not support native write register masked
+	/// Simple implementation of read modify write using and and or masks \n
+	/// Use on registers that do not support native write register masked
 	template<Dw3110::Register Reg, size_t Len, size_t Offset = 0>
 	modm::ResumableResult<void>
 	readModifyWriteRegister(const std::span<const uint8_t, Len> or_mask,
 							const std::span<const uint8_t, Len> and_mask);
 
-	// Do not use to clear "write 1 to clear" bits (2.3.1.2 Table 3)
+	/// Do not use to clear "write 1 to clear" bits (2.3.1.2 Table 3)
 	template<Dw3110::Register Reg, size_t Len, size_t Offset = 0>
 	modm::ResumableResult<void>
 	writeRegisterMasked(const std::span<const uint8_t, Len> or_mask,
 						const std::span<const uint8_t, Len> and_mask);
 
-	// Write a number of bytes to a register bank, useful for TX buffers and other
-	// large write transfers
+	/// Write a number of bytes to a register bank, useful for TX buffers and other
+	/// large write transfers
 	template<Dw3110::RegisterBank Reg>
 	modm::ResumableResult<void>
 	writeRegisterBank(const std::span<const uint8_t> val, size_t len);
 
+private:
 	PreciseTimeout timeout;
 
 	Dw3110::SystemStatus_t system_status{0};
