51780b44071183a76e0e1b7a304774ac495ffb61
Hannes Ellinger <hannes.ellinger@eas.iis.fraunhofer.de>
2025-07-13 16:48:59 +0200

    clean up gpio code

---
+++ src/modm/platform/gpio/nrf/connector.hpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020, Erik Henriksson
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+#include <modm/platform/core/peripherals.hpp>
+
+namespace modm
+{
+
+namespace platform
+{
+
+// GpioConnector only exists for backwards compability with bitbang API.
+template<Peripheral peripheral, class... Signals>
+struct GpioConnector
+{
+	template<class GpioQuery>
+	static constexpr bool Contains = true;
+
+	inline static void connect() {}
+	inline static void disconnect() {}
+};
+/// @endcond
+
+}  // namespace platform
+
+}  // namespace modm
