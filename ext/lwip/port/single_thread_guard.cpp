2a0d579933a5b90f40a638a6e56d078ce5dd537a
Kaelin Laundry <wasabifan@outlook.com>
2026-08-09 22:45:15 -0700

    fixup! [lwip] Add lwIP module and configs for NO_SYS API

--- ext/lwip/port/single_thread_guard.cpp
+++ ext/lwip/port/single_thread_guard.cpp
@@ -8,54 +8,40 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include <modm/architecture/detect.hpp>
 #include <modm/architecture/interface/assert.hpp>
-#ifdef MODM_CPU_CORTEX_M
-#include <modm/platform/device.hpp>
-#endif
+#include <modm/architecture/interface/interrupt.hpp>
+
+#include <atomic>
 
 namespace
 {
 
 // lwIP calls are synchronous and must not yield. Entry tracking catches
 // accidental re-entry added to these call paths later.
-bool entered{false};
-
-// TODO: is there a good place to put this in modm::architecture or modm::platform::core?
-bool
-isForeground()
-{
-#ifdef MODM_CPU_CORTEX_M
-	return __get_IPSR() == 0;
-#else
-	return true;
-#endif
-}
+std::atomic_bool entered{false};
 
 } // namespace
 
 extern "C" void
 modm_lwip_single_thread_acquire()
 {
-	modm_assert(isForeground(), "lwip.context",
+	modm_assert(not modm::isInterruptContext(), "lwip.context",
 			"lwIP cannot be entered from an interrupt");
-	modm_assert(not entered, "lwip.owner", "lwIP core already locked");
-	entered = true;
+	modm_assert(not entered.exchange(true), "lwip.owner", "lwIP core already locked");
 }
 
 extern "C" void
 modm_lwip_single_thread_release()
 {
-	modm_assert(isForeground(), "lwip.context",
+	modm_assert(not modm::isInterruptContext(), "lwip.context",
 			"lwIP cannot be released from an interrupt");
-	modm_assert(entered, "lwip.owner", "lwIP core not locked");
-	entered = false;
+	modm_assert(entered.exchange(false), "lwip.owner", "lwIP core not locked");
 }
 
 extern "C" void
 modm_lwip_assert_core_locked()
 {
-	modm_assert(isForeground(), "lwip.context",
+	modm_assert(not modm::isInterruptContext(), "lwip.context",
 			"lwIP cannot be entered from an interrupt");
-	modm_assert(entered, "lwip.owner", "lwIP core not locked");
+	modm_assert(entered.load(), "lwip.owner", "lwIP core not locked");
 }
