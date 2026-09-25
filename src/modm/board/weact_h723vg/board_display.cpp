baf4a0bb03eac0fdf545ced1dd03ce2819258c70
LucaFeggi <lucadona2005@gmail.com>
2026-09-25 10:57:56 +0200

    [stm32] Add WeAct MiniSTM32H723 board support

---
+++ src/modm/board/weact_h723vg/board_display.cpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2026, Niklas Hauser
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#include <modm/board.hpp>
+
+namespace Board
+{
+
+Display display{lcd::Profile};
+
+}  // namespace Board
